4025fbcf6cbf65b2b1f577d6acc009fc4cf31601
Aurimas K <4urimas@gmail.com>
2014-10-07 16:18:14 +0300

    Refactor profile edit controller from account settings #12 #15

---
+++ public/modules/users/controllers/profile.edit.client.controller.js
@@ -0,0 +1,94 @@
+'use strict';
+
+angular.module('users').controller('EditProfileController', ['$scope', '$modal', '$http', '$stateParams', '$state', '$location', 'Users', 'Authentication',
+  function($scope, $modal, $http, $stateParams, $state, $location, Users, Authentication) {
+    $scope.user = Authentication.user;
+    $scope.profile = false;
+
+    // If user is not signed in then redirect back home
+    if (!$scope.user) $location.path('/');
+
+    // Check if there are additional accounts
+    $scope.hasConnectedAdditionalSocialAccounts = function(provider) {
+      for (var i in $scope.user.additionalProvidersData) {
+        return true;
+      }
+
+      return false;
+    };
+
+    // Check if provider is already in use with current user
+    $scope.isConnectedSocialAccount = function(provider) {
+      return $scope.user.provider === provider || ($scope.user.additionalProvidersData && $scope.user.additionalProvidersData[provider]);
+    };
+
+    // Remove a user social account
+    $scope.removeUserSocialAccount = function(provider) {
+      $scope.success = $scope.error = null;
+
+      $http.delete('/users/accounts', {
+        params: {
+          provider: provider
+        }
+      }).success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        $scope.user = Authentication.user = response;
+      }).error(function(response) {
+        $scope.error = response.message;
+      });
+    };
+
+    // Update a user profile
+    $scope.updateUserProfile = function(isValid) {
+      if (isValid){
+        $scope.success = $scope.error = null;
+        var user = new Users($scope.user);
+
+        user.$update(function(response) {
+          $scope.success = true;
+          Authentication.user = response;
+          $state.go('profile-updated', {username: response.username, updated: true});
+        }, function(response) {
+          $scope.error = response.data.message;
+        });
+      } else {
+        $scope.submitted = true;
+      }
+    };
+
+    $scope.findProfile = function() {
+      if(!$stateParams.username) {
+        $scope.profile = $scope.user;
+      }
+      else {
+        $scope.profile = Users.get({
+          username: $stateParams.username
+        });
+      }
+    };
+
+    /**
+    * Open avatar -modal
+    */
+    $scope.avatarModal = function (user, $event) {
+
+      if($event) $event.preventDefault();
+
+      var modalInstance = $modal.open({
+        templateUrl: 'avatar.client.modal.html', //inline at template
+        controller: function ($scope, $modalInstance) {
+          $scope.user = user;
+          $scope.close = function () {
+            $modalInstance.dismiss('close');
+          };
+        }
+      });
+
+    };
+
+    $http.get('js/data.json').success (function(data){
+            $scope.guitarVariable = data;
+
+  }
+]);
--- public/modules/users/controllers/settings.client.controller.js
+++ public/modules/users/controllers/settings.client.controller.js
@@ -1,62 +1,12 @@
 'use strict';
 
-angular.module('users').controller('SettingsController', ['$scope', '$modal', '$http', '$stateParams', '$state', '$location', 'Users', 'Authentication',
-	function($scope, $modal, $http, $stateParams, $state, $location, Users, Authentication) {
-		$scope.user = Authentication.user;
-		$scope.profile = false;
+angular.module('users').controller('SettingsController', ['$scope', '$http', '$location', 'Users',
+	function($scope, $http, $location, Users) {
 
 		// If user is not signed in then redirect back home
 		if (!$scope.user) $location.path('/');
 
-		// Check if there are additional accounts
-		$scope.hasConnectedAdditionalSocialAccounts = function(provider) {
-			for (var i in $scope.user.additionalProvidersData) {
-				return true;
-			}
-
-			return false;
-		};
-
-		// Check if provider is already in use with current user
-		$scope.isConnectedSocialAccount = function(provider) {
-			return $scope.user.provider === provider || ($scope.user.additionalProvidersData && $scope.user.additionalProvidersData[provider]);
-		};
-
-		// Remove a user social account
-		$scope.removeUserSocialAccount = function(provider) {
-			$scope.success = $scope.error = null;
-
-			$http.delete('/users/accounts', {
-				params: {
-					provider: provider
-				}
-			}).success(function(response) {
-				// If successful show success message and clear form
-				$scope.success = true;
-				$scope.user = Authentication.user = response;
-			}).error(function(response) {
-				$scope.error = response.message;
-			});
-		};
-
-		// Update a user profile
-		$scope.updateUserProfile = function(isValid) {
-			if (isValid){
-				$scope.success = $scope.error = null;
-				var user = new Users($scope.user);
-
-				user.$update(function(response) {
-					$scope.success = true;
-					Authentication.user = response;
-					$state.go('profile-updated', {username: response.username, updated: true});
-				}, function(response) {
-					$scope.error = response.data.message;
-				});
-			} else {
-				$scope.submitted = true;
-			}
-		};
-
+	
 		// Change user password
 		$scope.changeUserPassword = function() {
 			$scope.success = $scope.error = null;
@@ -70,35 +20,6 @@ angular.module('users').controller('SettingsController', ['$scope', '$modal', '$
 			});
 		};
 
-		$scope.findProfile = function() {
-		  if(!$stateParams.username) {
-        $scope.profile = $scope.user;
-		  }
-		  else {
-        $scope.profile = Users.get({
-          username: $stateParams.username
-        });
-		  }
-		};
-
-		/**
-		* Open avatar -modal
-		*/
-		$scope.avatarModal = function (user, $event) {
-
-			if($event) $event.preventDefault();
-
-			var modalInstance = $modal.open({
-				templateUrl: 'avatar.client.modal.html', //inline at template
-				controller: function ($scope, $modalInstance) {
-					$scope.user = user;
-					$scope.close = function () {
-						$modalInstance.dismiss('close');
-					};
-				}
-			});
-
-		};
 
 
 		// Remove user permanently
