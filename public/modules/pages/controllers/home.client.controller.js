a3e296b55823ca58c4199648373f2501ca1e7fb8
Mikael Korpela <mikael@ihminen.org>
2014-10-29 05:41:36 +0200

    Work on .board class & home page
    - Separate .board into own .less file
    - Fix .board work better on narrow screens
    - Remove carousel from home and use .board instead
    - Remove unused images
    - Clean out front page html/js/less code

--- public/modules/pages/controllers/home.client.controller.js
+++ public/modules/pages/controllers/home.client.controller.js
@@ -1,15 +1,13 @@
 'use strict';
 
 
-angular.module('pages').controller('HomeController', ['$scope', '$state', '$log', 'Authentication',
-  function($scope, $state, $log, Authentication) {
-    // This provides Authentication context.
-    $scope.authentication = Authentication;
+angular.module('pages').controller('HomeController', ['$scope', '$state', 'Authentication',
+  function($scope, $state, Authentication) {
 
     // Redirect logged-in users out from front page
-        if( $scope.authentication.user ) {
-            $log.log('Auth user hit the home - redirect to search instead...');
-            $state.go('search');
+    if( Authentication.user ) {
+      $state.go('search');
     }
+
   }
 ]);
