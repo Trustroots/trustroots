f266a567f45fb400dac190237af14eab07e0c9db
Mikael Korpela <mikael@ihminen.org>
2014-10-30 11:41:34 +0200

    New about page, beta notification for homepage

--- public/modules/pages/controllers/about.client.controller.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('pages').controller('AboutController', ['$scope',
-  function($scope) {
-    // Controller Logic
-    // ...
-  }
-]);
--- public/modules/pages/controllers/home.client.controller.js
+++ public/modules/pages/controllers/home.client.controller.js
@@ -1,12 +1,17 @@
 'use strict';
 
 
-angular.module('pages').controller('HomeController', ['$scope', '$state', 'Authentication',
-  function($scope, $state, Authentication) {
+angular.module('pages').controller('HomeController', ['$scope', '$state', '$timeout', 'Authentication',
+  function($scope, $state, $timeout, Authentication) {
 
     // Redirect logged-in users out from front page
     if( Authentication.user ) {
       $state.go('search');
+    }
+    else {
+      $timeout(function(){
+        alert('Hey! This is a demo and still work in progress. \n\nYou can click around but things are likely to break, change or data might disappear before we publish this widely. \n\nThanks & enjoy!');
+      }, 1500);
     }
 
   }
