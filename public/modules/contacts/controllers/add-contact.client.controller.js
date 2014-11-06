e88f30553170df5a246b583364e23e47e6d9a6c0
Mikael Korpela <mikael@ihminen.org>
2014-11-06 11:33:21 +0200

    #6 Success notification

--- public/modules/contacts/controllers/add-contact.client.controller.js
+++ public/modules/contacts/controllers/add-contact.client.controller.js
@@ -9,10 +9,9 @@ angular.module('contacts').controller('AddContactController', ['$scope', '$rootS
     // If no friend ID defined, go to elsewhere
     if (!$stateParams.userId) $state.go('profile');
 
-
     $scope.user = Authentication.user;
     $scope.isConnected = false;
-    $scope.isLoading = true;
+    $scope.isLoading = false;
     $scope.contact = {
       message: '<p>Hi!</p><p>I would like to add you as a contact.</p><p>— ' + Authentication.user.displayName + '</p>'
     };
@@ -25,9 +24,6 @@ angular.module('contacts').controller('AddContactController', ['$scope', '$rootS
     $scope.friend = UsersMini.get({
         userId: $stateParams.userId
     });
-    $scope.findContact = function() {
-      $scope.isLoading = false;
-    };
 
     $scope.addContact = function() {
       $scope.isLoading = true;
@@ -38,10 +34,9 @@ angular.module('contacts').controller('AddContactController', ['$scope', '$rootS
       });
 
       contact.$save(function(response) {
-        // Done!
         $scope.isLoading = false;
-        //@ todo: direct to contacts tab at profile
-        $state.go('profile');
+        $scope.isConnected = true;
+        $scope.success = 'Done! We sent an email to your contact and he/she still needs to confirm it.';
       }, function(errorResponse) {
         $scope.isLoading = false;
         $scope.error = errorResponse.data.message;
