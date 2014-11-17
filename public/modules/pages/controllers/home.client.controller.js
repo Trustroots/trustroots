0986d4f97c125b0d6ded865dee6d60971ae2a7b9
Mikael Korpela <mikael@ihminen.org>
2014-11-18 01:10:30 +0200

    Less annoying demo alert

--- public/modules/pages/controllers/home.client.controller.js
+++ public/modules/pages/controllers/home.client.controller.js
@@ -1,18 +1,12 @@
 'use strict';
 
 
-angular.module('pages').controller('HomeController', ['$scope', '$rootScope', '$state', '$timeout', 'Authentication',
-  function($scope, $rootScope, $state, $timeout, Authentication) {
+angular.module('pages').controller('HomeController', ['$scope', '$state', '$timeout', 'Authentication',
+  function($scope, $state, $timeout, Authentication) {
 
     // Redirect logged-in users out from front page
     if( Authentication.user ) {
       $state.go('search');
-    }
-    else if(!$rootScope.demoNotificationDone) {
-      $rootScope.demoNotificationDone = true;
-      $timeout(function(){
-        alert('Hey! This is a demo and still work in progress. \n\nYou can click around but things are likely to break, change or data might disappear before we publish this widely. \n\nThanks & enjoy!');
-      }, 1000);
     }
 
   }
