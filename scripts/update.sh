fbe4911d4bbf0686b60ff4186ab806e586ba0cd3
Mikael Korpela <mikael@ihminen.org>
2014-11-18 09:01:01 -0500

    Add git pull to update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,6 +7,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 APP_DIR=$SCRIPT_DIR/..
 cd "$APP_DIR"
 
+echo "Fetch latest master..."
+git pull origin master
+
 echo "Updating assets..."
 npm update
 bower update
