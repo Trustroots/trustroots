bbd952be3e8301fb3e5b6afaa0c61ffd93c85d38
Mikael Korpela <mikael@ihminen.org>
2014-12-22 07:17:15 -0500

    Add S3 sync script, db backup will erase week old backups

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -1,15 +1,53 @@
 #!/bin/bash
-DUMPPATH=/srv/backups/db-dumps # dumps directory we just created
-MONGODBNAME=trust-roots # your MongoDB database name
-DAY=`/bin/date +%Y%m%d` # today's datetime
+MONGODBNAME="trust-roots" # MongoDB to backup
+
+DUMPPATH="/srv/backups/db-dumps"
+LOGSPATH="/srv/logs/db-backups"
+#TIMESTAMP=`/bin/date +%Y%m%d`
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+TIMESTART=$(date +%s)
+LOGFILE=$LOGSPATH/$MONGODBNAME_$TIMESTAMP.log
+
+# Make sure these directories exist
+if [ ! -d "$DUMPPATH" ]; then
+  echo ""
+  echo "Creating $DUMPPATH directory..."
+  mkdir -p $DUMPPATH
+fi
+if [ ! -d "$LOGSPATH" ]; then
+  echo ""
+  echo "Creating $LOGSPATH directory..."
+  mkdir -p $LOGSPATH
+fi
+
+echo "Backup:: Script Start -- $(date +%Y%m%d_%H%M)" >> $LOGFILE
 
 # Just to ensure this scripts works in any env
 export LC_ALL=C
 
-# Backup
-mongodump --db $MONGODBNAME --out $DUMPPATH/mongo_$DAY # run the command we those variables set
-cd $DUMPPATH/mongo_$DAY # navigate to the folder where the dump was saved
-tar -cvzf "$DUMPPATH/mongo_$DAY.tar" $MONGODBNAME # create an archive out of that dump
+# Delete older backups
+echo "Delete old backups..." >> $LOGFILE
+find $DUMPPATH -name "*.tar.gz" -type f -mtime +7 -print -delete >> $LOGFILE
 
-cd $DUMPPATH
-rm -rf $DUMPPATH/mongo_$DAY # remove the dump because we only keep the archive version
+# Delete older logfiles
+echo "Delete old logs..." >> $LOGFILE
+find $LOGSPATH -name "*.log" -type f -mtime +7 -print -delete >> $LOGFILE
+
+DUMPFILE=$MONGODBNAME-$TIMESTAMP
+
+echo "Exporting MongoDB to $DUMPFILE..." >> $LOGFILE
+# Backup MongoDB
+mongodump --db "$MONGODBNAME" --out "$DUMPPATH/$DUMPFILE" >> $LOGFILE
+
+echo "Store dump folder to $DUMPFILE.tar.gz..." >> $LOGFILE
+tar -zcvf "$DUMPPATH/$DUMPFILE.tar.gz" "$DUMPPATH/$DUMPFILE" >> $LOGFILE # create an archive out of that dump
+
+#cd $DUMPPATH
+rm -rf "$DUMPPATH/$DUMPFILE" >> $LOGFILE # remove the dump because we only keep the archive version
+
+
+TIMEEND=$(date +%s)
+TIMEELAPSED=$(expr $TIMEEND - $TIMESTART)
+
+echo "Backup :: Script End -- $(date +%Y%m%d_%H%M)" >> $LOGFILE
+echo "Elapsed Time ::  $(date -d 00:00:$TIMEELAPSED +%Hh:%Mm:%Ss) " >> $LOGFILE
