b89ae26a875b9f20c1f4c1f84f887456fd584e12
Mikael Korpela <mikael@ihminen.org>
2014-12-24 18:42:19 +0200

    #83 Decode broken hash-bang url

--- public/application.js
+++ public/application.js
@@ -15,6 +15,13 @@ angular.element(document).ready(function() {
   //Fixing facebook bug with redirect
   if (window.location.hash === '#_=_') window.location.hash = '#!';
 
+  //Fixing error where email clients would turn #! into #%21
+  var location = document.location,
+  decodedHref = decodeURI(location.href);
+  if(location.href !== decodedHref){
+    location.href = decodedHref;
+  }
+
   //Then init the app
   angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
 });
