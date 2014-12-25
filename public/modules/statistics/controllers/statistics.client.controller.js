196750e8798b4eb192baed0b1b8391c893e1ed73
Mikael Korpela <mikael@ihminen.org>
2014-12-25 02:21:03 +0200

    #80 fix wrong date at statistics, second try

--- public/modules/statistics/controllers/statistics.client.controller.js
+++ public/modules/statistics/controllers/statistics.client.controller.js
@@ -3,8 +3,8 @@
 angular.module('statistics').controller('StatisticsController', ['$scope', 'Statistics',
   function($scope, Statistics) {
 
-    // Yay!
-    $scope.launchDate = new Date(2014, 12, 23);
+    // Dec 23, 2014
+    $scope.launchDate = new Date(2014, 11, 23);
 
     $scope.statistics = Statistics.get();
 
