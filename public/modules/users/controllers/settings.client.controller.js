7906e8147f06d180884551c0ad7f4e07574709f1
Mikael Korpela <mikael@ihminen.org>
2015-01-03 01:35:42 +0200

    #81 Require current password to change it + text versions + fixing, it was buggy anyway
    - Turning that terrible if-else pyramid into a nice and swift Waterfall!
    - Text versions for password reset mails

--- public/modules/users/controllers/settings.client.controller.js
+++ public/modules/users/controllers/settings.client.controller.js
@@ -1,14 +1,19 @@
 'use strict';
 
-angular.module('users').controller('SettingsController', ['$scope', '$http', '$state', 'Users', 'Authentication',
-  function($scope, $http, $state, Users, Authentication) {
+/* This declares to JSHint that these are global variables: */
+/*global flashTimeout:false */
+
+angular.module('users').controller('SettingsController', ['$scope', '$http', '$state', 'Users', 'Authentication', 'messageCenterService',
+  function($scope, $http, $state, Users, Authentication, messageCenterService) {
 
     // If user is not signed in then redirect to login
     if (!Authentication.user) $state.go('signin');
 
     $scope.user = Authentication.user;
 
-    // Change user email
+    /**
+     * Change user email
+     */
     $scope.updateUserEmail = function() {
       $scope.emailSuccess = $scope.emailError = null;
       var user = new Users($scope.user);
@@ -21,7 +26,9 @@ angular.module('users').controller('SettingsController', ['$scope', '$http', '$s
       });
     };
 
-    // Resend confirmation email for already sent email
+    /**
+     * Resend confirmation email for already sent email
+     */
     $scope.resendUserEmailConfirm = function() {
       if($scope.user.emailTemporary) {
         $scope.user.email = $scope.user.emailTemporary;
@@ -29,34 +36,54 @@ angular.module('users').controller('SettingsController', ['$scope', '$http', '$s
       }
     };
 
-
-    // Change user email subscriptions
+    /**
+     * Change user email subscriptions
+     */
     $scope.updateUserSubscriptions = function() {
-      $scope.subscriptionsSuccess = $scope.subscriptionsError = null;
       var user = new Users($scope.user);
-
       user.$update(function(response) {
-        $scope.subscriptionsSuccess = 'Subscriptions updated.';
+        messageCenterService.add('success', 'Subscriptions updated.', { timeout: flashTimeout });
         Authentication.user = response;
       }, function(response) {
-        $scope.subscriptionsError = response.data.message;
+        messageCenterService.add('error', 'Error: ' + response.data.message, { timeout: flashTimeout });
       });
     };
 
-    // Change user password
+    /**
+     * Change user password
+     */
+    $scope.changeUserPasswordLoading = false;
+    $scope.currentPassword = '';
+    $scope.newPassword = '';
+    $scope.verifyPassword = '';
     $scope.changeUserPassword = function() {
-      $scope.passwordSuccess = $scope.passwordError = null;
 
-      $http.post('/users/password', $scope.passwordDetails).success(function(response) {
-        // If successful show success message and clear form
-        $scope.passwordSuccess = true;
-        $scope.passwordDetails = null;
+      $scope.changeUserPasswordLoading = true;
+
+      $http.post('/users/password', {
+        currentPassword: $scope.currentPassword,
+        newPassword: $scope.newPassword,
+        verifyPassword: $scope.verifyPassword
+      }).success(function(response) {
+        $scope.currentPassword = '';
+        $scope.newPassword = '';
+        $scope.verifyPassword = '';
+        $scope.changeUserPasswordLoading = false;
+
+        $scope.user = Authentication.user = response.user;
+
+        messageCenterService.add('success', response.message, { timeout: flashTimeout });
+
       }).error(function(response) {
-        $scope.passwordError = response.message;
+        $scope.changeUserPasswordLoading = false;
+        messageCenterService.add('danger', ((response.message && response.message !== '') ? response.message : 'Password not changed due error, try again.'), { timeout: 10000 });
       });
+
     };
 
-    // Remove user permanently
+    /*
+     * Remove user permanently
+     */
     $scope.removalConfirm = false;
     $scope.removeUser = function() {
       $scope.success = $scope.error = null;
