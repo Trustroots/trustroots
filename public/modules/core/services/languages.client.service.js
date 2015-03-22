c6fdd531990358addd08a76911518f6df5c7ee68
Mikael Korpela <mikael@ihminen.org>
2015-03-21 23:56:09 -0700

    #10 #206 Tiny optimizations in language factory

--- public/modules/core/services/languages.client.service.js
+++ public/modules/core/services/languages.client.service.js
@@ -1,18 +1,18 @@
 'use strict';
 
-angular.module('core').factory('Languages', [
-  function() {
+angular.module('core').factory('Languages', ['$window',
+  function($window) {
     var service = {};
 
     service.get = function (type) {
       if (type === 'array'){
         var langs_arr = [];
-        for (var code in window.languages) {
-          langs_arr.push({key: code, name: window.languages[code]});
+        for (var code in $window.languages) {
+          langs_arr[langs_arr.length] = {key: code, name: $window.languages[code]};
         }
         return langs_arr;
       } else if (type === 'object'){
-        return window.languages;
+        return $window.languages;
       }
     };
 
