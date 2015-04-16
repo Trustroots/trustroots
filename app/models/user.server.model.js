2106e34fb1a52f15ed01183e4344b05356b85629
Mikael Korpela <mikael@ihminen.org>
2015-04-16 22:27:25 +0300

    Remove obsolete UserSchema.statics.findUniqueUsername()
    This became obsolete with
    https://github.com/Trustroots/trustroots/commit/4806afc002e6a1273535ad84
    ec139c3d67b1f2d6

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -228,26 +228,4 @@ UserSchema.methods.authenticate = function(password) {
   return this.password === this.hashPassword(password);
 };
 
-/**
- * Find possible not used username
- */
-UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
-  var _this = this;
-  var possibleUsername = username + (suffix || '');
-
-  _this.findOne({
-    username: possibleUsername
-  }, function(err, user) {
-    if (!err) {
-      if (!user) {
-        callback(possibleUsername);
-      } else {
-        return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
-      }
-    } else {
-      callback(null);
-    }
-  });
-};
-
 mongoose.model('User', UserSchema);
