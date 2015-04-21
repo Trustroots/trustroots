0e93f13b461af66bc3b20413be02001faeead590
Mikael Korpela <mikael@ihminen.org>
2015-04-21 19:57:25 +0300

    Update migration tasks

--- migrations/1429124257492-add-user-displayUsername.js
+++ migrations/1429124257492-add-user-displayUsername.js
@@ -7,29 +7,63 @@
 var init = require('../config/init')(),
     config = require('../config/config'),
     mongoose = require('mongoose'),
-    models = require('../app/models/user'),
+    chalk = require('chalk'),
+    userModel = require('../app/models/user'),
     User = mongoose.model('User');
 
 exports.up = function(next) {
-    User.find({ displayUsername: {$exists: false} }).forEach(
-        function (elem) {
-            User.update(
-                {},
-                { $set: { displayUsername: elem.username } },
-                { multi: true }
-            );
-        }
-    );
-    next();
+  var db = mongoose.connect(config.db.uri, config.db.options, function(err) {
+    if (err) {
+      console.log(chalk.red(err));
+      mongoose.disconnect();
+      return;
+    }
+    try {
+      // Works fine for small db, but for bigger use snapshot()
+      // http://docs.mongodb.org/manual/reference/method/cursor.snapshot/
+      User
+        .find({ displayUsername: {$exists: false} }, function(err, users){
+        users.forEach(function (e) {
+
+          User.findByIdAndUpdate(
+            e._id,
+            { $set: { displayUsername: e.username } }
+          ).exec();
+        });
+        mongoose.disconnect();
+        next();
+        });
+    }
+    catch(err) {
+      console.log(chalk.red(err));
+      mongoose.disconnect();
+      next();
+    }
+  });
 };
 
 exports.down = function(next) {
-      User.update(
-          { displayUsername: { $exists: true } },
-          { '$unset': { displayUsername: "" } },
-          { multi: true }
-      );
-    next();
+  mongoose.connect(config.db.uri, config.db.options, function(err) {
+    if (err) {
+      console.log(chalk.red(err));
+      mongoose.disconnect();
+      return;
+    }
+    User.update(
+      { displayUsername: { $exists: true } },
+      { '$unset': { displayUsername: "" } },
+      { multi: true },
+      function (err, numberAffected, raw) {
+        if (err) {
+          console.log(chalk.red(err));
+          mongoose.disconnect();
+          return;
+        }
+        console.log('Affected rows:');
+        console.log(numberAffected);
+        console.log('');
+        mongoose.disconnect();
+        next();
+      });
+  });
 };
-
-
