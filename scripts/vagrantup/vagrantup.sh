daf167b91150522acbb1489129de96212cad5410
Mikael Korpela <mikael@ihminen.org>
2015-05-31 23:02:46 +0300

    Update Vagrant setup for Passenger 5.x

--- scripts/vagrantup/vagrantup.sh
+++ scripts/vagrantup/vagrantup.sh
@@ -64,7 +64,7 @@ echo "---------------------------------------------------------------------"
 echo ""
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
 sudo apt-get install apt-transport-https ca-certificates
-echo 'echo "deb https://oss-binaries.phusionpassenger.com/apt/passenger/4 trusty main" >> /etc/apt/sources.list.d/passenger.list' | sudo -s
+echo 'echo "deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main" >> /etc/apt/sources.list.d/passenger.list' | sudo -s
 sudo chown root: /etc/apt/sources.list.d/passenger.list
 sudo chmod 600 /etc/apt/sources.list.d/passenger.list
 sudo apt-get update -qq
