f5acd01637e7f6276e30f41fc177e802d0b142de
Mikael Korpela <mikael@ihminen.org>
2015-08-07 20:46:13 +0200

    Add spinner animation
    Finally a proper loading animation…

---
+++ modules/core/client/directives/tr-spinner.client.directive.js
@@ -0,0 +1,50 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('core')
+    .directive('trSpinner', trSpinnerDirective);
+
+  function trSpinnerDirective() {
+    return {
+      restrict: 'E',
+      link: function(scope, element, attrs) {
+
+        function generateSVG(size, square, stroke) {
+          return '<svg class="spinner spinner-' + size + '" width="' + square + 'px" height="' + square + 'px" viewBox="0 0 ' + (square + 1) + ' ' + (square + 1) + '" xmlns="http://www.w3.org/2000/svg">' +
+                   '<circle class="spinner-path" fill="none" stroke-width="' + stroke + '" stroke-linecap="round" cx="' + ((square + 1)/2) + '" cy="' + ((square + 1)/2) + '" r="' + (((square + 1)/2) - stroke) + '"></circle>' +
+                 '</svg>';
+        }
+
+        function renderSVG() {
+          var size = attrs.size || 'md',
+              square,
+              stroke;
+
+          if(size === 'lg') {
+            square = 85;
+            stroke = 4;
+          }
+          else if(size === 'md') {
+            square = 65;
+            stroke = 3;
+          }
+          else if(size === 'sm') {
+            square = 35;
+            stroke = 2;
+          }
+          else if(size === 'xs') {
+            square = 25;
+            stroke = 1;
+          }
+
+          element.html( generateSVG(size, square, stroke) );
+        }
+
+        renderSVG();
+
+      }
+    };
+  }
+
+})();
