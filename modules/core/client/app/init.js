995359f0b1b281370b71539183d46729bd1d08a7
Mikael Korpela <mikael@ihminen.org>
2015-09-01 22:42:28 +0300

    #284 Refactor appConfig to Angular best practises

--- modules/core/client/app/init.js
+++ modules/core/client/app/init.js
@@ -1,34 +1,43 @@
-'use strict';
+(function() {
+ 'use strict';
 
-//Start by defining the main module and adding the module dependencies
-angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
+  // Start by defining the main module and adding the module dependencies
+  angular
+    .module(ApplicationConfiguration.applicationModuleName,
+            ApplicationConfiguration.applicationModuleVendorDependencies);
 
-// Setting HTML5 Location Mode
-angular.module(ApplicationConfiguration.applicationModuleName).config(['$locationProvider',
-  function($locationProvider) {
+  // App configs
+  angular
+    .module(ApplicationConfiguration.applicationModuleName)
+    .config(initConfig);
+
+  /* @ngInject */
+  function initConfig(localStorageServiceProvider, $locationProvider) {
+
+    // Setting HTML5 Location Mode
     $locationProvider.html5Mode({
       enabled: true,
-      requireBase: false // We do have <base> tag defined, but requiring it for Karma tests breaks tests... @todo
+      // We do have <base> tag defined, but requiring it for Karma tests breaks tests...
+      // @todo better solution?
+      requireBase: false
     }).hashPrefix('!');
-  }
-]);
 
-// Configure local storage module
-// Not using localStorage here, but sessionStorage instead. It'll get wiped out when closing the browser.
-// @link https://github.com/grevory/angular-local-storage#configuration
-angular.module(ApplicationConfiguration.applicationModuleName).config(['localStorageServiceProvider',
-  function(localStorageServiceProvider) {
+    // Configure local storage module
+    // Not using localStorage here, but sessionStorage instead. It'll get wiped out when closing the browser.
+    // @link https://github.com/grevory/angular-local-storage#configuration
     localStorageServiceProvider
       .setPrefix(ApplicationConfiguration.applicationModuleName)
       .setStorageType('sessionStorage');
+
   }
-]);
 
-//Then define the init function for starting up the application
-angular.element(document).ready(function() {
-  //Fixing facebook bug with redirect
-  if (window.location.hash === '#_=_') window.location.hash = '#!';
+  // Then define the init function for starting up the application
+  angular.element(document).ready(function() {
+    // Fixing facebook bug with redirect
+    if (window.location.hash === '#_=_') window.location.hash = '#!';
 
-  //Then init the app
-  angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
-});
+    // Then init the app
+    angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
+  });
+
+})();
