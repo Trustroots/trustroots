170dd23aae7531b1f3648205610a51800b45704e
Mikael Korpela <mikael@ihminen.org>
2015-09-01 17:20:24 +0300

    Update deployment script
    - Pulls from currently active branch instead of production
    - Add npm prune
    - Include fontello Grunt task
    - Compatible with Phusion Passenger 5.x

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,25 +5,29 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 APP_DIR=$SCRIPT_DIR/..
 cd "$APP_DIR"
 
-echo "Pull latest from the production branch..."
-git pull origin production
+echo "Pull latest from the currently active branch..."
+git pull
 
+echo ""
 echo "Updating packages & assets..."
 npm update --production
-bower update
+npm prune --production
+bower update --allow-root --config.interactive=false
+grunt fontello
 NODE_ENV=production grunt build
 
 echo ""
-echo "Migrating database..."
+echo "Migrating the database..."
 export NODE_ENV=production
 migrate up
 
-echo "Redeploying the application..."
-# https://www.phusionpassenger.com/documentation/Users%20guide%20Nginx.html#_redeploying_restarting_the_rack_application
-touch tmp/restart.txt
+echo "Redeploying the application... (requires sudo password)"
+# https://www.phusionpassenger.com/library/admin/nginx/restart_app.html#passenger-config-restart-app
+sudo passenger-config restart-app /
 
 # Deployment notifications to New Relic
 # http://newrelic.com/
+echo ""
 NEW_RELIC_CONF="./config/secret/newrelic.conf"
 if [ ! -f "$NEW_RELIC_CONF" ]; then
   echo "Add New Relic API key to $NEW_RELIC_CONF and this script will notify New Relic each time you deploy."
@@ -33,5 +37,6 @@ else
   curl -H "x-api-key:$NEW_RELIC_API_KEY" -d "deployment[app_name]=Trustroots" -d "deployment[description]=Deployed via deploy.sh" https://api.newrelic.com/deployments.xml > /dev/null 2>&1
 fi
 
-echo "Show Passenger status (requires sudo)"
+echo ""
+echo "Passenger status (requires sudo password)"
 sudo passenger-status
