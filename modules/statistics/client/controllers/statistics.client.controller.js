e1b3b11b77e4f5da0d385909f0d7df3e3d7e1b58
Mikael Korpela <mikael@ihminen.org>
2015-09-15 14:39:28 +0300

    Show more stats

--- modules/statistics/client/controllers/statistics.client.controller.js
+++ modules/statistics/client/controllers/statistics.client.controller.js
@@ -11,15 +11,20 @@
     // ViewModel
     var vm = this;
 
-    vm.launchDate = new Date(2014, 11, 23); // Dec 23, 2014
     vm.data = data;
+    vm.launchDate = new Date(2014, 11, 23); // Dec 23, 2014
+
+    // Process data at init
+    data.$promise.then(function() {
+      processStats(data);
+    });
 
     // Update page every now and then while it's open
     var statsInterval = $interval(function() {
       Statistics.get({}, function(data) {
-        vm.data = data;
+        processStats(data);
       });
-    }, (5 * 60000)); // every 5mins
+    }, (10 * 60000)); // every 10 mins
 
     // Clean interval when leaving the page
     $scope.$on('$destroy', function() {
@@ -27,6 +32,27 @@
         $interval.cancel(statsInterval);
       }
     });
+
+    function processStats(data) {
+
+        angular.extend(vm, data);
+
+        vm.hostingTotal = data.hosting.yes + data.hosting.maybe;
+        vm.hostingPercentage = (vm.hostingTotal/data.total)*100;
+        vm.hostingYesPercentage = (data.hosting.yes/vm.hostingTotal)*100;
+        vm.hostingMaybePercentage = (data.hosting.maybe/vm.hostingTotal)*100;
+
+        vm.newsletterPercentage = (data.newsletter/data.total)*100;
+
+        vm.connections = [];
+        angular.forEach(data.connected, function(count, network) {
+          vm.connections.push({
+            network: network,
+            count: count
+          });
+        });
+
+    }
 
   }
 })();
--- modules/statistics/server/controllers/statistics.server.controller.js
+++ modules/statistics/server/controllers/statistics.server.controller.js
@@ -16,7 +16,11 @@ var path = require('path'),
  */
 exports.get = function(req, res) {
 
-  req.statistics = {};
+  req.statistics = {
+    connected: {},
+    hosting: {},
+    avatars: {}
+  };
 
   async.waterfall([
 
@@ -30,16 +34,94 @@ exports.get = function(req, res) {
         });
     },
 
-    // Hosting stats
+    // External sites - BeWelcome
     function(done) {
-      Offer.count({
-        $or: [
-          { status: 'yes' },
-          { status: 'maybe' }
-        ]
+
+      User.count({
+        extSitesBW: { $exists: true, $ne: '' }
+      }, function(err, count) {
+        req.statistics.connected.bewelcome = (count ? count : 0);
+        done(err);
+      });
+    },
+
+    // External sites - Couchsurfing
+    function(done) {
+      User.count({
+        extSitesCS: { $exists: true, $ne: '' }
+      }, function(err, count) {
+        req.statistics.connected.couchsurfing = (count ? count : 0);
+        done(err);
+      });
+    },
+
+    // External sites - Warmshowers
+    function(done) {
+      User.count({
+        extSitesWS: { $exists: true, $ne: '' }
+      }, function(err, count) {
+        req.statistics.connected.warmshowers = (count ? count : 0);
+        done(err);
+      });
+    },
+
+    // External sites - Facebook
+    function(done) {
+      User.count({
+        'additionalProvidersData.facebook': { $exists: true }
+      }, function(err, count) {
+        req.statistics.connected.facebook = (count ? count : 0);
+        done(err);
+      });
+    },
+
+    // External sites - Twitter
+    function(done) {
+      User.count({
+        'additionalProvidersData.twitter': { $exists: true }
+      }, function(err, count) {
+        req.statistics.connected.twitter = (count ? count : 0);
+        done(err);
+      });
+    },
+
+    // External sites - GitHub
+    function(done) {
+      User.count({
+        'additionalProvidersData.github': { $exists: true }
+      }, function(err, count) {
+        req.statistics.connected.github = (count ? count : 0);
+        done(err);
+      });
+    },
+
+    // Newsletter subscribers
+    function(done) {
+      User.count({
+        newsletter: true
       },
       function(err, count) {
-        req.statistics.hosting = count;
+        req.statistics.newsletter = (count ? count : 0);
+        done(err);
+      });
+    },
+
+    // Hosting stats
+    function(done) {
+      Offer.aggregate({
+        $group: {
+            _id : '$status',
+            count: { $sum: 1 }
+        }
+      },
+      function(err, counters) {
+        if(counters && counters.length > 0) {
+          counters.forEach(function(counter) {
+            if(['yes', 'maybe'].indexOf(counter._id) !== -1) {
+              req.statistics.hosting[counter._id] = counter.count;
+            }
+          });
+        }
         done(err);
       });
     },
@@ -48,7 +130,7 @@ exports.get = function(req, res) {
     // @link https://www.npmjs.com/package/git-rev
     function(done) {
       git.long(function (hash) {
-        req.statistics.commit = hash;
+        req.statistics.commit = (hash ? hash : '');
         done(null);
       });
     },
