5deb33be3cfa2f35d0dc04743c19d4af8f483b86
Mikael Korpela <mikael@ihminen.org>
2016-03-02 10:41:30 +0200

    Implement FastClick
    …as Angular 1.5 deprecated support for removing 300ms touch click delay.
    
    -
    https://github.com/angular/angular.js/commit/0dfc1dfebf26af7f951f301c4e3
    848ac46f05d7f
    - https://docs.angularjs.org/guide/migration#migrating-from-1-4-to-1-5
    - http://developer.telerik.com/featured/300-ms-click-delay-ios-8/

--- modules/core/client/app/init.js
+++ modules/core/client/app/init.js
@@ -50,6 +50,11 @@
     // Fixing facebook bug with redirect
     if (window.location.hash === '#_=_') window.location.hash = '';
 
+    // FastClick - Polyfill to remove click delays on browsers with touch UIs
+    // Attach polyfill to `<body>`
+    // @link https://github.com/ftlabs/fastclick
+    FastClick.attach(document.body);
+
     // Then init the app
     angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
   });
