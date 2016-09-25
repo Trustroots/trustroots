d8dcf2d4b9f6e8bce1fb3ec257236820acda2816
mrkvon <michal.salajka@email.cz>
2016-09-25 23:54:26 +0200

    ES6 -> ES5 (almost)
    mongoose.Promise = Promise still there to get rid of a warning.

--- scripts/fill-messages-to-influx.js
+++ scripts/fill-messages-to-influx.js
@@ -1,7 +1,7 @@
 'use strict';
 var path = require('path'),
     chalk = require('chalk'),
-    co = require('co'),
+    async = require('async'),
     config = require(path.resolve('./config/config')),
     configMongoose = require(path.resolve('./config/lib/mongoose')),
     mongoose = require('mongoose'),
@@ -9,7 +9,10 @@ var path = require('path'),
     messageModels = require(path.resolve('./modules/messages/server/models/message.server.model')),
     Message = mongoose.model('Message');
 
-mongoose.Promise = Promise;
+    // the expression below uses ES6 Promises, so it shouldn't belong here.
+    // there is a warning when one doesn't provide one's own library.
+    // it was present elsewhere i believe
+    mongoose.Promise = Promise;
 
 // Bootstrap db connection
 var db = mongoose.connect(config.db.uri, function(err) {
@@ -19,36 +22,45 @@ var db = mongoose.connect(config.db.uri, function(err) {
   }
 });
 
-// settings how often the progress will be printed to console
-// every PROGRESS_INTERVAL %
-const PROGRESS_INTERVAL = 1;
+async.waterfall([
 
-co(function * () {
-  let messages = yield Message.find();
+  function readAllMessages(done) {
+    Message.find(done);
+  },
 
-  console.log(`found ${messages.length} messages\n`);
-  console.log(`processing and adding the messages to influx now\n`);
+  function processTheMessages(messages, done) {
+    console.log('found ' + messages.length + ' messages\n');
+    console.log('processing and adding the messages to influx now\n');
 
-  let msgLen = messages.length;
-  let progress = 0;  // a progress counter
-  for (let msg of messages) {
-    yield fluxIn(msg);
+    // settings how often the progress will be printed to console
+    // every PROGRESS_INTERVAL %
+    const PROGRESS_INTERVAL = 1;
+    var msgLen = messages.length;
+    var progress = 0;  // a progress counter
 
-    //showing a progress
-    if(progress % Math.ceil(msgLen / 100 * PROGRESS_INTERVAL) === 0) {
-      console.log(`${ Math.floor(progress / msgLen * 100) }%`);
+    async.eachSeries(messages, saveMessageAndRunCounter, done);
+
+    function saveMessageAndRunCounter(message, callback) {
+      // showing a progress
+      if (progress % Math.ceil(msgLen / 100 * PROGRESS_INTERVAL) === 0) {
+        console.log(Math.floor(progress / msgLen * 100) + '%');
+      }
+      ++progress;
+
+      // processing and saving the point to database
+      fluxIn.save(message, callback);
     }
-    ++progress;
-  }
 
+  }
+], function (err) {
+  mongoose.disconnect();
+  if (err) {
+    console.log(err);
+    console.log('******************* errored **************************');
+    return;
+  }
   console.log('************** finished successfully *****************');
   console.log('******* you can play with the data in grafana ********');
   console.log('******************* or not ^_^ ***********************');
   console.log('********************* bye! ***************************');
-
-  mongoose.disconnect();
-}).catch(function (e) {
-  console.error(e);
-  console.log('******************* errored **************************');
-  mongoose.disconnect();
 });
