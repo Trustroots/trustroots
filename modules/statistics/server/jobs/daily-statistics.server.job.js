86423c74b44335910030dbd45b3979dee77e2ded
mrkvon <michal.salajka@email.cz>
2016-10-03 20:33:13 +0200

    removing influxdb enabled check
    fixing params of influxService.writePoint
    removed config
    and cosmetical changes

--- modules/statistics/server/jobs/daily-statistics.server.job.js
+++ modules/statistics/server/jobs/daily-statistics.server.job.js
@@ -8,13 +8,12 @@
  * Module dependencies.
  */
 var path = require('path'),
-    config = require(path.resolve('./config/config')),
     influxService = require(path.resolve('./modules/core/server/services/influx.server.service')),
     statistics = require(path.resolve('./modules/statistics/server/controllers/statistics.server.controller'));
 
-module.exports = function(job, agendaDone) {
+module.exports = function (job, agendaDone) {
 
-  statistics.getUsersCount(function(err, count) {
+  statistics.getUsersCount(function (err, count) {
 
     if (err) {
       console.error('Daily statistics: failed fetching user count.');
@@ -23,20 +22,14 @@ module.exports = function(job, agendaDone) {
     }
 
     // Save to influx here
-    influxService.writePoint('members', count, { members: 'members' }, function(err) { // removed result from callback function because of ESLint complains
-      if (err) {
-        // InfluxDB was not enabled, don't scream out error
-        if (config.influxdb || !config.influxdb.enabled) {
-          console.log('Skipped storing daily statistics: no InfluxDB configured.');
-          // Let `agendaDone()` succeed
-          err = null;
-        } else {
+    influxService.writePoint('members', { count: count }, { members: 'members' },
+      function (err, result) {
+        if (err) {
           console.error('Daily statistics: failed writing to InfluxDB.');
           console.error(err);
         }
-      }
-      agendaDone(err);
-    });
+        result; // satisfy ESLint unused variables
+        agendaDone(err);
+      });
   });
-
 };
