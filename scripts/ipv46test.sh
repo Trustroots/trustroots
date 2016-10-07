b989775e836ccb6557dcf49a10e42271f7bc3054
Mikael Korpela <mikael@ihminen.org>
2016-10-07 17:11:53 +0300

    Clean up ipv4/6 test script

--- scripts/ipv46test.sh
+++ scripts/ipv46test.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-# Modify these variables to your needs
-HOSTNAME=trustroots.org
-TEST_STRING="<!DOCTYPE html>"
+#
+# Test if you can reast host via ipv6 and ipv4.
+# Run:
+# ./ipv46test.sh
+#
+# Defaults to "trustroots.org" and test string "<!DOCTYPE html>"
+#
+# Or to set hostname, run:
+# ./ipv46test.sh example.org
+#
+# Or to set hostname and test string to find, run:
+# ./ipv46test.sh example.org "test string"
+#
 
+# Options
+DEFAULT_HOSTNAME="trustroots.org"
+DEFAULT_TEST_STRING="<!DOCTYPE html>"
 
+# Helpers
+HOSTNAME=${1:-$DEFAULT_HOSTNAME}
+TEST_STRING=${2:-$DEFAULT_TEST_STRING}
 IPV4_IP=$(dig ${HOSTNAME} A +short | tail -n1)
 IPV6_IP=$(dig ${HOSTNAME} AAAA +short | tail -n1)
 RED='\033[0;31m'
@@ -20,48 +36,70 @@ print_success() {
 }
 
 
-printf "\nTesting access to ${BOLD}$HOSTNAME${NC}:\n"
+printf "\nTesting access to \"${BOLD}$HOSTNAME${NC}\""
+printf "\nLooking for test string \"${BOLD}$TEST_STRING${NC}\"\n"
 
-printf "\n...via ${BOLD}IPv6${NC}:\n"
+#
+# IPV4
+#
+printf "\n...via ${BOLD}IPv4${NC}:\n"
 
+# IPV6 host
 TEST_NAME="$HOSTNAME"
-TEST=$(curl -4 -gsL "$HOSTNAME" | head -1 | xargs)
+TEST=$(curl -4 -gsL "$HOSTNAME" --max-time 5 | head -1 | xargs)
 if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
 
 TEST_NAME="www.$HOSTNAME"
-TEST=$(curl -4 -gsL "www.$HOSTNAME" | head -1 | xargs)
+TEST=$(curl -4 -gsL "www.$HOSTNAME" --max-time 5 | head -1 | xargs)
 if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
 
-TEST_NAME="https://$IPV4_IP"
-TEST=$(curl -4 -gskL "https://$IPV4_IP" | head -1 | xargs)
-if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
+# IPV6 ip
+if [ -n "$IPV4_IP" ]; then
+  TEST_NAME="https://$IPV4_IP"
+  TEST=$(curl -4 -gskL "https://$IPV4_IP" --max-time 5 | head -1 | xargs)
+  if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
 
-TEST_NAME="http://$IPV4_IP"
-TEST=$(curl -4 -gsL "http://$IPV4_IP" | head -1 | xargs)
-if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
+  TEST_NAME="http://$IPV4_IP"
+  TEST=$(curl -4 -gsL "http://$IPV4_IP" --max-time 5 | head -1 | xargs)
+  if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
 
-TEST_NAME="$IPV4_IP"
-TEST=$(curl -4 -gsL "$IPV4_IP" | head -1 | xargs)
-if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
+  TEST_NAME="$IPV4_IP"
+  TEST=$(curl -4 -gsL "$IPV4_IP" --max-time 5 | head -1 | xargs)
+  if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
+else
+  TEST_NAME="Could not get IPV4 IP address for hostname."
+  print_error
+fi
 
+
+#
+# IPV6
+#
 printf "\n...via ${BOLD}IPv6${NC}:\n"
 
+# IPV6 host
 TEST_NAME="$HOSTNAME"
-TEST=$(curl -6 -gsL "$HOSTNAME" | head -1 | xargs)
+TEST=$(curl -6 -gsL "$HOSTNAME" --max-time 5 | head -1 | xargs)
 if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
 
 TEST_NAME="www.$HOSTNAME"
-TEST=$(curl -6 -gsL "www.$HOSTNAME" | head -1 | xargs)
+TEST=$(curl -6 -gsL "www.$HOSTNAME" --max-time 5 | head -1 | xargs)
 if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
 
-TEST_NAME="http://$IPV6_IP"
-TEST=$(curl -6 -gsL "http://[$IPV6_IP]" | head -1 | xargs)
-if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
+# IPV6 ip
+if [ -n "$IPV6_IP" ]; then
+  TEST_NAME="http://$IPV6_IP"
+  TEST=$(curl -6 -gsL "http://[$IPV6_IP]" | head -1 | xargs)
+  if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
 
-TEST_NAME="https://$IPV6_IP"
-TEST=$(curl -6 -gskL "https://[$IPV6_IP]" | head -1 | xargs)
-if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
+  TEST_NAME="https://$IPV6_IP"
+  TEST=$(curl -6 -gskL "https://[$IPV6_IP]" | head -1 | xargs)
+  if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
 
-TEST_NAME="$IPV6_IP"
-TEST=$(curl -6 -gskL "[$IPV6_IP]" | head -1 | xargs)
-if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
+  TEST_NAME="$IPV6_IP"
+  TEST=$(curl -6 -gskL "[$IPV6_IP]" | head -1 | xargs)
+  if [ "$TEST" = "$TEST_STRING" ] ; then print_success ; else print_error ; fi
+else
+  TEST_NAME="Could not get IPV6 IP address for hostname."
+  print_error
+fi
