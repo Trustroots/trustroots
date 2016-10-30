bc97dbeb5b1bbf58bc913b202963f44385b1aea0
mrkvon <michal.salajka@email.cz>
2016-10-30 21:36:59 +0100

    test + implement basic ThreadStats model

---
+++ modules/messages/server/models/thread-stat.server.model.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+/**
+ * ThreadStat Schema
+ */
+var ThreadStatSchema = new Schema({
+  thread: {
+    type: Schema.ObjectId,
+    ref: 'Thread'
+  },
+  firstMessageUserFrom: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+  firstMessageUserTo: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+  firstMessageCreated: {
+    type: Date
+  },
+  firstMessageLength: {
+    type: Number
+  },
+  firstReplyCreated: {
+    type: Date,
+    default: null
+  },
+  firstReplyLength: {
+    type: Number,
+    default: null
+  }
+});
+
+mongoose.model('ThreadStat', ThreadStatSchema);
