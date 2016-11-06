4ae0a4ee0d0ff17c1cc077f1d41850732f5154d7
Mikael Korpela <mikael@ihminen.org>
2016-11-06 16:31:10 +0200

    Fix eslint issues #444

--- modules/core/server/jobs/send-email.server.job.js
+++ modules/core/server/jobs/send-email.server.job.js
@@ -10,22 +10,22 @@ module.exports = function(job, done) {
   var smtpTransport = nodemailer.createTransport(config.mailer.options);
 
   // The agenda docs don't show the shape of `job` so we're guessing here...
-  var jobId = _.get(job, '_id')
+  var jobId = _.get(job, '_id');
 
   // Log that we're sending an email
-  log('debug', 'Starting `send email` job #wGcxmQ', {jobId});
+  log('debug', 'Starting `send email` job #wGcxmQ', { jobId });
 
   smtpTransport.sendMail(job.attrs.data, function(err) {
     smtpTransport.close(); // close the connection pool
 
     if (err) {
       // Log the failure to send the message
-      log('error', 'The `send email` job failed #VDKMbr', {jobId, err})
+      log('error', 'The `send email` job failed #VDKMbr', { jobId, err });
 
       return done(err);
     } else {
       // Log the successful delivery of the message
-      log('info', 'Successfully finished `send email` job #4vO5Vt', {jobId})
+      log('info', 'Successfully finished `send email` job #4vO5Vt', { jobId });
 
       return done();
     }
