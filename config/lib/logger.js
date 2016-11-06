d28571412e56220b9ad9bc5e36d1e42826082f51
Callum Macdonald <git@callum-macdonald.com>
2016-11-06 20:01:20 +0100

    Add a console.log() on logger start. #444

--- config/lib/logger.js
+++ config/lib/logger.js
@@ -30,6 +30,7 @@ if (papertrailConfig.host && papertrailConfig.port) {
 
 // Log that the logger has been instantiated
 winston.log('info', 'Logger started #a5fKSK');
+console.log('Logger just started #SQrUgw');
 
 // We export `winston.log` so we can use `logger('info', ...)` etc
 module.exports = winston.log;
