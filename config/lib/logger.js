897fae868ee042fd302ce3911514c47d331fbbc2
Callum Macdonald <git@callum-macdonald.com>
2016-11-06 14:40:02 +0100

    Add logger to config/lib. #444

---
+++ config/lib/logger.js
@@ -0,0 +1,25 @@
+'use strict';
+// A general purpose logging service that can be used anywhere in the app
+
+/**
+ * Module dependencies.
+ */
+var path = require('path'),
+    _ = require('lodash'),
+    winston = require('winston'),
+    paperTrail = require('winston-papertrail').Papertrail,
+    config = require(path.resolve('./config/config'));
+
+var papertrailConfig = _.get(config, 'log.papertrail');
+
+// If there we have a host and port for papertrail, then configure it
+if (papertrailConfig.host && papertrailConfig.port) {
+  // Instantiate the papertrail transport
+  var winstonPapertrail = new winston.transports.Papertrail(papertrailConfig);
+
+  // Add the papertrail transport to winston
+  winston.add(winstonPapertrail);
+}
+
+// We export winston directly
+module.exports = winston;
