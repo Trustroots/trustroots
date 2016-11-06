5bb5eda0d157de91a306a4cd6166ab1f2614ccf9
Callum Macdonald <git@callum-macdonald.com>
2016-11-06 20:29:55 +0100

    Remove the `console.log()`. Tests are failing. #444

--- config/lib/logger.js
+++ config/lib/logger.js
@@ -31,7 +31,7 @@ if (papertrailConfig.host && papertrailConfig.port) {
 // Log that the logger has been instantiated
 winston.log('info', 'Logger started #a5fKSK');
 // Add a console.log() to help debugging logger issues
-console.log('Logger just started #SQrUgw');
+// console.log('Logger just started #SQrUgw');
 
 // We export `winston.log` so we can use `logger('info', ...)` etc
 module.exports = winston.log;
