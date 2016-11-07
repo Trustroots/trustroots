b11d9d90cf900768448393562a247ed0766f19eb
Mikael Korpela <mikael@ihminen.org>
2016-11-07 14:08:26 +0200

    Remove extra info from spark post test event

--- modules/sparkpost/tests/server/sparkpost.server.routes.tests.js
+++ modules/sparkpost/tests/server/sparkpost.server.routes.tests.js
@@ -31,52 +31,8 @@ describe('Sparkpost CRUD tests', function() {
           'msys': {
             'message_event': {
               'type': 'delivery',
-              'campaign_id': 'Example Campaign Name',
-              'customer_id': '1',
-              'delv_method': 'esmtp',
-              'device_token': '45c19189783f867973f6e6a5cca60061ffe4fa77c547150563a1192fa9847f8a',
-              'event_id': '92356927693813856',
-              'friendly_from': 'sender@example.com',
-              'ip_address': '127.0.0.1',
-              'ip_pool': 'Example-Ip-Pool',
-              'message_id': '000443ee14578172be22',
-              'msg_from': 'sender@example.com',
-              'msg_size': '1337',
-              'num_retries': '2',
-              'queue_time': '12',
-              'rcpt_meta': {
-                'customKey': 'customValue'
-              },
-              'rcpt_tags': [
-                'male',
-                'US'
-              ],
-              'rcpt_to': 'recipient@example.com',
-              'raw_rcpt_to': 'recipient@example.com',
-              'rcpt_type': 'cc',
-              'routing_domain': 'example.com',
-              'sending_ip': '127.0.0.1',
-              'subaccount_id': '101',
-              'subject': 'Summer deals are here!',
-              'sms_coding': 'ASCII',
-              'sms_dst': '7876712656',
-              'sms_dst_npi': 'E164',
-              'sms_dst_ton': 'International',
-              'sms_remoteids': [
-                '0000',
-                '0001',
-                '0002',
-                '0003',
-                '0004'
-              ],
-              'sms_segments': 5,
-              'sms_src': '1234',
-              'sms_src_npi': 'E164',
-              'sms_src_ton': 'Unknown',
-              'template_id': 'templ-1234',
-              'template_version': '1',
-              'timestamp': '1454442600',
-              'transmission_id': '65832150921904138'
+              'campaign_id': 'example',
+              'timestamp': '1454442600'
             }
           }
         }
