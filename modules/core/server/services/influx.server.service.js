0cd6eb380a1454d3b045a5a219ef66cc79698385
Mikael Korpela <mikael@ihminen.org>
2016-11-28 02:46:29 +0200

    Simplify Influxdb service API
    - Always expect`fields.time` to be a `Date` and turn it to integer (ms
    precision)
    - Improve error logging
    - Mock promise based Influx service (new version v5 of node-influx is promise based now)
    - Invoke callback at the end of `writeMeasurement`
    - Handle promises from node-influx in `writeMeasurement`

--- modules/core/server/services/influx.server.service.js
+++ modules/core/server/services/influx.server.service.js
@@ -6,7 +6,8 @@
 var path = require('path'),
     Influx = require('influx'),
     _ = require('lodash'),
-    config = require(path.resolve('./config/config'));
+    config = require(path.resolve('./config/config')),
+    log = require(path.resolve('./config/lib/logger'));
 
 /**
  * Get InfluxDB Client
@@ -36,7 +37,7 @@ exports.getClient = function(callback) {
  *   - key in camelCase
  *   - value - string or number (other options?)
  * You can specify time by passing a property called time (default: now)
- *   - time can either be an integer (default ms) or a date object.
+ *   - time should always be Date object.
  * tags - object of tag key: value pairs. To save to influxdb
  *   - key in camelCase
  *   - value should be string or will be casted to string
@@ -46,34 +47,56 @@ exports.getClient = function(callback) {
  * influxdb (camelCase)
  * @param {Object} fields - key: value pairs will be saved in influxdb as field
  * key: field value
- * @param {number|Date} [fields.time=new Date()] - time of measurement with precision nanosecond
+ * @param {number|Date} [fields.time=new Date()] - time of measurement
  * @param {Object} tags - key: value pairs will be saved in influxdb as tag key:
  * tag value
  * @param {function} callback - expected to be like function (err, result) {}
  */
 exports.writeMeasurement = function(measurementName, fields, tags, callback) {
 
+  var errorMessage;
+
   if (!measurementName || typeof measurementName !== 'string'
     || measurementName.length === 0) {
-    return callback(new Error('InfluxDB Service: no `measurementName` defined.'));
+    errorMessage = 'InfluxDB Service: no `measurementName` defined. #ghi3kH';
+    // Log the failure
+    log('error', errorMessage);
+    return callback(new Error(errorMessage));
   }
 
   if (!_.isPlainObject(fields)) {
-    return callback(new Error('InfluxDB Service: no `fields` defined.'));
+    errorMessage = 'InfluxDB Service: no `fields` defined. #ghugGJ';
+    // Log the failure
+    log('error', errorMessage, {
+      measurement: measurementName
+    });
+    return callback(new Error(errorMessage));
   }
 
   if (!_.isPlainObject(tags)) {
-    return callback(new Error('InfluxDB Service: no `tags` defined.'));
+    errorMessage = 'InfluxDB Service: no `tags` defined. #ghj3ig';
+    // Log the failure
+    log('error', errorMessage, {
+      measurement: measurementName
+    });
+    return callback(new Error(errorMessage));
   }
 
-  // Validate time: it should always be a `Date` object
-  if (fields.time && !_.isDate(fields.time)) {
-    return callback(new Error('InfluxDB Service: expected `fields.time` to be `Date` object.'));
-  }
-
-  // Add current time to `fields` if it's missing
   if (!fields.time) {
-    fields.time = new Date();
+    // Add current time to `fields` if it's missing
+    fields.time = new Date().getTime();
+  } else if (!_.isDate(fields.time)) {
+    // Validate time: it should always be a `Date` object
+    errorMessage = 'InfluxDB Service: expected `fields.time` to be `Date` object. #f93jkh';
+    // Log the failure
+    log('error', errorMessage, {
+      measurement: measurementName,
+      time: fields.time
+    });
+    return callback(new Error(errorMessage));
+  } else {
+    // Turn Date object into an integer (ms precision)
+    fields.time = fields.time.getTime();
   }
 
   exports.getClient(function(err, client) {
@@ -86,7 +109,19 @@ exports.writeMeasurement = function(measurementName, fields, tags, callback) {
         fields: fields,
         tags: tags
       }
-    ]);
+    ])
+    .then(function() {
+      if (callback) callback();
+    })
+    .catch(function(err) {
+      // Log the failure
+      log('error', 'InfluxDB Service: Error while writing to InfluxDB #fj38hh', {
+        error: err,
+        measurement: measurementName,
+        fields: fields,
+        tags: tags
+      });
+    });
 
   });
 };
