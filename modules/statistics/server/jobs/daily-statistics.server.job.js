dd237b3b62730276581e3f594eb89aec90d767a0
mrkvon <michal.salajka@protonmail.com>
2017-02-04 21:11:44 +0100

    generalized daily stats job

--- modules/statistics/server/jobs/daily-statistics.server.job.js
+++ modules/statistics/server/jobs/daily-statistics.server.job.js
@@ -8,7 +8,7 @@
  * Module dependencies.
  */
 var path = require('path'),
-    influxService = require(path.resolve('./modules/stats/server/services/influx.server.service')),
+    statsService = require(path.resolve('./modules/stats/server/services/stats.server.service')),
     statistics = require(path.resolve('./modules/statistics/server/controllers/statistics.server.controller'));
 
 module.exports = function (job, agendaDone) {
@@ -21,16 +21,39 @@ module.exports = function (job, agendaDone) {
       return agendaDone(err);
     }
 
-    // Save to influx here
-    // @TODO remake for a general Stats API
-    influxService._writeMeasurement('members', { count: count }, { members: 'members' },
-      function (err, result) {
-        if (err) {
-          console.error('Daily statistics: failed writing to InfluxDB.');
+    // Save to influx and stathat via Stats api
+    statsService.stat({
+      namespace: 'members',
+      values: {
+        count: count
+      },
+      tags: {
+        members: 'members'
+      }
+    }, function (err, result) { // eslint-disable-line no-unused-vars
+      if (err) {
+        console.error('Daily statistics: failed writing to some endpoints.');
+
+        if (err.message === 'Writing to Influx or Stathat service failed.') {
+          if (err.errors && err.errors.influx) {
+            console.error('Daily statistics: failed writing to influx');
+            console.error(err.errors.influx);
+          }
+          if (err.errors && err.errors.stathat) {
+            console.error('Daily statistics: failed writing to stathat');
+            console.error(err.errors.stathat);
+          }
+
+          // when the job fails writing to an endpoint, we probably don't want
+          // to retry, because writing to the non-failing one would be duplicate
+          // @TODO decide this better
+          err = null;
+        } else {
           console.error(err);
         }
-        result; // satisfy ESLint unused variables
-        agendaDone(err);
-      });
+      }
+
+      agendaDone(err);
+    });
   });
 };
---
+++ modules/statistics/tests/server/jobs/daily-statistics.server.job.tests.js
@@ -0,0 +1,156 @@
+'use strict';
+
+// test whether the daily statistics job reaches influxdb and stathat via Stats api
+
+var should = require('should'),
+    path = require('path'),
+    influx = require('influx'),
+    stathat = require('stathat'),
+    sinon = require('sinon'),
+    _ = require('lodash'),
+    config = require(path.resolve('./config/config')),
+    statsJob = require(path.resolve('./modules/statistics/server/jobs/daily-statistics.server.job'));
+
+describe('Daily Statistics Job - Unit Test', function () {
+  // replace the influx & stathat service stat() functions with fake version
+  var sandbox;
+
+  // initializing and clearing the sinon sandbox
+  beforeEach(function () {
+    // sandboxing in sinon helps restore the spied/stubbed/mocked functions
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function () {
+    // restore the stubbed services
+    sandbox.restore();
+  });
+
+  // stub the influx and stathat endpoints
+  beforeEach(function () {
+    // stub the influx endpoint(s)
+    sandbox.stub(influx.InfluxDB.prototype, 'writeMeasurement');
+
+    // and writeMeasurement returns a Promise
+    influx.InfluxDB.prototype.writeMeasurement.returns(
+      new Promise(function (resolve) {
+        process.nextTick(resolve());
+      })
+    );
+
+    // provide config options for influxdb
+    sandbox.stub(config.influxdb, 'options', {
+      host: 'localhost',
+      port: 8086,
+      protocol: 'http',
+      database: 'trustroots-test'
+    });
+
+    // stub the stathat endpoints
+    sandbox.stub(stathat, 'trackEZCount');
+    stathat.trackEZCount.callsArgAsync(3);
+
+    sandbox.stub(stathat, 'trackEZCountWithTime');
+    stathat.trackEZCountWithTime.callsArgAsync(4);
+
+    sandbox.stub(stathat, 'trackEZValue');
+    stathat.trackEZValue.callsArgAsync(3);
+
+    sandbox.stub(stathat, 'trackEZValueWithTime');
+    stathat.trackEZValueWithTime.callsArgAsync(4);
+  });
+
+  context('influxdb configured', function () {
+    beforeEach(function () {
+      // stub the config.stathat.key
+      // sandbox.stub(config.stathat, 'key', 'stathatkey');
+
+      // stub enable stathat in config
+      sandbox.stub(config.stathat, 'enabled', false);
+
+      // stub enable influx in config
+      sandbox.stub(config.influxdb, 'enabled', true);
+    });
+
+    it('should reach the influxdb with data in correct format', function (done) {
+      statsJob(null, function (e) {
+        if (e) return done(e);
+
+        try {
+          // test influx endpoint
+          sinon.assert.callCount(influx.InfluxDB.prototype.writeMeasurement, 1);
+
+          var measurement = influx.InfluxDB.prototype.writeMeasurement.getCall(0).args[0];
+          var points = influx.InfluxDB.prototype.writeMeasurement.getCall(0).args[1];
+          should(points.length).eql(1);
+          var point = points[0];
+
+          should(measurement).eql('members');
+          should(point).have.propertyByPath('fields', 'count').eql(0);
+          should(point).have.propertyByPath('tags', 'members').eql('members');
+          should(point).not.have.property('timestamp');
+
+          return done();
+        } catch (e) {
+          return done(e);
+        }
+      });
+    });
+  });
+
+  context('stathat configured', function () {
+    beforeEach(function () {
+      // stub the config.stathat.key
+      sandbox.stub(config.stathat, 'key', 'stathatkey');
+
+      // stub enable stathat in config
+      sandbox.stub(config.stathat, 'enabled', true);
+
+      // stub enable influx in config
+      sandbox.stub(config.influxdb, 'enabled', false);
+    });
+
+    it('should reach stathat with data in correct format', function (done) {
+      statsJob(null, function (e) {
+        if (e) return done(e);
+
+        try {
+          // test stathat endpoint
+          sinon.assert.callCount(stathat.trackEZValue, 2);
+
+          var calledWith = [
+            stathat.trackEZValue.getCall(0).args,
+            stathat.trackEZValue.getCall(1).args
+          ];
+
+          var groupedArgs = _.zip.apply(this, calledWith);
+
+          // the first argument to the endpoint should be the stathat key
+          should(groupedArgs[0]).deepEqual([config.stathat.key, config.stathat.key]);
+
+          // the 2nd argument to the endpoint should be the name
+          _.forEach([
+            'members.count',
+            'members.count.members.members' // with the members tag
+          ], function (value) {
+            should(groupedArgs[1]).containEql(value);
+          });
+
+          // the 3rd argument to the endpoint should be a value (values)
+          should(groupedArgs[2]).deepEqual([0, 0]);
+
+          // the 4th argument to the endpoint is a callback
+          _.forEach(groupedArgs[3], function (arg) {
+            should(arg).be.Function();
+          });
+
+          return done();
+        } catch (e) {
+          return done(e);
+        }
+      });
+    });
+
+  });
+
+});
