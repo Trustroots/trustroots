5b72699a00de3f31b02ec4c09a5e7d39d28450bd
Mikael Korpela <mikael@ihminen.org>
2017-02-22 21:34:49 +0200

    #342 Add `suspended` user role and handle suspended users
    - Won’t let users with `suspended` role sign in
    - Destroys active sessions of users with `suspended` role and causes
    API endpoints return 403
    - Redirects returning users with active sessions to “Your account has
    been suspended.” page with links to rules and contact pages.

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -197,7 +197,7 @@ var UserSchema = new Schema({
   roles: {
     type: [{
       type: String,
-      enum: ['user', 'admin']
+      enum: ['user', 'admin', 'suspended']
     }],
     default: ['user']
   },
