82a5eb1f52e87db3337c968115131cfe4be0fafe
Callum Macdonald <git@callum-macdonald.com>
2017-03-26 19:19:34 +0200

    First step towards sending script.

--- scripts/mass-email/sendMassMail.js
+++ scripts/mass-email/sendMassMail.js
@@ -1,8 +1,30 @@
-const fs = require("fs");
-const storage = require("node-persist");
+const _ = require("lodash");
+const shortid = require("shortid");
 
-storage.initSync({
-  dir: "state"
-});
+// nedb is a local disk backed mongo alternative for simple local scripts
+const Datastore = require("nedb");
+const Users = new Datastore({ filename: "data/users.db", autoload: true });
 
-const json = fs.readFileSync("users.json", { encoding: "utf-8" });
+// Grab 20 records and whip through them
+const sendTwenty = function() {
+  const cursor = Users.find({ sentAt: { $exists: false } })
+    .limit(20)
+    .exec((err, users) => {
+      // Send the actual emails
+
+      // Log the time of successful delivery
+      const when = new Date();
+      const batchId = shortid.generate();
+
+      // Record success
+      _.each(users, user => {
+        console.log(user);
+        Users.update(
+          { _id: user._id },
+          { $set: { sentAt: when, sentBatchId: batchId } }
+        );
+      });
+    });
+};
+
+sendTwenty();
