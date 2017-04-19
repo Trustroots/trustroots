415dc31d044961ac64eb6f241f6a23173f84f9df
Nick Sellen <git@nicksellen.co.uk>
2017-04-19 14:27:35 +0200

    Document use of push.init()

--- modules/core/client/config/core.client.run.js
+++ modules/core/client/config/core.client.run.js
@@ -12,6 +12,9 @@
     // If this fails, we'll try this again on successfull login
     Facebook.init();
 
+    // Initialize the push service if available
+    // It will check if user intended to enable push for this browser
+    // and setup the service-worker and backend registration accordingly
     push.init();
 
   }
