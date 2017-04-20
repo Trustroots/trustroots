2fbb051d064bbe3c31359f1847c1dd5fc3e802f3
Mikael Korpela <mikael@ihminen.org>
2017-04-20 17:39:12 +0300

    Turn Angular `strictDi` on for production

--- modules/core/client/app/init.js
+++ modules/core/client/app/init.js
@@ -77,10 +77,9 @@
     if (window.location.hash === '#_=_') window.location.hash = '';
 
     // Then init the app
-    // @todo: turn strictDi true for production
-    // @link https://docs.angularjs.org/guide/production
+    // @link https://docs.angularjs.org/guide/production#strict-di-mode
     angular.bootstrap(document, [AppConfig.appModuleName], {
-      strictDi: false
+      strictDi: (AppConfig.appEnv === 'production')
     });
 
     // Register base service worker
