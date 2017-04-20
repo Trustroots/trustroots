8f25400501ec81618b272e39ceae5b677731fd09
Nick Sellen <git@nicksellen.co.uk>
2017-04-20 11:43:08 +0200

    Revert to PhantomJS for testing
    Firebase is more abstracted now so should cause less problems not
    having all the latest browser APIs.

--- karma.conf.js
+++ karma.conf.js
@@ -41,7 +41,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Firefox'],
+    browsers: ['PhantomJS'],
 
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
