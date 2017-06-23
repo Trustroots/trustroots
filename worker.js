7664a1e290d434e47afaaefe67aa54439be983f2
Mikael Korpela <mikael@ihminen.org>
2017-06-23 17:09:51 +0300

    Refactor Agenda worker initialization
    - Gracefully exit Agenda on `SIGTERM` and `SIGINT`
    - Attempt to unlock Agenda jobs that were stuck due server restart. See https://github.com/agenda/agenda/issues/410

--- worker.js
+++ worker.js
@@ -1,16 +1,46 @@
 'use strict';
+
 /**
  * Trustroots
  *
  * Worker main entry file
  */
-var worker = require('./config/lib/worker'),
-    mongoose = require('./config/lib/mongoose');
+var async = require('async'),
+    mongoose = require('./config/lib/mongoose'),
+    worker = require('./config/lib/worker');
 
-mongoose.connect(function() {
-  mongoose.loadModels();
-  worker.start({
-    maxAttempts: 10,
-    retryDelaySeconds: 10
-  });
+async.waterfall([
+
+  // Bootstrap db connection
+  function(done) {
+    mongoose.connect(function() {
+      done();
+    });
+  },
+
+  // Load models
+  function(done) {
+    mongoose.loadModels(done);
+  },
+
+  // Clean out database
+  function(done) {
+    // Attempt to unlock jobs that were stuck due server restart
+    // See https://github.com/agenda/agenda/issues/410
+    worker.unlockAgendaJobs(done);
+  },
+
+  function() {
+    // Start the worker
+    worker.start({
+      maxAttempts: 10,
+      retryDelaySeconds: 10
+    });
+  }
+
+], function (err) {
+  if (err) {
+    console.log('[Worker] error on initializing worker:');
+    console.error(err);
+  }
 });
