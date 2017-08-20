6d14e7debf487fb5b062443d82dc38eeab08f306
Mikael Korpela <mikael@ihminen.org>
2017-08-21 00:47:43 +0300

    Set URL history state when opening offers on map
    Fixes #544

--- modules/search/client/controllers/search-map.client.controller.js
+++ modules/search/client/controllers/search-map.client.controller.js
@@ -6,7 +6,7 @@
     .controller('SearchMapController', SearchMapController);
 
   /* @ngInject */
-  function SearchMapController($scope, $stateParams, $timeout, $analytics, $window, OffersService, Authentication, leafletData, messageCenterService, MapLayersFactory, MapMarkersFactory, SearchMapService, FiltersService) {
+  function SearchMapController($scope, $state, $stateParams, $timeout, $analytics, $window, OffersService, Authentication, leafletData, messageCenterService, MapLayersFactory, MapMarkersFactory, SearchMapService, FiltersService) {
 
     // `search-map-canvas` is id of <leaflet> element
     var mapId = 'search-map-canvas';
@@ -112,6 +112,9 @@
       // If offer gets closed elsewhere
       $scope.$on('search.closeOffer', function() {
         vm.mapLayers.overlays.selectedOffers.visible = false;
+
+        // Set history state + URL without reloading the view
+        setOfferUrl('');
       });
 
       // Listen to new map location values from other controllers
@@ -175,6 +178,9 @@
         // Let parent controller handle setting this to scope
         $scope.$emit('search.previewOffer', offer);
 
+        // Set history state + URL without reloading the view
+        setOfferUrl(offer._id);
+
         // Position circle
         // If (click) event was passed to us, use coordinates from that.
         // This is because on map the dot is always at `fuzzyLocation`, but for
@@ -208,6 +214,22 @@
     function closeOffer() {
       vm.mapLayers.overlays.selectedOffers.visible = false;
       $scope.$emit('search.closeOffer');
+    }
+
+    /**
+     * Set URL history state without reloading the page
+     */
+    function setOfferUrl(offerId) {
+      $state.go(
+        'search.map',
+        { offer: offerId || '' },
+        {
+          location: true, // will update the url in the location bar,
+          inherit: true, // will inherit url parameters from current url.
+          notify: false, // will not broadcast $stateChangeStart and $stateChangeSuccess events.
+          reload: false // will not force transition even if no state or params have changed
+        }
+      );
     }
 
     /**
