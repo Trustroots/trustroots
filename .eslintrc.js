5a62b0475b9a768145b70dfa5368a2995d2fe000
Mikael Korpela <mikael@ihminen.org>
2017-08-26 02:01:35 +0300

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     'no-caller': 2,
     'no-console': 0,
     'no-else-return': 0,
-    'no-empty-class': 0,
+    'no-empty-character-class': 2,
     'no-multi-spaces': 2,
     'no-param-reassign': 0,
     'no-shadow': 0,
