5e76aacb122c7d4c2558ffceed43ced58b4527b6
Mikael Korpela <mikael@ihminen.org>
2017-10-17 14:03:43 +0300

    Add stats collecting endpoint

--- modules/statistics/server/routes/statistics.server.routes.js
+++ modules/statistics/server/routes/statistics.server.routes.js
@@ -5,6 +5,8 @@ module.exports = function (app) {
   var statistics = require('../controllers/statistics.server.controller');
 
   // Setting up the statistics api
-  app.route('/api/statistics').get(statistics.getPublicStatistics);
+  app.route('/api/statistics')
+    .post(statistics.collectStatistics)
+    .get(statistics.getPublicStatistics);
 
 };
