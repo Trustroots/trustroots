ea55f5d09797963f47a8343b8fbdd1491cdaa76e
Mikael Korpela <mikael@ihminen.org>
2017-10-17 14:06:18 +0300

    Send postMessage() on sign-out and sign-in to signal mobile app
    Mobile app listens to messages in WebView:
    
    https://github.com/Trustroots/trustroots-expo-mobile

--- modules/core/client/controllers/app.client.controller.js
+++ modules/core/client/controllers/app.client.controller.js
@@ -9,7 +9,7 @@
     .controller('AppController', AppController);
 
   /* @ngInject */
-  function AppController($scope, $rootScope, $uibModal, $window, $state, $analytics, Authentication, SettingsFactory, Languages, locker, PollMessagesCount, push) {
+  function AppController($location, $scope, $rootScope, $uibModal, $window, $state, $analytics, Authentication, SettingsFactory, Languages, locker, PollMessagesCount, push) {
 
     // ViewModel
     var vm = this;
@@ -240,6 +240,11 @@
         // Do the signout and refresh the page
         $window.top.location.href = '/api/auth/signout';
       });
+
+      // This will tell Mobile apps wrapping the site to disable push notifications at the device
+      if (typeof $window.postMessage === 'function') {
+        $window.postMessage('unAuthenticated', $location.protocol() + '://' + $location.host());
+      }
     }
 
 
