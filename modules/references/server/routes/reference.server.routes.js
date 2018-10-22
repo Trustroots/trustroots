889e97b7da634a08ccbbe97c4c1d34e2ca3838ce
mrkvon <mrkvon@users.noreply.github.com>
2018-10-22 19:21:15 +0200

    change variable naming

--- modules/references/server/routes/reference.server.routes.js
+++ modules/references/server/routes/reference.server.routes.js
@@ -3,11 +3,11 @@
 var path = require('path'),
     config = require(path.resolve('./config/config')),
     referencePolicy = require('../policies/references.server.policy'),
-    referenceThread = require('../controllers/reference.server.controller');
+    references = require('../controllers/reference.server.controller');
 
 module.exports = function (app) {
   if (config.featureFlags.reference) {
     app.route('/api/references').all(referencePolicy.isAllowed)
-      .post(referenceThread.create);
+      .post(references.create);
   }
 };
