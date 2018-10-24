b8646d4beccb9ef4bc56ecb61debf98e3e1adee4
Vlad Topala <topalavlad@gmail.com>
2018-10-24 12:59:20 +0300

    #### Proposed Changes
    Remove common tribes from user tribe list Since there's already a list of common tribes they shouldn't be shown again in the full list of the user's tribes right under the common tribes. Before showing the list of tribes the user is a member of the common ones will be removed
    
    #### Testing Instructions
    
    Check other user profiles with common tribes; without common tribes; with only common tribes; Also check currently logged in user profile
    
    Fixes #744

--- modules/users/client/directives/tr-memberships-list.client.directive.js
+++ modules/users/client/directives/tr-memberships-list.client.directive.js
@@ -9,16 +9,53 @@
     .directive('trMembershipsList', trMembershipsListDirective);
 
   /* @ngInject */
-  function trMembershipsListDirective() {
+  function trMembershipsListDirective(Authentication) {
     return {
       templateUrl: '/modules/users/views/directives/tr-memberships-list.client.view.html',
       restrict: 'A',
       replace: true,
       scope: {
-        memberships: '=trMembershipsList',
+        trMembershipsList: '=',
         isOwnProfile: '='
-      }
+      },
+      controller: trMembershipsListController,
+      controllerAs: 'tribesMembershipsList'
     };
-  }
 
+    /* @ngInject */
+    function trMembershipsListController($scope) {
+
+      // View Model
+      var vm = this;
+
+      // Exposed to the view
+      vm.memberships = [];
+
+      activate();
+
+      /**
+       * Initialize directive controller
+       */
+      function activate() {
+        var nonCommonTribes = [];
+
+        if ($scope.isOwnProfile) {
+          vm.memberships = $scope.trMembershipsList;
+          // Loop all tribes memberships
+        } else if (angular.isDefined($scope.trMembershipsList) &&
+          angular.isArray($scope.trMembershipsList) &&
+          Authentication.user.memberIds &&
+          Authentication.user.memberIds.length > 0) {
+          angular.forEach($scope.trMembershipsList, function (membership) {
+            // If authenticated user has it as well, it's already visible in the common tribes list
+            // so do not add it
+            if (membership.tribe && Authentication.user.memberIds.indexOf(membership.tribe._id) < 0) {
+              nonCommonTribes.push(membership);
+            }
+          });
+          vm.memberships = nonCommonTribes;
+        }
+      }
+    }
+  }
 }());
