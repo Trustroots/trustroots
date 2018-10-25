41e72e5b446d532d5469bf17fa626048b7f59e97
Kasper Souren <kasper@guaka.org>
2018-10-25 15:27:25 +0200

    bin/findUser.sh: simple shell script

---
+++ bin/findUser.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#########
+#
+# This is a simple shell script to help finding users
+#
+
+CMD="mongo --quiet trustroots-dev --eval"
+
+
+$CMD 'db.users.find({$or: [ 
+                     { "username": { $regex: /.*'$1'.*/i }},
+		     { "displayName": { $regex: /.*'$1'.*/i }},
+		     { "email": { $regex: /.*'$1'.*/i }}		     
+            	     ]
+                    }
+                   ).map (function(u) { return [ u.username, u.email ] })'
