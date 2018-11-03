214b4c5281629015b3d436df428908ef8ffb31e4
Nick Sellen <git@nicksellen.co.uk>
2018-11-03 12:44:17 +0000

    Auto import all react components

--- config/webpack/entries/main.js
+++ config/webpack/entries/main.js
@@ -6,7 +6,53 @@
  */
 
 import '@/public/dist/uib-templates';
+import angular from 'angular';
+import { react2angular } from 'react2angular';
 
 if (process.env.NODE_ENV === 'production') {
   require('@/public/dist/templates');
+}
+
+/*
+ *  Imports all react components from modules/ and register them as angular components
+ *
+ *  So if you defined modules/users/client/components/TrustrootsGreeting.component.js as:
+ *
+ *    export default function TrustrootsGreeting({ name }) {
+ *      return <p>Hello {name}, from Trustroots!</p>;
+ *    };
+ *
+ *  It will be available to use in (any) angular template as:
+ *
+ *    <trustroots-greeting name="'Donald'"></trustroots-greeting>
+ *
+ *  Uses a webpack require context
+ *  See https://webpack.js.org/guides/dependency-management/#require-context
+ */
+importComponents(require.context('../../../modules/', true, /\.component\.js$/));
+
+function importComponents(r) {
+  r.keys().forEach(path => {
+    const Component = r(path).default;
+    const name = extractComponentNameFromPath(path);
+    if (name !== Component.name) {
+      throw new Error(`Component filename and component name do not match: ${name || '<empty>'} vs ${Component.name || '<empty>'}`);
+    }
+    if (!Component.propTypes) {
+      throw new Error(`You must define propTypes on your component, e.g. ${name}.propTypes = {};`);
+    }
+    const propNames = Object.keys(Component.propTypes);
+    angular
+      .module('trustroots')
+      .component(lowercaseFirstLetter(Component.name), react2angular(Component, propNames));
+  });
+}
+
+function lowercaseFirstLetter(str) {
+  return str.charAt(0).toLowerCase() + str.slice(1);
+}
+
+function extractComponentNameFromPath(path) {
+  const m = /\/([^/]+)\.component\.js$/.exec(path);
+  if (m) return m[1];
 }
