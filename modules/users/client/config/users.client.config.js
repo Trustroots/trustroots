a917f03823cf3273ea4cd2d5c0a7afc8026364cf
mertmalcok <mert@mynextmatch.com>
2018-11-08 22:38:23 +0300

    Prevent httpProvider interceptor work for external domains

--- modules/users/client/config/users.client.config.js
+++ modules/users/client/config/users.client.config.js
@@ -14,19 +14,20 @@
       function ($q, $location, Authentication) {
         return {
           responseError: function (rejection) {
-            switch (rejection.status) {
-              case 401:
-                // Deauthenticate the global user
-                Authentication.user = null;
+            if (rejection.config.url.startsWith('/api/')) {
+              switch (rejection.status) {
+                case 401:
+                  // Deauthenticate the global user
+                  Authentication.user = null;
 
-                // Redirect to signin page
-                $location.path('/signin');
-                break;
-              case 403:
-                // Add unauthorized behaviour
-                break;
+                  // Redirect to signin page
+                  $location.path('/signin');
+                  break;
+                case 403:
+                  // Add unauthorized behaviour
+                  break;
+              }
             }
-
             return $q.reject(rejection);
           }
         };
