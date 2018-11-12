9b7861b30723b38b954750535ce25e5d4f0f1312
Kasper Souren <kasper@guaka.org>
2018-11-12 11:15:40 +0100

    bin/admin/findUser.js also show displayName

--- bin/admin/findUser.js
+++ bin/admin/findUser.js
@@ -17,7 +17,7 @@ User.find( { $or: [
   { 'displayName': requery },
 ]}, async function(err, docs) {
   await _.map(docs, function(d) {
-    console.log(d.username, d.email, d.roles);
+    console.log(d.username, d.email, d.displayName, d.roles, d._id);
   });
   trshell.weAreDone();
 });
