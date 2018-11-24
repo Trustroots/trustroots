485aeca2e215b11fb131b3b6af7a57a22e929850
Mikael Korpela <mikael@ihminen.org>
2018-11-24 18:12:48 +0200

    Fix previous rebases (#862)

--- modules/core/server/services/email.server.service.js
+++ modules/core/server/services/email.server.service.js
@@ -237,7 +237,7 @@ exports.sendSupportRequest = function (replyTo, supportRequest, callback) {
     name: 'Trustroots Support', // `To:`
     email: config.supportEmail, // `To:`
     replyTo: replyTo,
-    subject: 'Support request from ' + supportRequest.username + ' (' + supportRequest.displayName + ')',
+    subject: subject,
     request: supportRequest,
     skipHtmlTemplate: true, // Don't render html template for this email
     sparkpostCampaign: 'support-request'
