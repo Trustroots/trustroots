70f92a685560ebf0a71c5b44fc4a1418020a1e29
Kasper Souren <kasper@guaka.org>
2018-11-25 17:26:51 +0100

    plugging in express-redirect #742

--- modules/core/server/routes/core.server.routes.js
+++ modules/core/server/routes/core.server.routes.js
@@ -4,12 +4,19 @@
  * Module dependencies.
  */
 var path = require('path'),
+    redirect = require("express-redirect"),
     facebookNotificationService = require(path.resolve('./modules/core/server/services/facebook-notification.server.service')),
     core = require('../controllers/core.server.controller'),
     userProfile = require(path.resolve('./modules/users/server/controllers/users.profile.server.controller')),
     tribes = require(path.resolve('./modules/tribes/server/controllers/tribes.server.controller'));
 
+
 module.exports = function (app) {
+
+  redirect(app);
+
+  app.redirect('/tribes/lgbt', '/tribes/lgbtq');
+  app.redirect('/tribes/invite', '/signup');
 
   // Gives the service worker access to any config it needs
   app.route('/config/sw.js').get(core.renderServiceWorkerConfig);
