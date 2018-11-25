b33e40d9c79a3a772a96bd9d1e9e5333aa9df291
Mikael Korpela <mikael@ihminen.org>
2018-11-25 18:13:17 +0200

    Add eval CSP for Webpack
    This was recently removed when AddThis script was removed with invite-page.

--- config/lib/express.js
+++ config/lib/express.js
@@ -290,6 +290,8 @@ module.exports.initHelmetHeaders = function (app) {
 
       // Defines the origins from which scripts can be loaded.
       scriptSrc: [
+        // For Webpack
+        '\'unsafe-eval\'',
         // IE Edge does not support `nonce`, thus we need `unsafe-inline`. :-(
         // Using sha instead could work.
         '\'unsafe-inline\'',
