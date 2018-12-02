606d8df9e9c6575c136c992b4205ca949d8aa436
mrkvon <mrkvon@users.noreply.github.com>
2018-12-02 22:54:52 +0100

    move react component profile-view-basics
    to be part of the angularjs component, not to replace it

--- modules/users/client/components/ProfileViewBasics.component.js
+++ modules/users/client/components/ProfileViewBasics.component.js
@@ -4,15 +4,11 @@ import moment from 'moment';
 
 export default function ProfileViewBasics({ profile }) {
   return <>
-    <div className="panel panel-default profile-overview">
-      <div className="panel-body">
-        <div className="profile-sidebar-section">
-          Member since {moment(profile.created).format('MMM Do, YYYY')}
-        </div>
-        <div className="profile-sidebar-section">
-          {profile.seen ? <span>Online {moment(profile.seen).fromNow()}</span> : <span>Online long ago</span>}
-        </div>
-      </div>
+    <div className="profile-sidebar-section">
+      Member since {moment(profile.created).format('MMM Do, YYYY')}
+    </div>
+    <div className="profile-sidebar-section">
+      {profile.seen ? <span>Online {moment(profile.seen).fromNow()}</span> : <span>Online long ago</span>}
     </div>
   </>;
 };
