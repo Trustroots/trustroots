c7c1f1726aa88ef6a3c836c14f2b3973b970451b
Guaka <kasper@guaka.org>
2018-12-07 12:01:18 +0100

    React (#912)
    Merging back the react proof of concept #790. + fixes
    
    * Make the `npm run start:prod` pass.
    
    also
    - add react version to .eslintrc
    - make dependency versioning in package.json consistent (^ => ~)
    
    * remove firebase from devDependencies (let's do this change in different PR)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  extends: [
+    'plugin:react/recommended'
+  ],
+  settings: {
+    react: {
+      version: '16.6'
+    }
+  },
   rules: {
     camelcase: 0,
     'comma-dangle': [2, 'never'],
