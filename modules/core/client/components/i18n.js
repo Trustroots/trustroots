4dbe0c904239c2f11e712e4ddaa809b823de5c24
mrkvon <mrkvon@users.noreply.github.com>
2018-12-11 00:15:16 +0100

    add more complex translations and settings

--- modules/core/client/components/i18n.js
+++ modules/core/client/components/i18n.js
@@ -1,6 +1,7 @@
 import i18n from 'i18next';
 import { reactI18nextModule } from 'react-i18next';
 import backend from 'i18next-xhr-backend';
+import moment from 'moment';
 
 // translations are already at
 // '../public/locales/**/translation.json'
@@ -11,11 +12,29 @@ i18n
   .use(reactI18nextModule) // passes i18n down to react-i18next
   .init({
     lng: 'eng',
-
+    // allow keys to be phrases having `:`, `.`
+    nsSeparator: false,
+    keySeparator: false,
+    // saveMissing: true, // @TODO send not translated keys to endpoint
     keySeparator: false, // we do not use keys in form messages.welcome
-
     interpolation: {
-      escapeValue: false // react already safes from xss
+      escapeValue: false, // react already safes from xss
+      format: (value, format, languageCode) => {
+        // these are the codes that momentjs supports
+        // we could replace the 3letter codes with these
+        // these seem to be more widely used (wikipedia in different languages, momentjs, ...)
+        const codes = {
+          eng: 'en',
+          cze: 'cs'
+        };
+
+        if (format) {
+          moment.locale(codes[languageCode]);
+          if (format === 'fromNow') return moment(value).fromNow();
+          return moment(value).format(format);
+        }
+        return value;
+      }
     }
   });
 
