bd129a94f6174b4d999c79bd6b076b7e51c9e13d
mrkvon <mrkvon@users.noreply.github.com>
2018-12-10 15:14:29 +0100

    change language codes from ISO 639-1 to ISO 639-2
    change variable name

--- modules/core/client/components/LanguageSwitch.component.js
+++ modules/core/client/components/LanguageSwitch.component.js
@@ -2,14 +2,14 @@ import React from 'react';
 import i18n from './i18n';
 
 export default function LanguageSwitch() {
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
+  const changeLanguage = (languageCode) => {
+    i18n.changeLanguage(languageCode);
   };
 
   return (
     <select onChange={(event) => changeLanguage(event.target.value)}>
-      <option value="en">en</option>
-      <option value="cs">cs</option>
+      <option value="eng">eng</option>
+      <option value="cze">cze</option>
     </select>
   );
 };
--- modules/core/client/components/i18n.js
+++ modules/core/client/components/i18n.js
@@ -1,16 +1,16 @@
 import i18n from 'i18next';
 import { reactI18nextModule } from 'react-i18next';
-import * as translationEN from '../../../../public/locales/en/translation';
-import * as translationCS from '../../../../public/locales/cs/translation';
+import * as translationENG from '@/public/locales/eng/translation';
+import * as translationCZE from '@/public/locales/cze/translation';
 
 // the translations
 // (tip move them in a JSON file and import them)
 const resources = {
-  en: {
-    translation: translationEN
+  eng: {
+    translation: translationENG
   },
-  cs: {
-    translation: translationCS
+  cze: {
+    translation: translationCZE
   }
 };
 
@@ -18,7 +18,7 @@ i18n
   .use(reactI18nextModule) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'en',
+    lng: 'eng',
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
