b2b9de638c8864cec14fd88d5814d69de749556a
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2018-12-13 13:10:29 +0100

    link to actual profile pages

--- modules/search/client/components/UsersList.js
+++ modules/search/client/components/UsersList.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Avatar = ({ avatarSize, avatarSource, userData }) => (
+const Avatar = ({ avatarSize, avatarSource, displayName, profileUrl }) => (
   <div data-size={avatarSize}>
-    <a href={userData.profileUrl}
-      aria-label={`Open user profile for ${userData.displayName}`}>
+    <a href={profileUrl}
+      aria-label={`Open user profile for ${displayName}`}>
       <img className={`avatar avatar-${avatarSize} avatar-${avatarSource}`}
         alt=''
         // {alt={`Profile picture for ${userData.displayName}`}}
         aria-hidden="true"
         src={avatarSource}
-        href={userData.profileUrl}
+        href={profileUrl}
         draggable="false" />
     </a>
   </div>
@@ -23,34 +23,37 @@ Avatar.defaultProps = {
 Avatar.propTypes = {
   avatarSize: PropTypes.number,
   avatarSource: PropTypes.string,
-  userData: PropTypes.object
+  displayName: PropTypes.string,
+  profileUrl: PropTypes.string
 };
 
-const User = ({ userData }) => (
-  <div className="contacts-contact panel panel-default">
-    <Avatar avatarSize={128} avatarSource="/test.html" userData={userData}/>
-    <h4>
-      <a href="profile.about({username: contactCtrl.contact.user.username})">
-        {userData.displayName}
-      </a>
-    </h4>
-    <div ng-if="::contactCtrl.contact.user.locationLiving">
-      <i className="icon-fw icon-building text-muted"></i>
-      <small>Lives in <a ui-sref="search.map({location: contactCtrl.contact.user.locationLiving})"
-        ng-bind="::contactCtrl.contact.user.locationLiving"></a></small>
+const User = ({ userData }) => {
+  const profileUrl = `/profile/${userData.username}`;
+  return (
+    <div className="contacts-contact panel panel-default">
+      <Avatar avatarSize={128} avatarSource="/test.html"
+        displayName={userData.displayName}
+        profileUrl={profileUrl}/>
+      <h4>
+        <a href={profileUrl}>
+          {userData.displayName}
+        </a>
+      </h4>
+      <div ng-if="::contactCtrl.contact.user.locationLiving">
+        <i className="icon-fw icon-building text-muted"></i>
+        <small>Lives in <a ui-sref="search.map({location: contactCtrl.contact.user.locationLiving})"
+          ng-bind="::contactCtrl.contact.user.locationLiving"></a></small>
+      </div>
+
+      <div ng-if="::contactCtrl.contact.user.locationFrom">
+        <i className="icon-fw icon-home text-muted"></i>
+        <small>From <a ui-sref="search.map({location: contactCtrl.contact.user.locationFrom})"
+          ng-bind="::contactCtrl.contact.user.locationFrom"></a></small>
+      </div>
+
     </div>
-
-    <div ng-if="::contactCtrl.contact.user.locationFrom">
-      <i className="icon-fw icon-home text-muted"></i>
-      <small>From <a ui-sref="search.map({location: contactCtrl.contact.user.locationFrom})"
-        ng-bind="::contactCtrl.contact.user.locationFrom"></a></small>
-    </div>
-
-  </div>
-
-  // tr-contact-profile-id="::profileCtrl.profile._id"
-  // tr-contact="::contact"></div>
-);
+  );
+};
 
 User.propTypes = {
   userData: PropTypes.object
