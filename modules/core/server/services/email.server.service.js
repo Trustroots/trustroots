11e4ac40ad0382303d67469c4e48f5c3ff6bdce3
mrkvon <mrkvon@users.noreply.github.com>
2018-12-13 03:54:36 +0100

    enable link to profile in email footer

--- modules/core/server/services/email.server.service.js
+++ modules/core/server/services/email.server.service.js
@@ -417,6 +417,7 @@ exports.sendReferenceNotificationFirst = function (userFrom, userTo, callback) {
   var params = exports.addEmailBaseTemplateParams({
     subject: 'New reference from ' + userFrom.username,
     email: userTo.email,
+    username: userTo.username, // data needed for link to profile in footer
     userFrom: userFrom,
     userTo: userTo,
     userFromProfileUrl: url + '/profile/' + userFrom.username,
@@ -434,6 +435,7 @@ exports.sendReferenceNotificationSecond = function (userFrom, userTo, reference,
   var params = exports.addEmailBaseTemplateParams({
     subject: 'New reference from ' + userFrom.username,
     email: userTo.email,
+    username: userTo.username, // data needed for link to profile in footer
     userFrom: userFrom,
     userTo: userTo,
     userFromProfileUrl: url + '/profile/' + userFrom.username,
