ebfed3141139a58f8175cb3fc4d97208ed7fe70f
Talita <dzikilubiabloto@protonmail.com>
2018-12-17 20:38:40 +0100

    move elsewhere section to react

--- modules/users/client/components/ProfileViewBasics.component.js
+++ modules/users/client/components/ProfileViewBasics.component.js
@@ -4,8 +4,8 @@ import '@/config/lib/i18n';
 import PropTypes from 'prop-types';
 import * as languages from '@/config/languages/languages';
 
-export default function ProfileViewBasics({ profile }) {
-  return (<NamespacesConsumer ns={['user-profile']}>{ t => (<>
+export default function ProfileViewBasics({ profile, hasConnectedAdditionalSocialAccounts, isWarmshowersId, socialAccountLink }) {
+  return (<NamespacesConsumer ns={['user-profile', 'languages']}>{ (t) => (<>
     {/* show div only if replyrate or replyTime is present */}
     {(profile.replyRate || profile.replyTime) &&
     <div className="profile-sidebar-section text-muted">
@@ -49,14 +49,72 @@ export default function ProfileViewBasics({ profile }) {
     {profile.languages.length &&
       <div className="profile-sidebar-section">
         <h4 id="profile-languages">{t('Languages')}</h4>
-        <NamespacesConsumer ns={['languages']}>{ t => (
-          <ul className="list-unstyled" aria-describedby="profile-languages">
-            {profile.languages.map(code => <li key={code}>{t(languages[code]) || code}</li>)}
-          </ul>)}</NamespacesConsumer>
+        <ul className="list-unstyled" aria-describedby="profile-languages">
+          {profile.languages.map(code => <li key={code}>{t(languages[code], { ns: 'languages' }) || code}</li>)}
+        </ul>
       </div>}
+      { (hasConnectedAdditionalSocialAccounts() || profile.extSitesBW || profile.extSitesCS || profile.extSitesWS) &&
+        <div className="profile-sidebar-section">
+          <h4 id="profile-networks" aria-label="Member in other networks">
+            {t('Elsewhere')}
+          </h4>
+          <ul className="social-profiles list-unstyled"
+            aria-describedby="profile-networks">
+            {/*
+              Facebook profile link is hidden here until issue with their API gets resolved
+              See https://github.com/Trustroots/trustroots/issues/237
+            */}
+            {profile.additionalProvidersData && Object.keys(profile.additionalProvidersData).map(
+              (key, indeks) =>
+              {return (key !== 'facebook') && <li className="social-profile" key={indeks}><i className={`social-profile-icon icon-fw icon-lg icon-${key}`}></i>
+                <a rel="noopener"
+                  className="social-profile-handle text-capitalize"
+                  href={socialAccountLink(key, profile.additionalProvidersData[key])}>{key}
+                </a>
+              </li>;
+              }
+            )}
+
+            {profile.extSitesBW &&
+            <li className="social-profile">
+              <i className="social-profile-icon icon-fw icon-lg icon-bw"></i>
+              <a rel="noopener"
+                className="social-profile-handle"
+                href={`https://www.bewelcome.org/members/${profile.extSitesBW}`}>
+                BeWelcome
+              </a>
+            </li>}
+
+            {profile.extSitesCS &&
+            <li className="social-profile">
+              <i className="social-profile-icon icon-fw icon-lg icon-cs"></i>
+              {/*
+                Link here has `noreferrer` because;
+                https://github.com/Trustroots/trustroots/issues/464
+              */}
+              <a rel="noreferrer noopener"
+                className="social-profile-handle"
+                href={`https://www.couchsurfing.com/people/${profile.extSitesCS }`}>
+                Couchsurfing
+              </a>
+            </li>}
+
+            {profile.extSitesWS &&
+            <li className="social-profile">
+              <i className="social-profile-icon icon-fw icon-lg icon-warmshowers"></i>
+              <a className="social-profile-handle"
+                href={`https://www.warmshowers.org/${isWarmshowersId ? 'user' : 'users' }/${profile.extSitesWS}`}>
+                Warmshowers
+              </a>
+            </li>}
+          </ul>
+        </div>}
   </>) }</NamespacesConsumer>);
 };
 
 ProfileViewBasics.propTypes = {
-  profile: PropTypes.object
+  profile: PropTypes.object,
+  hasConnectedAdditionalSocialAccounts: PropTypes.func,
+  isWarmshowersId: PropTypes.func,
+  socialAccountLink: PropTypes.func
 };
