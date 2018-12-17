7ae8c1f87cd5632d2473d0936bf5a3dc1e9171b7
Talita <dzikilubiabloto@protonmail.com>
2018-12-17 20:21:35 +0100

    move from,location,languages section to react

--- modules/users/client/components/ProfileViewBasics.component.js
+++ modules/users/client/components/ProfileViewBasics.component.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { NamespacesConsumer } from 'react-i18next';
 import '@/config/lib/i18n';
 import PropTypes from 'prop-types';
+import * as languages from '@/config/languages/languages';
 
 export default function ProfileViewBasics({ profile }) {
-  return (<NamespacesConsumer ns="user-profile">{ t => (<>
+  return (<NamespacesConsumer ns={['user-profile']}>{ t => (<>
     {/* show div only if replyrate or replyTime is present */}
     {(profile.replyRate || profile.replyTime) &&
     <div className="profile-sidebar-section text-muted">
@@ -23,7 +24,7 @@ export default function ProfileViewBasics({ profile }) {
     <div className="profile-sidebar-section" >
       {profile.birthdate && t('{{birthdate, age}} years', { birthdate: new Date(profile.birthdate) })}
       {(profile.birthdate && profile.gender) && <span>, </span>}
-      <span className={(!profile.birthdate) && 'text-capitalize'}>{t(profile.gender)}.</span>
+      <span className={(!profile.birthdate) ? 'text-capitalize' : null }>{t(profile.gender)}.</span>
     </div>}
     <div className="profile-sidebar-section">
       {t('Member since {{date, MMM Do, YYYY}}', { date: new Date(profile.created) })}
@@ -33,6 +34,26 @@ export default function ProfileViewBasics({ profile }) {
         {(profile.seen) ? t('Online {{date, fromNow}}', { date: new Date(profile.seen) }) : t('Online long ago')}
       </span>
     </div>
+    {profile.locationLiving &&
+      <div className="profile-sidebar-section">
+        <i className="icon-fw icon-building text-muted"></i>
+        {t('Lives in')} <a ui-sref="search.map({location: profileCtrl.profile.locationLiving})"
+        >{profile.locationLiving}</a>
+      </div>}
+    {profile.locationFrom &&
+      <div className="profile-sidebar-section">
+        <i className="icon-fw icon-home text-muted"></i>
+        {t('From')} <a ui-sref="search.map({location: profileCtrl.profile.locationFrom})">
+          {profile.locationFrom}</a>
+      </div>}
+    {profile.languages.length &&
+      <div className="profile-sidebar-section">
+        <h4 id="profile-languages">{t('Languages')}</h4>
+        <NamespacesConsumer ns={['languages']}>{ t => (
+          <ul className="list-unstyled" aria-describedby="profile-languages">
+            {profile.languages.map(code => <li key={code}>{t(languages[code]) || code}</li>)}
+          </ul>)}</NamespacesConsumer>
+      </div>}
   </>) }</NamespacesConsumer>);
 };
 
