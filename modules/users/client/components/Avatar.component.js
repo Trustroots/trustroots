e502c40b18e52f6a2691e474922478953f859e2e
mrkvon <mrkvon@users.noreply.github.com>
2018-12-19 17:15:45 +0100

    comments

--- modules/users/client/components/Avatar.component.js
+++ modules/users/client/components/Avatar.component.js
@@ -4,6 +4,13 @@ import has from 'lodash/has';
 import '@/config/lib/i18n';
 import { withNamespaces } from 'react-i18next';
 
+/**
+ * User's avatar
+ * @param {object} user - User object
+ * @param {integer=256} size - Size of the image <img>. Supported values are 2048, 1024, 512, 256, 128, 64, 36, 32, 24 and 16. See avatar.less for details. Defaults to 256.
+ * @param {string=''} source - Leave empty to use user's selected source. Values "none", "facebook", "local", "gravatar".
+ * @param {boolean=true} link - Include a link to user's profile. Defaults to true.
+ */
 export function Avatar({ t, user, size=256, source='', link=true }) {
 
   source = source || user.avatarSource;
@@ -53,14 +60,26 @@ Object.defineProperty(AvatarHOC, 'name', { value: 'Avatar' });
 export default AvatarHOC;
 
 
-// Avatar via FB
-// @link https://developers.facebook.com/docs/graph-api/reference/user/picture/
+/**
+ * Generate avatar url from facebook
+ * @link https://developers.facebook.com/docs/graph-api/reference/user/picture/
+ * @param {object} user - user object
+ * @param {string} user.additionalProvidersData.facebook.id - user's facebook id
+ * @param {number} size - size of the image
+ * @returns {string} - the url
+ */
 function facebookAvatarUrl(user, size) {
   const isValid = has(user, 'additionalProvidersData.facebook.id');
 
   return isValid && `https://graph.facebook.com/${user.additionalProvidersData.facebook.id}/picture/?width=${size}&height=${size}`;
 }
 
+/**
+ * Generate url to avatar image that user uploaded
+ * @param {object} user - user object
+ * @param {number} size - size of the image
+ * @returns {string} - the url
+ */
 function localAvatarUrl(user, size) {
   const isValid = user && user.avatarUploaded && user._id;
 
@@ -74,8 +93,16 @@ function localAvatarUrl(user, size) {
   }
 }
 
-// Avatar via Gravatar
-// @link https://en.gravatar.com/site/implement/images/
+/**
+ * Generate avatar url from Gravatar
+ * @link https://en.gravatar.com/site/implement/images/
+ * @param {object} user - user object
+ * @param {string} user.emailHash - gravatar identifies users by their email hashes
+ * @param {number} size - size of the image
+ * @returns {string} - the url
+ *
+ * @todo fallback image is provided from trustroots.org; it should rather come from config
+ */
 function gravatarUrl(user, size, defaultAvatar) {
   const isValid = user.emailHash;
 
@@ -86,16 +113,17 @@ function gravatarUrl(user, size, defaultAvatar) {
   return isValid && `https://gravatar.com/avatar/${user.emailHash}?s=${size}&d=${encodeURIComponent(fallbackImage)}`;
 }
 
+/**
+ * Generate avatar url
+ * @param {object} user - user object
+ * @param {integer} size - size of the image. Supported values are 2048, 1024, 512, 256, 128, 64, 36, 32, 24, 16.
+ * @param {string} source - avatar source. One of ['' (user's selected source), 'none', 'facebook', 'gravatar', 'local']
+ * @param {string} defaultAvatar - url to a default avatar
+ * @returns {string} - the url
+ */
 function avatarUrl(user, source, size, defaultAvatar) {
   return (source === 'local' && localAvatarUrl(user, size))
     || (source === 'gravatar' && gravatarUrl(user, size, defaultAvatar))
     || (source === 'facebook' && facebookAvatarUrl(user, size))
     || `${defaultAvatar}?none`;
 }
-
-/**
- * @param {object} user User object
- * @param {int} size Size of the image <img>. Supported values are 2048, 1024, 512, 256, 128, 64, 36, 32, 24 and 16. See avatar.less for details. Defaults to 256.
- * @param {string} source Leave empty to use user's selected source. Values "none", "facebook", "local", "gravatar".
- * @param {boolean} link Link to user's profile. Defaults to true.
- */
