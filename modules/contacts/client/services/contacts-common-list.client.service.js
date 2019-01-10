597c3fb27d639c8148f244d8bf7eb044dd5447f5
RGHuynh <randall.g.huynh@gmail.com>
2019-01-09 20:33:22 -0500

    Refactor contacts-common-list service

--- modules/contacts/client/services/contacts-common-list.client.service.js
+++ modules/contacts/client/services/contacts-common-list.client.service.js
@@ -1,22 +1,3 @@
-// (function () {
-//   'use strict';
-
-//   // Read common contacts list by userId.
-//   // Returns a list of contacts currently
-//   // authenticated user has in common with profile.
-//   angular
-//     .module('contacts')
-//     .factory('ContactsCommonListService', ContactsCommonListService);
-
-//   /* @ngInject */
-//   function ContactsCommonListService($resource) {
-//     return $resource('/api/contacts/:listUserId/common', {
-//       listUserId: '@id'
-//     });
-//   }
-
-// }());
-
 var axios = require('axios');
 
 exports.contactsCommonListService = function (id) {
