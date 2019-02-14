443882d5a272da9d5a417e58c6eaa04cc014f836
Talita <dzikilubiabloto@protonmail.com>
2019-02-14 13:01:53 +0100

    simplify properties, add component state for profile toggle

--- modules/users/client/components/AboutMe.component.js
+++ modules/users/client/components/AboutMe.component.js
@@ -1,52 +1,72 @@
-import React from 'react';
-import { NamespacesConsumer } from 'react-i18next';
+import React, { Component } from 'react';
+import { withNamespaces } from '@/modules/core/client/utils/i18n-angular-load';
+{/* import { Trans } from 'react-i18next'; */}
 import '@/config/lib/i18n';
 import PropTypes from 'prop-types';
 import { limitTo, plainTextLength } from './utils/filters';
 
-export default function AboutMe({ profile, profileDescriptionToggle, user, appSettings }) {
-  return (<NamespacesConsumer ns={['user-profile']}>{ (t) => (<>
-    <section className="panel panel-default">
-      <header className="panel-heading">
-        {t('About me')}
-      </header>
-      <div className="panel-body">
-        {/* Short descriptions */}
-        {(profile.description.length < 2400 || profileDescriptionToggle) &&
-        <div dangerouslySetInnerHTML={{ __html: profile.description }} ></div>}{/* TODO Can we trust this HTML?*/}
+export class AboutMe extends Component {
+  constructor(props) {
+    super(props);
+    this.changeProfileDescriptionToggle = this.changeProfileDescriptionToggle.bind(this);
 
-        {/* Long descriptions */}
-        {profile.description.length >= 2400 && !profileDescriptionToggle &&
-        <div
-          className="panel-more-wrap" ng-click="profileCtrl.profileDescriptionToggle=true">
-          <div className="panel-more-excerpt" setInnerHTML={limitTo(profile.description, 2400)}> </div>
-          <div className="panel-more-fade">{t('Show more...')}</div>
-        </div>}
+    this.state = {
+      profileDescriptionToggle: false
+    };
+  }
+  changeProfileDescriptionToggle(){
+    this.setState((prevState) => ({
+      profileDescriptionToggle: !prevState.profileDescriptionToggle
+    }));
+  }
 
-        {/* If no description, show deep thoughts... */}
-        {(!profile.description || profile.description === '') &&
-        <blockquote className="profile-quote"
-          aria-label={t('Member has not written description about themself.')}>
-        “Everyone is necessarily the hero of their own life story.”
-        </blockquote>}
-      </div>
+  render(){
+    const { t, profile, isSelf, appSettings } = this.props;
+    return (<>
+      <section className="panel panel-default">
+        <header className="panel-heading">
+          {t('About me')}
+        </header>
+        <div className="panel-body">
+          {/* Short descriptions */}
+          {(profile.description.length < 2400 || this.state.profileDescriptionToggle) &&
+          <div>{profile.description }</div>}{/* TODO Can we trust this HTML?*/}
 
-      {/* User watching their own profile and it's too short */}
-      {(user._id === profile._id && (!profile.description || plainTextLength(profile.description) < appSettings.profileMinimumLength)) &&
-      <footer className="panel-footer">
-        <p className="lead">
-          {t('Your profile description should be longer so that you can send messages.')}
-          <br/><br />
-          <a href="/profile/edit" className="btn btn-primary">{t('Fill your profile')}</a>
-        </p>
-      </footer>}
-    </section>
-  </>) }</NamespacesConsumer>);
+          {/* Long descriptions */}
+          {profile.description.length >= 2400 && !this.state.profileDescriptionToggle &&
+          <div
+            className="panel-more-wrap">
+            <div className="panel-more-excerpt">{limitTo(profile.description, 2400)}> </div>
+            <div className="panel-more-fade" onClick={this.changeProfileDescriptionToggle}>{t('Show more...')}</div>
+          </div>}
+
+          {/* If no description, show deep thoughts... */}
+          {(!profile.description || profile.description === '') &&
+          <blockquote className="profile-quote"
+            aria-label={t('Member has not written description about themself.')}>
+          “Everyone is necessarily the hero of their own life story.”
+          </blockquote>}
+        </div>
+
+        {/* User watching their own profile and it's too short */}
+        {(isSelf && (!profile.description || plainTextLength(profile.description) < appSettings.profileMinimumLength)) &&
+        <footer className="panel-footer">
+          <p className="lead">
+            {t('Your profile description should be longer so that you can send messages.')}
+            <br/><br />
+            <a href="/profile/edit" className="btn btn-primary">{t('Fill your profile')}</a>
+          </p>
+        </footer>}
+      </section>
+    </>);
+  }
 };
 
 AboutMe.propTypes = {
   profile: PropTypes.object,
-  profileDescriptionToggle: PropTypes.bool,
-  user: PropTypes.object,
-  appSettings: PropTypes.object
+  isSelf: PropTypes.bool,
+  appSettings: PropTypes.object,
+  t: PropTypes.func
 };
+
+export default withNamespaces(['user-profile'])(AboutMe);
