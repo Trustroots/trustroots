24847029f8cc4cb6b4f2527607d7e4255562bc75
mrkvon <mrkvon@users.noreply.github.com>
2019-03-01 22:00:05 +0100

    migrate bottom navigation for small screens to react

---
+++ modules/users/client/components/BottomNavigationSmall.component.js
@@ -0,0 +1,70 @@
+/**
+ * Bottom navigation for small screens
+ */
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import classNames from 'classnames';
+
+export default function BottomNavigationSmall({ username, isSelf, contactCount }) {
+  // @TODO the default value for 'active' will need to be fetched in more React way
+  // when we have a router in place
+  const [active, setActive] = useState(window.location.pathname.split('/')[3] || 'about');
+
+  const { t } = useTranslation('user');
+
+  const tabs = [
+    {
+      key: 'overview',
+      label: t('Overview'),
+      link: `/profile/${username}/overview`
+    },
+    {
+      key: 'about',
+      label: t('About'),
+      link: `/profile/${username}`
+    },
+    {
+      key: 'accommodation',
+      label: t('Hosting'),
+      link: `/profile/${username}/accommodation`
+    }
+  ];
+
+  // contacts tab is shown only sometimes
+  if (contactCount > 0 || isSelf) {
+    tabs.push({
+      key: 'contacts',
+      label: <>{t('Contacts')} <span className="badge">{contactCount}</span></>,
+      link: `/profile/${username}/contacts`
+    });
+  }
+
+  return (
+    <nav className="navbar navbar-default navbar-fixed-bottom visible-xs-block" role="navigation">
+      <div className="container">
+        <ul className="nav navbar-nav" role="tablist">
+          {tabs.map(({ key, label, link }) => (
+            <li
+              key={key}
+              className={classNames({ active: active === key })}
+              ui-sref-active="active" role="presentation">
+              <a
+                href={link}
+                role="tab"
+                onClick={() => setActive(key)}
+              >{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+BottomNavigationSmall.propTypes = {
+  isSelf: PropTypes.bool.isRequired,
+  contactCount: PropTypes.number.isRequired,
+  username: PropTypes.string.isRequired
+};
