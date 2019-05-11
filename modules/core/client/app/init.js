cd3eb6dc944fd91508f7ee975e3bc76f57493c92
Mikael Korpela <mikael@ihminen.org>
2019-05-12 00:12:00 +0300

    Fix failing Chrome
    not sure what's going on but let's see if  this fixes it.
    
    We're now hosting the library locally anyway so we don't need a fancy UI to configure it globally via Angular.

--- modules/core/client/app/init.js
+++ modules/core/client/app/init.js
@@ -12,7 +12,7 @@
     .config(initConfig);
 
   /* @ngInject */
-  function initConfig(lockerProvider, cfpLoadingBarProvider, $locationProvider, $urlMatcherFactoryProvider, $messageCenterServiceProvider, $compileProvider, $uibTooltipProvider) {
+  function initConfig(lockerProvider, cfpLoadingBarProvider, $locationProvider, $urlMatcherFactoryProvider, $compileProvider, $uibTooltipProvider) {
 
     // Setting HTML5 Location Mode
     $locationProvider.html5Mode({
@@ -39,9 +39,6 @@
       eventsEnabled: false,
       extend: {}
     });
-
-    // Default timeout for success, error etc messages
-    $messageCenterServiceProvider.setGlobalOptions({ timeout: 6000 });
 
     // Disabling Debug Data for production environment
     // @link https://docs.angularjs.org/guide/production
