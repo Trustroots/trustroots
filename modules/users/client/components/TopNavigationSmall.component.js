f8ba223f2862b007976f0528601fb490ed596321
mrkvon <mrkvon@users.noreply.github.com>
2019-11-12 00:04:59 +0100

    add translations, refactors, fixes. getting there.

--- modules/users/client/components/TopNavigationSmall.component.js
+++ modules/users/client/components/TopNavigationSmall.component.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import RemoveContact from '../../../contacts/client/components/RemoveContactContainer';
 
-export default function TopNavigationSmall({ isSelf, username, contact, areReferences, isResolved }) {
+// @TODO isResolved can be removed when migration is finished
+export default function TopNavigationSmall({ username, contact, areReferences, isResolved, selfId, userId }) {
+
+  // @TODO this hacky fix should be removed and the data should be consistent
+  if (contact) {
+    if (typeof contact.userFrom === 'object') {
+      contact.userFrom = contact.userFrom._id;
+    }
+    if (typeof contact.userTo === 'object') {
+      contact.userTo = contact.userTo._id;
+    }
+  }
+
+  const [showRemoveModal, setShowRemoveModal] = useState(false);
+
+  const { t } = useTranslation('user');
+
+  const isSelf = selfId === userId;
 
   let links;
-  console.log('*********************************', isSelf, username, contact, areReferences, isResolved);
-
   if (isSelf) {
     links = [
       {
-        label: 'Edit your profile',
+        label: t('Edit your profile'),
         link: '/profile/edit'
       }
     ];
   } else {
     links = [
       {
-        label: 'Send a message',
+        label: t('Send a message'),
         link: `/messages/${username}`
       }
     ];
 
     if (areReferences) {
       links.push({
-        label: 'Write a reference',
+        label: t('Write a reference'),
         link: `/profile/${username}/references/new`
       });
     }
@@ -32,57 +49,95 @@ export default function TopNavigationSmall({ isSelf, username, contact, areRefer
     if (isResolved) {
       const contactLink = (!contact._id) ?
         {
-          label: 'Add contact',
-          link: `/messages/${username}`
+          label: t('Add contact'),
+          link: `/contact-add/${userId}`
         }
         :
         (contact.confirmed) ?
           {
-            label: labelWithTooltip({ label: 'Remove contact', tooltip: 'Contacts since {{ ::profileCtrl.contact.created | date:\'mediumDate\''}),
-            link: `/messages/${username}`,
-            tooltip: 'Contacts since {{ ::profileCtrl.contact.created | date:\'mediumDate\''
+            label: t('Remove contact'),
+            tooltip: t('Contacts since {{created, MMM DD, YYYY}}', { created: new Date(contact.created) }),
+            onClick: () => setShowRemoveModal(true)
           }
           :
           {
-            label: 'Delete contact request',
-            link: `/messages/${username}`,
-            tooltip: 'Request sent {{ ::profileCtrl.contact.created | date:\'mediumDate\' }}'
+            label: t('Delete contact request'),
+            tooltip: t('Request sent {{created, MMM DD, YYYY}}', { created: new Date(contact.created) }),
+            onClick: () => setShowRemoveModal(true)
           };
 
       links.push(contactLink);
     }
   }
 
+  function handleRemoveSuccess() {
+    setShowRemoveModal(false);
 
+    // @TODO!!!!!!!!!!!! broadcast the removal to angular
+    // onContactRemoved(contact);
+  }
 
   return (
-    <nav className="navbar navbar-white navbar-fixed-top navbar-fixed-top-below visible-xs-block">
-      <div className="container">
-        <ul className="nav navbar-nav" role="navigation">
-          {links.map(({ label, link }, index) => (
-            <li key={index}><a href={link}>{label}</a></li>
-          )
-          )}
-        </ul>
-      </div>
-    </nav>
+    <>
+      {contact && <RemoveContact
+        selfId={selfId}
+        contact={contact}
+        show={showRemoveModal}
+        onSuccess={handleRemoveSuccess}
+        onCancel={() => setShowRemoveModal(false)}
+      />}
+      <nav className="navbar navbar-white navbar-fixed-top navbar-fixed-top-below visible-xs-block">
+        <div className="container">
+          <ul className="nav navbar-nav" role="navigation">
+            {links.map(({ label, link, tooltip, onClick }, index) => (
+              <li key={index}>
+                <NavButton label={label} link={link} tooltip={tooltip} onClick={onClick} />
+              </li>
+            )
+            )}
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 }
 
-function labelWithTooltip({ label, tooltip }) {
+function LabelWithTooltip({ label, tooltip }) {
   return (
     <OverlayTrigger
       placement="bottom"
       overlay={<Tooltip>{tooltip}</Tooltip>}
     >
-        <span>{label}</span>
+      <span>{label}</span>
     </OverlayTrigger>
   );
 }
 
+function NavButton({ label, link, tooltip, onClick }) {
+
+  const labelWithTooltip = (tooltip) ? <LabelWithTooltip label={label} tooltip={tooltip} /> : label;
+
+  return (link)
+    ? <a href={link}>{labelWithTooltip}</a>
+    : <a onClick={onClick}>{labelWithTooltip}</a>;
+}
+
+NavButton.propTypes = {
+  label: PropTypes.any.isRequired,
+  link: PropTypes.string,
+  tooltip: PropTypes.string,
+  onClick: PropTypes.func
+};
+
+LabelWithTooltip.propTypes = {
+  label: PropTypes.string.isRequired,
+  tooltip: PropTypes.string.isRequired
+};
+
 TopNavigationSmall.propTypes = {
-  isSelf: PropTypes.bool.isRequired,
   username: PropTypes.string.isRequired,
+  selfId: PropTypes.string.isRequired,
+  userId: PropTypes.string.isRequired,
   contact: PropTypes.object.isRequired,
   areReferences: PropTypes.bool.isRequired,
   isResolved: PropTypes.bool.isRequired
