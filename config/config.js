6f6eeda0c947e3f68590265c82afc9196250ee27
Mikael Korpela <mikael@ihminen.org>
2019-11-15 22:59:02 +0200

    Fix eslint `no-prototype-builtins` issues

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ var getGlobbedPaths = function (globPatterns, excludes) {
         files = files.map(function (file) {
           if (_.isArray(excludes)) {
             for (var i in excludes) {
-              if (excludes.hasOwnProperty(i)) {
+              if (Object.prototype.hasOwnProperty.call(excludes, i)) {
                 file = file.replace(excludes[i], '');
               }
             }
