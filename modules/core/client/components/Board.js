579a646c24e77268116f0c8e670aae474864d607
mrkvon <mrkvon@users.noreply.github.com>
2019-11-28 01:01:22 +0100

    basic implemented

---
+++ modules/core/client/components/Board.js
@@ -0,0 +1,14 @@
+import { Children, cloneElement, useEffect } from 'react';
+import { selectPhoto } from '../services/photos.service';
+
+export default function Board({ name='bokeh', children, onNameChanged }) {
+
+  // update
+  useEffect(() => {
+    onNameChanged();
+  }, [name]);
+
+  const photo = selectPhoto(name);
+  const style = { backgroundImage: `url("${photo.imageUrl}")` };
+  return Children.map(children, child => cloneElement(child, { style }));
+}
---
+++ modules/tribes/client/components/TribesHeader.component.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import Board from '../../../core/client/components/Board';
+
+export default function TribesHeader({ isLogged }) {
+
+  const { t } = useTranslation('tribes');
+
+  return <Board name="tribes-1">
+    <section className="board tribes-header" tr-boards="'tribes-1'">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 text-center">
+            <br /><br />
+            <h2>{t('Discover Tribes')}</h2>
+            <br />
+            <p className="lead">
+              {t('Joining Tribes helps you find likeminded Trustroots members.')}
+            </p>
+            {!isLogged && <div>
+              <hr className="hr-white hr-xs"/>
+              <a href="/signup" className="btn btn-action btn-default">
+                {t('Sign up with Trustroots')}
+              </a>
+            </div>}
+          </div>
+        </div>
+      </div>
+    </section>
+  </Board>;
+}
+
+TribesHeader.propTypes = {
+  isLogged: PropTypes.bool.isRequired
+};
