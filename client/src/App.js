2b7a75edd83f237b146fae3fb5abb6d02dd2a0c8
mrkvon <mrkvon@users.noreply.github.com>
2019-11-28 02:40:05 +0100

    run server and client concurrently, cleanup App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let data
+      try {
+        ({ data } = await axios.get('/api/users/1'));
+      } catch (e) {
+        ({ data } = e.response);
+      } finally {
+        console.log('data', data);
+        setUser(data);
+      }
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>app, users {JSON.stringify(user)}</div>
   );
 }
 
