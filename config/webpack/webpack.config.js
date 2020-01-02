b7af71e97b99946a17400211e26fc9cffe15e575
Mikael Korpela <mikael@ihminen.org>
2020-01-02 11:59:23 +0200

    Build: Add support for optional chain and null coalesce
    Enables two stage 3 proposals:
    
    Optional chaining: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining (via https://babeljs.io/docs/en/next/babel-plugin-proposal-optional-chaining)
    
    Nullish coalescing operator: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator (via https://babeljs.io/docs/en/next/babel-plugin-proposal-nullish-coalescing-operator)

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -38,7 +38,9 @@ module.exports = merge(shims, {
               ['@babel/preset-react']
             ],
             plugins: [
+              '@babel/plugin-proposal-nullish-coalescing-operator',
               '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-proposal-optional-chaining',
               'angularjs-annotate'
             ]
           }
