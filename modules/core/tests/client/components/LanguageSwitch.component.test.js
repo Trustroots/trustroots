db77e1321f70ca0c505a6a7baab8cd0cbbcc2476
Mikael Korpela <mikael@ihminen.org>
2020-01-03 01:34:06 +0200

    Fix test

--- modules/core/tests/client/components/LanguageSwitch.component.test.js
+++ modules/core/tests/client/components/LanguageSwitch.component.test.js
@@ -20,7 +20,7 @@ describe('default presentation', () => {
   it('has a menu item for each language', async () => {
     const { getAllByRole } = render(<LanguageSwitch/>);
     const items = getAllByRole('menuitem');
-    expect(items).toHaveLength(3);
+    expect(items).toHaveLength(4);
     const names = items.map(item => item.innerHTML);
     expect(names).toEqual(localeNames);
   });
@@ -45,7 +45,7 @@ describe('select presentation', () => {
   it('has an option for each language', async () => {
     const { getAllByRole } = render(<LanguageSwitch presentation="select"/>);
     const items = getAllByRole('option');
-    expect(items).toHaveLength(3);
+    expect(items).toHaveLength(4);
     const names = items.map(item => item.innerHTML);
     expect(names).toEqual(localeNames);
   });
