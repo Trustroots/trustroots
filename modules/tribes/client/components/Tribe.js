6095b912be765a1412dda90b8c4c691ba9e6f116
mrkvon <mrkvon@users.noreply.github.com>
2020-01-05 21:27:32 +0100

    Migrate list of tribes to React (#1144)
    + introduce styled-components (https://www.styled-components.com/)

--- modules/tribes/client/components/Tribe.js
+++ modules/tribes/client/components/Tribe.js
@@ -2,25 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
 
 import JoinButton from './JoinButton';
 
 import getTribeBackgroundStyle from './helpers/getTribeBackgroundStyle';
 
-/**
- * @TODO maybe rename to Tribe
- */
-export default function TribeItem({ tribe, user, onMembershipUpdated }) {
+const Container = styled.div.attrs({
+  className: 'panel tribe tribe-image',
+})`
+  // the following styles should have high specificity
+  // https://www.styled-components.com/docs/faqs#how-can-i-override-styles-with-higher-specificity
+  &&& {
+    position: relative;
+    ${({ tribe }) => getTribeBackgroundStyle(tribe, { isProgressive: true, dimensions: '742x496' })}
+  }
+`;
+
+export default function Tribe({ tribe, user, onMembershipUpdated }) {
   const { t } = useTranslation('tribes');
 
   const countInfo = (tribe.count === 0)
     ? t('No members yet')
     : t('{{count, number}} members', { count: tribe.count });
 
-  return <div
-    className="panel tribe tribe-image"
-    style={getTribeBackgroundStyle(tribe, { isProgressive: true, dimensions: '742x496' })}
-  >
+  return <Container tribe={tribe}>
     <a href={`/tribes/${tribe.slug}`} className="tribe-link">
       {tribe.new && <span className="tribe-new" aria-hidden={true}>
         <span className="label label-primary">
@@ -40,10 +46,10 @@ export default function TribeItem({ tribe, user, onMembershipUpdated }) {
         onUpdated={onMembershipUpdated}
       />}
     </div>
-  </div>;
+  </Container>;
 }
 
-TribeItem.propTypes = {
+Tribe.propTypes = {
   tribe: PropTypes.object.isRequired,
   user: PropTypes.object,
   onMembershipUpdated: PropTypes.func.isRequired,
