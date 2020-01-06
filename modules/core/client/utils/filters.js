cb71c2ca812891a9efff865d7d0a5a59f09822e7
Mikael Korpela <mikael@ihminen.org>
2020-01-06 09:42:22 +0200

    Fix document-check bug

--- modules/core/client/utils/filters.js
+++ modules/core/client/utils/filters.js
@@ -1,5 +1,5 @@
 export const plainText = (html) => {
-  if (!html || typeof html !== 'string' || typeof document !== 'undefined') {
+  if (!html || typeof html !== 'string' || typeof document === 'undefined') {
     return '';
   }
   const div = document.createElement('div');
