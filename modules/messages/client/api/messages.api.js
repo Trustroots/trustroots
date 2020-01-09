9a5740a058692db19261367cc426238e128f3dc7
Nick Sellen <git@nicksellen.co.uk>
2020-01-09 14:59:13 +0100

    Remove unused API methods

--- modules/messages/client/api/messages.api.js
+++ modules/messages/client/api/messages.api.js
@@ -18,14 +18,3 @@ export async function fetchThreads(params = {}) {
     nextParams,
   };
 }
-
-export async function fetchMessages(userId) {
-  const { data: messages } = await axios.get(`/api/messages/${userId}`);
-  return messages;
-}
-
-// TODO this doesn't belong here
-export async function fetchUser(username) {
-  const { data: user } = await axios.get(`/api/users/${username}`);
-  return user;
-}
