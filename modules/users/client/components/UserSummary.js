3291cc9491369b60c2ba741d5fe1d4a6b2e4ac0f
Mikael Korpela <mikael@ihminen.org>
2020-01-11 18:31:01 +0200

    Move styled container outside component and rename

--- modules/users/client/components/UserSummary.js
+++ modules/users/client/components/UserSummary.js
@@ -6,35 +6,35 @@ import styled from 'styled-components';
 // Internal dependencies
 import Avatar from '@/modules/users/client/components/Avatar.component';
 
-export default function UserSummary({ user }) {
-  const UserSummary = styled.div.attrs({
-    className: 'panel panel-default',
-  })`
-    margin: 0;
-    overflow: hidden;
-    .avatar {
-      border-radius: 0;
-      float: left;
-      margin: 0 10px 0 0;
+const Container = styled.div.attrs({
+  className: 'panel panel-default',
+})`
+  margin: 0;
+  overflow: hidden;
+  .avatar {
+    border-radius: 0;
+    float: left;
+    margin: 0 10px 0 0;
+  }
+  h4 {
+    margin-left: 5px;
+    margin-right: 5px;
+    a {
+      text-decoration: none;
     }
-    h4 {
-      margin-left: 5px;
-      margin-right: 5px;
-      a {
-        text-decoration: none;
-      }
-    }
-  `;
+  }
+`;
 
+export default function UserSummary({ user }) {
   return (
-    <UserSummary>
+    <Container>
       <Avatar link size={128} user={user} />
       <h4>
         <a href={`/profile/${user.username}`}>
           {user.displayName}
         </a>
       </h4>
-    </UserSummary>
+    </Container>
   );
 }
 
