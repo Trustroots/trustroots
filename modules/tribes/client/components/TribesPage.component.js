3f5145a6fe17d38ab80f11c8120cbbd0126d3cfe
mrkvon <mrkvon@users.noreply.github.com>
2020-01-19 22:47:56 +0100

    add a comment and TODO to a method that updates tribes state

--- modules/tribes/client/components/TribesPage.component.js
+++ modules/tribes/client/components/TribesPage.component.js
@@ -13,6 +13,13 @@ export default function TribesPage({ user, onDisplayPhoto, onHidePhoto, onMember
   const handleMembershipUpdated = data => {
     // update the tribes in state
 
+    /**
+     * When user's membership in a tribe changes
+     * replace the tribe in react state with the new tribe returned by API
+     * and update the state with a new array of tribes.
+     * @TODO extract the mapReplace method to be reusable
+     * https://github.com/Trustroots/trustroots/pull/1145#discussion_r368321566
+     */
     setTribes(tribes => tribes.map(tribe => tribe._id === data.tribe._id ? data.tribe : tribe));
 
     onMembershipUpdated(data);
