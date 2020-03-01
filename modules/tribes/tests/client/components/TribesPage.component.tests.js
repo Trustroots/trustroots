985349c841ae761c8506bffd03e9f1cb5677c68f
mrkvon <mrkvon@users.noreply.github.com>
2020-03-01 15:10:28 +0100

    Generate fake test data (#1190)
    * generate fake test data
    
    and use them in TribesPage tests
    
    * add a note about code relationship to bin/fillTestData
    
    * group lodash imports
    
    * make use of lodash range method
    
    * improve generating id
    
    * generate fake data in testutils/common/data.common.testutil.js
    
    and simplify testutils/common/data.common.testutil.js a bit
    
    Co-authored-by: Nick Sellen <git@nicksellen.co.uk>

--- modules/tribes/tests/client/components/TribesPage.component.tests.js
+++ modules/tribes/tests/client/components/TribesPage.component.tests.js
@@ -3,6 +3,10 @@ import { within } from '@testing-library/react';
 import { render, fireEvent, waitForElement } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import '@/config/client/i18n';
+import {
+  generateUsers,
+  generateTribes,
+} from '@/testutils/common/data.common.testutil';
 
 import TribesPage from '@/modules/tribes/client/components/TribesPage.component';
 import * as tribesApi from '@/modules/tribes/client/api/tribes.api';
@@ -13,45 +17,6 @@ jest.mock('@/modules/tribes/client/api/tribes.api');
 
 const onMembershipUpdated = jest.fn();
 
-const dummyTribes = [
-  {
-    _id: 'aaaa',
-    id: 'aaaa',
-    count: 20000,
-    slug: 'hitchhikers',
-    label: 'Hitchhikers',
-  },
-  {
-    _id: 'bbbb',
-    id: 'bbbb',
-    count: 5000,
-    slug: 'snails',
-    label: 'Snails',
-  },
-  {
-    _id: 'cccc',
-    id: 'cccc',
-    count: 1500,
-    slug: 'teddybears',
-    label: 'Teddy Bears',
-  },
-];
-
-const dummyUser = {
-  _id: 'user0',
-  id: 'user0',
-  memberIds: ['cccc', 'aaaa'],
-};
-
-api.tribes.read.mockImplementation(async () => dummyTribes);
-api.tribes.join.mockImplementation(async id => ({
-  tribe: dummyTribes.find(tribe => tribe._id === id),
-}));
-api.tribes.leave.mockImplementation(async id => ({
-  tribe: dummyTribes.find(tribe => tribe._id === id),
-}));
-
-// helper function to determine a membership
 const isMember = (user, tribe) => user.memberIds.includes(tribe._id);
 
 // TextMatch function for finding an element with arbitrarily nested text
@@ -78,11 +43,29 @@ const renderAndWaitForTribes = async ({ user }) => {
 };
 
 describe('TribesPage', () => {
+  const fake = (() => {
+    const tribes = generateTribes(5);
+    return {
+      tribes,
+      users: generateUsers(1, {}, 'client', tribes),
+    };
+  })();
+
+  let page;
+
+  beforeEach(() => {
+    api.tribes.read.mockImplementation(async () => fake.tribes);
+    api.tribes.join.mockImplementation(async id => ({
+      tribe: fake.tribes.find(tribe => tribe._id === id),
+    }));
+    api.tribes.leave.mockImplementation(async id => ({
+      tribe: fake.tribes.find(tribe => tribe._id === id),
+    }));
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
-
-  let page;
 
   describe('not signed in', () => {
     beforeEach(async () => {
@@ -93,10 +76,10 @@ describe('TribesPage', () => {
       // get tribes and omit the last one, which is "Missing your Tribe?"
       const tribes = page.getAllByRole('listitem').slice(0, -1);
 
-      expect(tribes).toHaveLength(dummyTribes.length);
+      expect(tribes).toHaveLength(fake.tribes.length);
 
       tribes.forEach((tribe, i) => {
-        expect(tribe).toHaveTextContent(dummyTribes[i].label);
+        expect(tribe).toHaveTextContent(fake.tribes[i].label);
       });
 
       // the api should be called only once
@@ -108,9 +91,9 @@ describe('TribesPage', () => {
       // get tribes and omit the last one, which is "Missing your Tribe?"
       const buttons = page.getAllByText('Join', { selector: 'a' });
 
-      expect(buttons).toHaveLength(dummyTribes.length);
+      expect(buttons).toHaveLength(fake.tribes.length);
 
-      dummyTribes.forEach((tribeData, i) => {
+      fake.tribes.forEach((tribeData, i) => {
         expect(buttons[i]).toHaveAttribute(
           'href',
           `/signup?tribe=${tribeData.slug}`,
@@ -121,17 +104,17 @@ describe('TribesPage', () => {
 
   describe('signed in', () => {
     beforeEach(async () => {
-      page = await renderAndWaitForTribes({ user: dummyUser });
+      page = await renderAndWaitForTribes({ user: fake.users[0] });
     });
 
     it('show tribes on page', async () => {
       // get tribes and omit the last one, which is "Missing your Tribe?"
       const tribes = page.getAllByRole('listitem').slice(0, -1);
 
-      expect(tribes).toHaveLength(dummyTribes.length);
+      expect(tribes).toHaveLength(fake.tribes.length);
 
       tribes.forEach((tribe, i) => {
-        expect(tribe).toHaveTextContent(dummyTribes[i].label);
+        expect(tribe).toHaveTextContent(fake.tribes[i].label);
       });
 
       // during the test the api should be called only once
@@ -140,12 +123,12 @@ describe('TribesPage', () => {
     });
 
     it('user is member of some tribes and not member of others', async () => {
-      dummyTribes.forEach(tribeData => {
+      fake.tribes.forEach(tribeData => {
         const tribe = page.getByText(nestedTextMatch(tribeData.label), {
           selector: 'li',
         });
 
-        if (isMember(dummyUser, tribeData)) {
+        if (isMember(fake.users[0], tribeData)) {
           within(tribe).getByText('Joined', { selector: 'button' });
         } else {
           within(tribe).getByText('Join', { selector: 'button' });
@@ -153,8 +136,8 @@ describe('TribesPage', () => {
       });
     });
 
-    for (const tribeData of dummyTribes) {
-      if (isMember(dummyUser, tribeData)) {
+    for (const tribeData of fake.tribes) {
+      if (isMember(fake.users[0], tribeData)) {
         it('[user is a member] click leave with modal and send api request', async () => {
           expect(api.tribes.leave).toHaveBeenCalledTimes(0);
 
