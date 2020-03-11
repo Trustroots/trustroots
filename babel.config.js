82ceb0202fadd6d617489945a2b82ee59fc756a7
mrkvon <mrkvon@users.noreply.github.com>
2020-03-11 01:39:01 +0100

    use babel-plugin-i18next-extract

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
 const compact = require('lodash/compact');
+const path = require('path');
+const locales = require(path.resolve('./config/shared/locales'));
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
@@ -20,5 +22,16 @@ module.exports = {
     '@babel/plugin-proposal-optional-chaining',
     'angularjs-annotate',
     isDevelopment && 'react-refresh/babel',
+    [
+      'i18next-extract',
+      {
+        nsSeparator: false,
+        locales: locales.map(locale => locale.code),
+        keySeparator: false,
+        outputPath: 'public/locales/{{locale}}/{{ns}}.json',
+        keyAsDefaultValue: ['en'],
+        discardOldKeys: true,
+      },
+    ],
   ]),
 };
