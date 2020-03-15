0d4d7138f06872844b3f56397ec10a27ad4e06a5
Mikael Korpela <mikael@ihminen.org>
2020-03-15 15:46:04 +0200

    Fix meet listing

--- modules/offers/client/components/OfferLocation.component.js
+++ modules/offers/client/components/OfferLocation.component.js
@@ -7,23 +7,24 @@ import Map from '@/modules/core/client/components/Map/index';
 import OfferLocationOverlay from './OfferLocationOverlay';
 
 export default function OfferLocation({ location, offerType, offerStatus }) {
+  if (!location || location.length !== 2) {
+    return null;
+  }
+
   return (
-    location &&
-    location.length === 2 && (
-      <Map
-        className="offer-location"
-        height={320}
+    <Map
+      className="offer-location"
+      height={320}
+      location={location}
+      width="100%"
+      zoom={11}
+    >
+      <OfferLocationOverlay
         location={location}
-        width="100%"
-        zoom={11}
-      >
-        <OfferLocationOverlay
-          location={location}
-          offerType={offerType}
-          offerStatus={offerStatus}
-        />
-      </Map>
-    )
+        offerType={offerType}
+        offerStatus={offerStatus}
+      />
+    </Map>
   );
 }
 
