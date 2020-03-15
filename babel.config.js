20276ed3c4afbe0259176e6e788bde8a88bb8664
Mikael Korpela <mikael@ihminen.org>
2020-03-15 15:28:11 +0200

    Clean out class stuff

--- babel.config.js
+++ babel.config.js
@@ -15,7 +15,6 @@ module.exports = {
     ['@babel/preset-react'],
   ],
   plugins: compact([
-    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-optional-chaining',
--- config/webpack/entries/main.js
+++ config/webpack/entries/main.js
@@ -62,10 +62,7 @@ function importComponents(r) {
   r.keys().forEach(path => {
     const Component = r(path).default;
     const name = extractComponentNameFromPath(path);
-    if (
-      ![name, '_class'].includes(Component.name) &&
-      process.env.NODE_ENV !== 'production'
-    ) {
+    if (name !== Component.name && process.env.NODE_ENV !== 'production') {
       throw new Error(
         `Component filename and component name do not match: ${name ||
           '<empty>'} vs ${Component.name || '<empty>'}`,
