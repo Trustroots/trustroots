833e2312c05ac552d94bcff699bbf78e2638ed74
Mikael Korpela <mikael@ihminen.org>
2020-03-15 16:53:23 +0200

    Add doc-blocks

--- modules/offers/client/utils/markers.js
+++ modules/offers/client/utils/markers.js
@@ -1,3 +1,12 @@
+/**
+ * Calculate how many meters one pixel of a map contains, based on zoom and latitude level
+ * @link https://stackoverflow.com/a/30773300
+ * @link https://wiki.openstreetmap.org/wiki/Zoom_levels
+ *
+ * @param  {[float]} latitude
+ * @param  {[number]} zoom
+ * @return {[number]}
+ */
 const metersPerPixel = ({ latitude, zoom }) => {
   const earthCircumference = 40075017;
   const latitudeRadians = latitude * (Math.PI / 180);
@@ -6,9 +15,25 @@ const metersPerPixel = ({ latitude, zoom }) => {
   );
 };
 
+/**
+ * Turn latitude, meters, and zoom level into a value that can be used as radius for SVG circle element on a Map.
+ * @link https://stackoverflow.com/a/30773300
+ *
+ * @param  {[float]} latitude
+ * @param  {[number]} meters
+ * @param  {[number]} zoom
+ * @return {[number]} Value to be passed for radius of a Circle SVG element.
+ */
 export const zoomToPixelMeters = ({ latitude, meters, zoom }) =>
   meters / metersPerPixel({ latitude, zoom });
 
+/**
+ * Return HEX color for the type of hosting offer
+ *
+ * @param  {String} [offerType='other']
+ * @param  {String} [offerStatus='yes']
+ * @return {string} HEX value
+ */
 export const getOfferHexColor = ({
   offerType = 'other',
   offerStatus = 'yes',
