594bd7f3d85afa28b2ba519776fc8f0e90b1823a
Mikael Korpela <mikael@ihminen.org>
2020-03-15 17:13:14 +0200

    Add map scaffolding

---
+++ modules/search/client/components/SearchMap.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SearchMap extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Map</div>;
+  }
+}
+
+SearchMap.propTypes = {};
+
+export default SearchMap;
