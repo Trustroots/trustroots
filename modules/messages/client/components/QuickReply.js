14945f62738f002a24cd7df67e061d263e08c124
Nick Sellen <git@nicksellen.co.uk>
2020-04-03 13:57:31 +0200

    Don't use translation function with dynamic text

--- modules/messages/client/components/QuickReply.js
+++ modules/messages/client/components/QuickReply.js
@@ -2,23 +2,33 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-const replies = [
-  {
-    host: true,
-    content: 'Yes, I can host!',
-  },
-  {
-    host: false,
-    content: "Sorry I can't host",
-  },
-  {
-    write: true,
-    content: 'Write back',
-  },
-];
-
 export default function QuickReply({ onSend, onFocus }) {
   const { t } = useTranslation('messages');
+
+  // It's not quite clear to me how to handle the i18n of
+  // these messages. They are a bit of a hack to start with.
+  // They rely on the "data-hosting" attribute being set on the
+  // p element, but it still has message content that will not
+  // be translated later, it's stuck at it's original content.
+  // I chose to write the message content in the language the
+  // user has set.
+  // Probably better would be not to include any message content,
+  // but to handle "special" messages properly.
+  const replies = [
+    {
+      host: true,
+      content: t('Yes, I can host!'),
+    },
+    {
+      host: false,
+      content: t("Sorry I can't host"),
+    },
+    {
+      write: true,
+      content: t('Write back'),
+    },
+  ];
+
   return (
     <div
       className="btn-toolbar"
@@ -34,9 +44,6 @@ export default function QuickReply({ onSend, onFocus }) {
           if (write) {
             onFocus();
           } else {
-            // we write the english content into the message
-            // potentially we auto-translate those "special"
-            // messages at some point
             onSend(`
               <p data-hosting="${host ? 'yes' : 'no'}">
                 <b><i>${content}</i></b>
@@ -51,7 +58,7 @@ export default function QuickReply({ onSend, onFocus }) {
             className={`btn btn-sm ${className}`}
             onClick={onClick}
           >
-            {t(content)}
+            {content}
           </button>
         );
       })}
