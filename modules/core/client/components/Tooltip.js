0c1df54f33840b42e06e2e05756fef0729f8a2a0
mrkvon <mrkvon@users.noreply.github.com>
2020-04-07 01:06:44 +0200

    move docs from README to the component itself

--- modules/core/client/components/Tooltip.js
+++ modules/core/client/components/Tooltip.js
@@ -3,7 +3,16 @@ import PropTypes from 'prop-types';
 import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 
 /**
- * A component that adds tooltip to its children
+ * Tooltip component adds a tooltip to its children. It appears on hover.
+ * Usage: `<Tooltip {...props><WrappedComponent /></Tooltip>`
+ *
+ * @param {string} tooltip - the text to display as tooltip
+ *                           ( in fact it could be any kind of React.node)
+ * @param {left|top|right|bottom} [placement=top] - placement of the tooltip
+ * @param {boolean} [hidden=false] - when set to true, the tooltip is not rendered
+ * @param {string} [id=hover-tooltip] - id is required for accessibility
+ *                                      setting your own will avoid id duplicates
+ * @param {React.node} children - normally, pass this as Tooltip children, not in props
  */
 export default function HoverTooltip({
   children,
