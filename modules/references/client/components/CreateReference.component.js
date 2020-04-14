0ae3f6902e7de1577e14c50322ee2e77b8e4fdd1
Dario Hornsteiner <dario@hornsteiner.de>
2020-04-14 16:14:09 +0200

    replace bsStyle with variant

--- modules/references/client/components/CreateReference.component.js
+++ modules/references/client/components/CreateReference.component.js
@@ -146,7 +146,7 @@ export default function CreateReference({ userFrom, userTo }) {
     <div>
       <Tabs
         activeKey={step}
-        bsStyle="pills"
+        variant="pills"
         onSelect={() => {}}
         id="create-reference-tabs"
       >
