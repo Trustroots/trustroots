36f99fd7e07cb0cfe3d3a82cbadb04bb0528a430
Nick Sellen <git@nicksellen.co.uk>
2020-04-16 11:26:00 +0200

    Use angular $timeout over setTimeout
    Maybe ensures angular is initialized by then

--- modules/messages/client/config/messages.client.run.js
+++ modules/messages/client/config/messages.client.run.js
@@ -2,9 +2,13 @@ import { enable as enableUnreadMessageCountPolling } from '@/modules/messages/cl
 import { enable as enableVisibilityWatching } from '@/modules/messages/client/services/visibility.client.service';
 import { enable as enableFaviconUpdater } from '@/modules/messages/client/services/messages-count-favicon-updater.client.service';
 
-angular.module('messages').run(() => {
+angular.module('messages').run(messagesRun);
+
+/* @ngInject */
+function messagesRun($timeout) {
   enableVisibilityWatching();
   enableFaviconUpdater();
-  // Without setImmediate the stuff in angular-compat cannot load as it's too early
-  setTimeout(enableUnreadMessageCountPolling, 0);
-});
+  // This needs to be delayed just a little bit until angular is initialized
+  // or stuff in angular-compat can't be used
+  $timeout(enableUnreadMessageCountPolling);
+}
