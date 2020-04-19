9f5d4bc0cc94fbd9bf1eed0ccaea5a53a68bdb8c
Nick Sellen <git@nicksellen.co.uk>
2020-04-19 15:29:56 +0200

    Add references public feedback step UI

---
+++ modules/references/client/components/create-reference/Feedback.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+export default function Feedback({ feedback, onChangeFeedback }) {
+  const { t } = useTranslation('references');
+
+  return (
+    <div className="panel panel-default">
+      <div className="panel-heading">
+        <h4>{t('Would you like to describe something about them?')}</h4>
+      </div>
+      <div className="panel-body">
+        <label htmlFor="feedback-message" className="control-label">
+          {t('Public feedback')}
+        </label>
+        <textarea
+          className="form-control input-lg"
+          rows="7"
+          id="feedback-message"
+          onChange={event => onChangeFeedback(event.target.value)}
+          value={feedback}
+        ></textarea>
+        <span className="help-block">
+          {t('Were they were they great at cooking? or singing?')}
+          <br />
+        </span>
+      </div>
+    </div>
+  );
+}
+
+Feedback.propTypes = {
+  feedback: PropTypes.string.isRequired,
+  onChangeFeedback: PropTypes.func.isRequired,
+};
