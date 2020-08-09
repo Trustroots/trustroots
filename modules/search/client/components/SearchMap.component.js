0684bba8860c357f7d7be8cd677620fe742ee765
Mikael Korpela <mikael@ihminen.org>
2020-08-09 17:14:46 +0300

    Update SearchMap.component.js

--- modules/search/client/components/SearchMap.component.js
+++ modules/search/client/components/SearchMap.component.js
@@ -3,17 +3,17 @@
 import { Source, Layer, WebMercatorViewport } from 'react-map-gl';
 import { useDebouncedCallback } from 'use-debounce';
 import PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'; //eslint-disable-line
 
 // Internal dependencies
-import * as api from '../api/offers.api';
-import Map from '@/modules/core/client/components/Map/index';
 import './search-map.less';
 import {
   clusterLayer,
   clusterCountLayer,
   unclusteredPointLayer,
 } from './layers';
+import * as api from '../api/offers.api';
+import Map from '@/modules/core/client/components/Map/index';
 import SearchMapLoading from './SearchMapLoading';
 
 // export default class SearchMap extends Component {
@@ -24,12 +24,14 @@ export default function SearchMap(props) {
   const sourceRef = React.createRef();
   const { location } = props;
 
-  const [geojson, setGeojson] = useState({});
+  console.log('location:', location); //eslint-disable-line
+
+  const [geojson, setGeojson] = useState(null); //eslint-disable-line
 
   // https://github.com/visgl/react-map-gl/blob/5.2-release/examples/zoom-to-bounds/src/app.js
   const onClickMap = event => {
     const mapboxSource = sourceRef.current.getSource();
-    console.log(event); //eslint-disable-line
+    console.log(event, mapboxSource); //eslint-disable-line
 
     if (!event.features?.length) {
       return;
@@ -61,31 +63,41 @@ export default function SearchMap(props) {
 
   const [debouncedUpdateOffers] = useDebouncedCallback(
     ({ viewState }) => {
+      console.log('useDebouncedCallback'); //eslint-disable-line
       // Build bounding box
+      // @TODO: better way to get bounds? Where's the utility function for it?
       const mercatorViewport = new WebMercatorViewport(viewState);
-      const p1 = mercatorViewport.unproject([viewState.width, 0]);
-      const p2 = mercatorViewport.unproject([0, viewState.height]);
+      const northEast = mercatorViewport.unproject([viewState.width, 0]);
+      const southWest = mercatorViewport.unproject([0, viewState.height]);
 
-      console.log('->update:', viewState, mercatorViewport, p1, p2); //eslint-disable-line
-
+      /*
       setGeojson({
         type: 'FeatureCollection',
         features: [
           {
             type: 'Feature',
             geometry: {
-              type: 'Polygon',
-              coordinates: [[p1, p2]],
+              type: 'Point',
+              coordinates: northEast,
+            },
+          },
+          {
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: southWest,
             },
           },
         ],
       });
+      */
 
+      // @TODO: no need to fetch if in same area as in previous fetch
       fetchOffers({
-        northEastLat: p1[0],
-        northEastLng: p1[1],
-        southWestLat: p2[0],
-        southWestLng: p2[1],
+        northEastLat: northEast[1],
+        northEastLng: northEast[0],
+        southWestLat: southWest[1],
+        southWestLng: southWest[0],
       });
     },
     // delay in ms
@@ -95,26 +107,31 @@ export default function SearchMap(props) {
   );
 
   async function fetchOffers(query) {
+    console.log('fetchOffers:', query); //eslint-disable-line
     setIsFetching(true);
     try {
       //   filters=%7B%22tribes%22:%5B%5D,%22types%22:%5B%22host%22,%22meet%22%5D,%22languages%22:%5B%5D,%22seen%22:%7B%22months%22:6%7D%7D&northEastLat=54.879278856608266&northEastLng=20.10172526041667&southWestLat=42.05680822944813&southWestLng=-1.8204752604166667
       // @TODO: filters
       // @TODO: cancellation when need to re-fetch
       const data = await api.getOffers(query);
-      setOffers(data);
+      if (data.features.length) {
+        setOffers(data);
+      }
     } finally {
       setIsFetching(false);
     }
   }
 
+  /*
   useEffect(() => {
     fetchOffers({
-      northEastLat: 54.879278856608266,
-      northEastLng: 20.10172526041667,
-      southWestLat: 42.05680822944813,
-      southWestLng: -1.8204752604166667,
+      southWestLng: 5.6465709209,
+      northEastLng: 6.6291606426,
+      northEastLat: 50.2054799638,
+      southWestLat: 49.3366519447,
     });
   }, []);
+  */
 
   return (
     <Map
@@ -143,13 +160,21 @@ export default function SearchMap(props) {
         <Layer {...clusterCountLayer} />
         <Layer {...unclusteredPointLayer} />
       </Source>
-      <Source id="example" type="geojson" data={geojson}>
-        <Layer type="fill" paint={{ 'fill-color': '#007cbf' }} />
-      </Source>
+      {geojson && (
+        <Source id="example" type="geojson" data={geojson}>
+          <Layer
+            type="circle"
+            paint={{
+              'circle-color': '#f1f075', // #f28cb1
+              'circle-radius': 20,
+            }}
+          />
+        </Source>
+      )}
     </Map>
   );
 }
 
 SearchMap.propTypes = {
-  location: PropTypes.array,
+  location: PropTypes.object,
 };
