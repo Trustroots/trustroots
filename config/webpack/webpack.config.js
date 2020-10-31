9343d9aefc363f71333ab12b3e0c6bbf95beb3cb
Mikael Korpela <mikael@ihminen.org>
2020-10-31 20:20:02 +0200

    Fix correct options object

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -33,7 +33,10 @@ const styleLoaders = [
   {
     loader: 'postcss-loader',
     options: {
-      plugins: [require('autoprefixer')],
+      postcssOptions: {
+        plugins: [require('autoprefixer')],
+      },
+      sourceMap: isDevelopment,
     },
   },
 ];
