93c571f0da796a953fd3b34c09e57989e3c585d1
Mikael Korpela <mikael@ihminen.org>
2020-12-06 00:13:39 +0200

    Update jest.setup.js

--- jest/jest.setup.js
+++ jest/jest.setup.js
@@ -1,5 +1,6 @@
 global.L = require('leaflet');
 global.jQuery = require('jquery');
+require('angular');
 require('angular-mocks');
 require('@/modules/core/client/app/init');
 require('@/modules/core/client/core.client.module');
