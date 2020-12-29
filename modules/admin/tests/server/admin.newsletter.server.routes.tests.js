3c8c8761687e2f62f18c53af5fc65b78bdccbc2d
Mikael Korpela <mikael@ihminen.org>
2020-12-29 21:11:39 +0200

    Simplify test file structure

--- modules/admin/tests/server/admin.newsletter.server.routes.tests.js
+++ modules/admin/tests/server/admin.newsletter.server.routes.tests.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const express = require(path.resolve('./config/lib/express'));
 const utils = require(path.resolve('./testutils/server/data.server.testutil'));
 
-describe('Admin Audit Log CRUD tests', () => {
+describe('Admin Newsletter subscribers CRUD tests', () => {
   // Get application
   const app = express.init(mongoose.connection);
   const agent = request.agent(app);
@@ -43,33 +43,31 @@ describe('Admin Audit Log CRUD tests', () => {
 
   after(utils.clearDatabase);
 
-  describe('Newsletter subscribers', () => {
-    it('non-authenticated users should not be allowed to read newsletter subscribers', async () => {
-      await agent.get('/api/admin/newsletter-subscribers').expect(403);
-    });
+  it('non-authenticated users should not be allowed to read newsletter subscribers', async () => {
+    await agent.get('/api/admin/newsletter-subscribers').expect(403);
+  });
 
-    it('non-admin users should not be allowed to read newsletter subscribers', async () => {
-      await utils.signIn(nonAdminAuth, agent);
-      agent.get('/api/admin/newsletter-subscribers').expect(403);
-      await utils.signOut(agent);
-    });
+  it('non-admin users should not be allowed to read newsletter subscribers', async () => {
+    await utils.signIn(nonAdminAuth, agent);
+    agent.get('/api/admin/newsletter-subscribers').expect(403);
+    await utils.signOut(agent);
+  });
 
-    it('admin users should be allowed to read newsletter subscribers', async () => {
-      await utils.signIn(adminAuth, agent);
+  it('admin users should be allowed to read newsletter subscribers', async () => {
+    await utils.signIn(adminAuth, agent);
 
-      const { type, text } = await agent
-        .get('/api/admin/newsletter-subscribers')
-        .expect(200);
+    const { type, text } = await agent
+      .get('/api/admin/newsletter-subscribers')
+      .expect(200);
 
-      type.should.equal('text/csv');
+    type.should.equal('text/csv');
 
-      const lines = text.split('\n');
-      lines.length.should.equal(3);
-      lines[0].should.equal('Email Address,First Name,Last Name');
-      lines[1].should.equal(`${users[2].email},First oname,Last name`);
-      lines[2].should.equal(`${users[3].email},First name,Last name`);
+    const lines = text.split('\n');
+    lines.length.should.equal(3);
+    lines[0].should.equal('Email Address,First Name,Last Name');
+    lines[1].should.equal(`${users[2].email},First oname,Last name`);
+    lines[2].should.equal(`${users[3].email},First name,Last name`);
 
-      await utils.signOut(agent);
-    });
+    await utils.signOut(agent);
   });
 });
