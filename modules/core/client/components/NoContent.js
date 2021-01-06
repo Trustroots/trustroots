dabdddaf5a1020ed78355cd1aa3db5c5fa14bcab
Mikael Korpela <mikael@ihminen.org>
2021-01-06 18:43:54 +0200

    Experiences: update copy for "no references" (#2009)

--- modules/core/client/components/NoContent.js
+++ modules/core/client/components/NoContent.js
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function NoContent({ icon, message }) {
+export default function NoContent({ children = null, icon, message }) {
   return (
     <div className="row content-empty">
       {icon && <i className={`icon-3x icon-${icon}`}></i>}
       <h4>{message}</h4>
+      {children}
     </div>
   );
 }
 
 NoContent.propTypes = {
+  children: PropTypes.node,
   icon: PropTypes.string,
   message: PropTypes.string.isRequired,
 };
