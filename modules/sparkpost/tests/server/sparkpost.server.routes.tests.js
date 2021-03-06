7cc97bf65450857bb65154a1946719f3bf54579e
Mikael Korpela <mikael@ihminen.org>
2021-03-06 21:01:36 +0200

    Modernize route tests

--- modules/sparkpost/tests/server/sparkpost.server.routes.tests.js
+++ modules/sparkpost/tests/server/sparkpost.server.routes.tests.js
@@ -5,77 +5,57 @@ const express = require(path.resolve('./config/lib/express'));
 const config = require(path.resolve('./config/config'));
 
 /**
- * Globals
- */
-let app;
-let agent;
-let events;
-
-/**
  * Sparkpost routes tests
  */
-describe('Sparkpost CRUD tests', function () {
-  describe('Webhook endpoint', function () {
-    before(function (done) {
-      // Get application
-      app = express.init(mongoose.connection);
-      agent = request.agent(app);
+describe('Sparkpost CRUD tests', () => {
+  const app = express.init(mongoose.connection);
+  const agent = request.agent(app);
 
-      // Sparkpost webhook event example
-      events = [
-        {
-          msys: {
-            message_event: {
-              type: 'delivery',
-              campaign_id: 'example',
-              timestamp: '1454442600',
-              mailbox_provider: 'Gsuite',
-              reason: 'MAIL REFUSED - IP (a.b.c.d) is in black list',
-            },
-          },
+  // Sparkpost webhook event example
+  const events = [
+    {
+      msys: {
+        message_event: {
+          type: 'delivery',
+          campaign_id: 'example',
+          timestamp: '1454442600',
+          mailbox_provider: 'Gsuite',
+          reason: 'MAIL REFUSED - IP (a.b.c.d) is in black list',
         },
-      ];
+      },
+    },
+  ];
 
-      done();
+  describe('Webhook endpoint', () => {
+    it('Should not be able to request using GET method', async () => {
+      await agent.get('/api/sparkpost/webhook').expect(404);
     });
 
-    it('Should not be able to request using GET method', function (done) {
-      agent.get('/api/sparkpost/webhook').expect(404).end(done);
-    });
-
-    it('Should not be allowed to access endpoint without credentials', function (done) {
-      agent
+    it('Should not be allowed to access endpoint without credentials', async () => {
+      const { body, headers } = await agent
         .post('/api/sparkpost/webhook')
-        .expect(401)
-        .end(function (err, res) {
-          res.headers['www-authenticate'].should.equal(
-            'Basic realm="Knock Knock"',
-          );
-          res.body.message.should.equal('Access denied');
+        .expect(401);
 
-          // Call the assertion callback
-          return done(err);
-        });
+      headers['www-authenticate'].should.equal('Basic realm="Knock Knock"');
+      body.message.should.equal('Access denied');
     });
 
-    it('Should not be allowed to access endpoint with wrong credentials', function (done) {
-      agent
+    it('Should not be allowed to access endpoint with wrong credentials', async () => {
+      await agent
         .post('/api/sparkpost/webhook')
         .auth('wrong', 'wrong')
-        .expect(401)
-        .end(done);
+        .expect(401);
     });
 
-    it('Should be allowed to access endpoint with credentials', function (done) {
-      agent
+    it('Should be allowed to access endpoint with credentials', async () => {
+      await agent
         .post('/api/sparkpost/webhook')
         .auth(
           config.sparkpostWebhook.username,
           config.sparkpostWebhook.password,
         )
         .send(events)
-        .expect(200)
-        .end(done);
+        .expect(200);
     });
   });
 });
