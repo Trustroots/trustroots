19f79c4eee1ad82f80d7b49c8a8440489ad20a7b
Mikael Korpela <mikael@ihminen.org>
2021-03-07 06:06:49 +0200

    Improved language picking

--- modules/core/tests/client/components/LanguageSwitch.component.tests.js
+++ modules/core/tests/client/components/LanguageSwitch.component.tests.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { getLocales } from '@/modules/core/client/utils/locales';
 import i18n from '@/config/client/i18n';
-import locales from '@/config/shared/locales';
 import LanguageSwitch from '@/modules/core/client/components/LanguageSwitch.component';
-
 import * as users from '@/modules/users/client/api/users.api';
+
 const api = { users };
 
+const locales = getLocales();
 const localeNames = locales.map(locale => locale.label);
 
 jest.mock('@/modules/users/client/api/users.api');
