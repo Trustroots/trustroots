cb73229a340a7f302ab83ff797b277c5739e440b
Mikael Korpela <mikael@ihminen.org>
2021-04-25 11:12:51 +0300

    Add unblocking and informational banner

---
+++ modules/users/client/components/BlockMember.component.js
@@ -0,0 +1,92 @@
+import { useTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import '@/config/client/i18n';
+import * as api from '../api/block.api';
+
+export default function BlockMember({ username, isBlocked, className }) {
+  const { t } = useTranslation('users');
+
+  function refreshUser() {
+    // Refresh the page to reset global `window.app.user`
+    // Once we have global state handler and whole user profile is in React, we can remove this.
+    window.location.reload();
+  }
+
+  // Unblock member
+  async function handleUnBlockMember(username) {
+    const confirmation = window.confirm(
+      t(
+        'Are you sure you want to unblock them? They will be able to see your profile and messaage you again.',
+      ),
+    );
+
+    if (confirmation) {
+      const unblock = await api.unblock(username);
+      console.log('unblock:', unblock); //eslint-disable-line
+
+      if (unblock) {
+        refreshUser();
+      } else {
+        window.alert(
+          `${t('Could not unblock this member.')}\n\n${t(
+            'Please ensure you are connected to internet and try again.',
+          )}`,
+        );
+      }
+    }
+  }
+
+  // Block member
+  async function handleBlockMember(username) {
+    const confirmation = window.confirm(
+      t(
+        'Are you sure you want to block them? They will not be able to see or message you.',
+      ),
+    );
+
+    if (confirmation) {
+      const block = await api.block(username);
+      console.log('blocked:', block); //eslint-disable-line
+
+      if (block) {
+        refreshUser();
+      } else {
+        alert(
+          `${t('Could not block this member.')}\n\n${t(
+            'Please ensure you are connected to internet and try again.',
+          )}`,
+        );
+      }
+    }
+  }
+
+  return (
+    <>
+      {isBlocked ? (
+        <button
+          aria-label={t('Unblock member "{{username}}"', { username })}
+          className={className}
+          onClick={() => handleUnBlockMember(username)}
+        >
+          {t('Unblock member')}
+        </button>
+      ) : (
+        <button
+          aria-label={t('Block member "{{username}}"', { username })}
+          className={className}
+          onClick={() => handleBlockMember(username)}
+        >
+          {t('Block member')}
+        </button>
+      )}
+    </>
+  );
+}
+
+BlockMember.propTypes = {
+  isBlocked: PropTypes.bool,
+  className: PropTypes.string,
+  username: PropTypes.string.isRequired,
+};
