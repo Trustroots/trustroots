4cd64eac1e81abfa4f1a9d0720b6200523237bc3
Mikael Korpela <mikael@ihminen.org>
2021-05-23 22:47:09 +0300

    Rewrite Jest html transform

--- jest/jest.transform.html.js
+++ jest/jest.transform.html.js
@@ -1,24 +1,18 @@
-/*
- * This is a bit of a hack, using webpack loaders to transform templates in jest.
- */
-
-/*
-const templateLoader = require('../config/webpack/templateloader');
-const htmlLoader = require('html-loader');
-
-module.exports = {
-  process(src, filename) {
-    return templateLoader.call(
-      { resourcePath: filename },
-      htmlLoader.call({ resourcePath: filename }, src),
-    );
-  },
-};
-*/
+const fs = require('fs');
 
 module.exports = {
   // eslint-disable-next-line no-unused-vars
   process(src, filename, config, options) {
-    return filename;
+    const html = fs.readFileSync(filename, { encoding: 'utf-8' });
+
+    // Originally from Webpack html-loader
+    // https://github.com/webpack-contrib/html-loader/blob/39e496670defcf0e0f6da9de91e91fe953f67d91/src/utils.js#L1204-L1208
+    const code = JSON.stringify(html)
+      // Invalid in JavaScript but valid HTML
+      .replace(/[\u2028\u2029]/g, str =>
+        str === '\u2029' ? '\\u2029' : '\\u2028',
+      );
+
+    return { code };
   },
 };
