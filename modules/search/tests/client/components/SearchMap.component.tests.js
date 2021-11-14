68cfe4858242f6a8c17beda888aef64be86f988e
Mikael Korpela <mikael@ihminen.org>
2021-11-14 19:36:48 +0200

    Clean out test

--- modules/search/tests/client/components/SearchMap.component.tests.js
+++ modules/search/tests/client/components/SearchMap.component.tests.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import '@/config/client/i18n';
 
 import SearchMap from '@/modules/search/client/components/SearchMap.component';
-// import * as tribesApi from '@/modules/tribes/client/api/tribes.api';
-
-// jest.mock('@/modules/tribes/client/api/tribes.api');
-// jest.mock('@/modules/core/client/services/angular-compat');
 
 describe('Search', () => {
   it('Map loads', async () => {
