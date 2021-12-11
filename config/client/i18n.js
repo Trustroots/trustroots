b6fd09c820bc5c8fa60bda63bac6b6362a0e0795
Mikael Korpela <mikael@ihminen.org>
2021-12-11 18:37:03 +0200

    i18n: migrate from i18next-xhr-backend to i18next-http-backend (#2489)

--- config/client/i18n.js
+++ config/client/i18n.js
@@ -1,5 +1,5 @@
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-xhr-backend';
+import HttpApi from 'i18next-http-backend';
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import moment from 'moment';
@@ -74,9 +74,9 @@ function format(value, format, languageCode) {
 }
 
 if (!isTest) {
-  // load translation using xhr -> see /public/locales
-  // learn more: https://github.com/i18next/i18next-xhr-backend
-  i18n.use(Backend);
+  // load translation using Fetch -> see /public/locales
+  // learn more: https://github.com/i18next/i18next-http-backend
+  i18n.use(HttpApi);
 }
 
 i18n
