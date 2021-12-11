07f6896b0fcec01824351183f256e1249a96fb84
Alex Kupin <interstates21@gmail.com>
2021-12-11 16:10:36 +0100

    Offer Types

--- app/src/AppRouter.tsx
+++ app/src/AppRouter.tsx
@@ -2,8 +2,7 @@
 import SearchPage from "./pages/Search";
 import { Routes, Route } from "react-router-dom";
 import HomeLayout from './HomeLayout'
-
-
+import Profile from './pages/Profile'
 
 
 const Router = () => { 
@@ -12,16 +11,12 @@ const Router = () => {
      <Route element={<HomeLayout />}>
         {/* <Route index element={< />} /> */}
         <Route path="search" element={<SearchPage />}>
-        {/* <Route path=":teamId" element={<Team />} /> */}
-        {/* <Route path=":teamId/edit" element={<EditTeam />} /> */}
-        {/* <Route path="new" element={<NewTeamForm />} /> */}
+
+        <Route path="profile">
+          <Route path=":userId" element={<Profile />} />
         </Route>
-    </Route>
-    {/* <Route element={<PageLayout />}>
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/tos" element={<Tos />} />
-    </Route> */}
-    {/* <Route path="contact-us" element={<Contact />} /> */}
+        </Route>
+      </Route>
     </Routes>
   );
 }
