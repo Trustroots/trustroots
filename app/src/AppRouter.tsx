e201038fe18e3ea25c45bd28106dcf2efc243fb4
Alex Kupin <interstates21@gmail.com>
2021-12-11 16:33:31 +0100

    Add not found page

--- app/src/AppRouter.tsx
+++ app/src/AppRouter.tsx
@@ -3,19 +3,19 @@ import SearchPage from "./pages/Search";
 import { Routes, Route } from "react-router-dom";
 import HomeLayout from './HomeLayout'
 import Profile from './pages/Profile'
-
+import Page404 from './pages/Page404'
 
 const Router = () => { 
   return (
     <Routes>
      <Route element={<HomeLayout />}>
-        {/* <Route index element={< />} /> */}
         <Route path="search" element={<SearchPage />}>
         <Route path="profile">
           <Route path=":userId" element={<Profile />} />
         </Route>
         </Route>
       </Route>
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 }
---
+++ app/src/pages/Page404/index.tsx
@@ -0,0 +1,14 @@
+import {FC} from 'react'
+import styles from './.module.scss';
+
+interface Page404Props {
+
+}
+
+const Page404: FC<Page404Props> = () => {
+        return (
+<div>Not found</div>
+);
+}
+
+export default Page404;
