0bddbcdf150563fa6dc3db7bea06ba3715c61b6f
Callum Macdonald <git@callum-macdonald.com>
2022-09-24 16:28:24 +0200

    Automate docker build and tagging.

---
+++ deploy/docker/dockerBuild.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+STATUS=$(git status --porcelain | grep -c '^')
+
+if [[ "$STATUS" != "0" ]]
+then
+  echo "Cowardly refusing to build with a dirty git"
+  exit 1
+fi
+
+# Get the current commit hash of the repository
+COMMIT=$(git rev-parse --short HEAD)
+
+OS=$(uname -s)
+
+# Switch to the repository root directory
+cd ../..
+
+if [ "$OS" = "Darwin" ]; then
+  docker build -f ./production.Dockerfile . -t trustrootsops/trustroots:latest -t "trustrootsops/trustroots:git-${COMMIT}"
+else
+  sudo docker build -f ./production.Dockerfile . -t trustrootsops/trustroots:latest -t "trustrootsops/trustroots:git-${COMMIT}"
+fi
