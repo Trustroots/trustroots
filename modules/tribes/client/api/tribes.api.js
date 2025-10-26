e19589a34d825fa8f190d3ee34337f8678dc6ab1
guaka <guaka@guaka.org>
2025-10-26 23:51:31 +0000

    Update tribes.api.js
    tribe limit 150

--- modules/tribes/client/api/tribes.api.js
+++ modules/tribes/client/api/tribes.api.js
@@ -14,7 +14,7 @@ export async function leave(tribeId) {
  * @param {Number} limit
  * @param {String} sortBy Values count|alphabetically. Sort either by count or by alphabetical label order; defaults to count.
  */
-export async function read({ limit = 50 } = {}) {
+export async function read({ limit = 150 } = {}) {
   const { data } = await axios.get('/api/tribes', { params: { limit } });
   return data;
 }
