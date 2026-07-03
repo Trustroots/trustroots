Trustroots/trustroots

commit c7963f775d2dda01acd7bfdf482ecf0ed4196195

Allow users change their usernames every 3 months (#606)

modules/users/server/controllers/users.profile.server.controller.js
@@ -29,6 +29,7 @@ var _ = require('lodash'),
     fs = require('fs'),
     os = require('os'),
     mmmagic = require('mmmagic'),
+    moment = require('moment'),
     multerConfig = require(path.resolve('./config/lib/multer')),
     User = mongoose.model('User');
 
@@ -288,7 +289,7 @@ exports.avatarUpload = function (req, res) {
 };
 
 /**
- * Update
+ * Update user profile
  */
 exports.update = function (req, res) {
 
@@ -300,37 +301,36 @@ exports.update = function (req, res) {
 
   async.waterfall([
 
-    // If user is changing email, check if it's available
     function (done) {
-
-      // Check if email changed and proceed with extra checks if so
-      if (req.body.email && req.body.email !== req.user.email) {
-        User.findOne({
-          $or: [
-            { emailTemporary: req.body.email.toLowerCase() },
-            { email: req.body.email.toLowerCase() }
-          ]
-        }, 'emailTemporary email', function (err, emailUser) {
-          // Not free
-          if (emailUser) {
-            // If the user we found with this email is currently authenticated user, let user pass to resend confirmation email
-            if (emailUser._id.equals(req.user._id)) {
-              done(null);
-            } else {
-              // Otherwise it was someone else's email. Block the way.
-              return res.status(403).send({
-                message: 'This email is already in use. Please use another one.'
-              });
-            }
-          } else {
-            // Free, proceed generating the token
-            done(null);
-          }
-        });
-      } else {
-        // Email didn't change, just continue
-        done(null);
+      // Email didn't change, just continue
+      if (!req.body.email || req.body.email === req.user.email) {
+        return done();
       }
+
+      // User is changing email, check if it's available
+      User.findOne({
+        $or: [
+          { emailTemporary: req.body.email.toLowerCase() },
+          { email: req.body.email.toLowerCase() }
+        ]
+      }, 'emailTemporary email', function (err, emailUser) {
+        // Not available
+        if (emailUser) {
+          // If the user we found with this email is currently authenticated user,
+          // let user pass to resend confirmation email
+          if (emailUser._id.equals(req.user._id)) {
+            done();
+          } else {
+            // Otherwise it was someone else's email. Block the way.
+            return res.status(403).send({
+              message: 'This email is already in use. Please use another one.'
+            });
+          }
+        } else {
+          // Email available, proceed generating the token
+          done();
+        }
+      });
     },
 
     // Check if we should generate new email token
@@ -346,6 +346,23 @@ exports.update = function (req, res) {
         // Email didn't change, just continue
         done(null, false, false);
       }
+    },
+
+    // User wants to change the username
+    function (token, email, done) {
+      if (req.body.username && req.body.username !== req.user.username) {
+        // They are not allowed to do so
+        if (!isUsernameUpdateAllowed(req.user)) {
+          return res.status(403).send({
+            message: 'You cannot change your username at this time.'
+          });
+        }
+
+        // Mark username updated
+        req.user.usernameUpdated = new Date();
+      }
+
+      done(null, token, email);
     },
 
     // Update user
@@ -367,8 +384,8 @@ exports.update = function (req, res) {
       delete req.body.emailToken;
       delete req.body.emailTemporary;
       delete req.body.provider;
-      delete req.body.username;
       delete req.body.displayUsername;
+      delete req.body.usernameUpdated;
       delete req.body.salt;
       delete req.body.password;
       delete req.body.resetPasswordToken;
@@ -421,7 +438,7 @@ exports.update = function (req, res) {
 
     // Return user
     function (user) {
-      user = exports.sanitizeProfile(user);
+      user = exports.sanitizeProfile(user, req.user);
       return res.json(user);
     }
 
@@ -849,6 +866,16 @@ exports.userByUsername = function (req, res, next, username) {
 
 };
 
+/**
+ * Has 3 months passed since user changed their username OR signed up?
+ *
+ * @return {Bool}
+ */
+function isUsernameUpdateAllowed(user) {
+  var allowedDate = moment(user.usernameUpdated || user.created)
+    .add(3, 'months');
+  return moment().isSameOrAfter(allowedDate);
+};
 
 /**
  * Sanitize profile before sending it to frontend
@@ -891,10 +918,15 @@ exports.sanitizeProfile = function (profile, authenticatedUser) {
     });
   }
 
-  // Profile does not belong to currently authenticated user
-  // Remove data we don't need from other member's profile
-  if (!authenticatedUser || !authenticatedUser._id.equals(profile._id)) {
+  // Profile belongs to currently authenticated user
+  if (authenticatedUser && authenticatedUser._id.equals(profile._id)) {
+    // Is user allowed to update their username?
+    profile.usernameUpdateAllowed = isUsernameUpdateAllowed(profile);
+  } else {
+    // Remove data we don't need from other member's profile
+    delete profile.updated;
     delete profile.passwordUpdated;
+    delete profile.usernameUpdated;
   }
 
   // This info totally shouldn't be at the frontend

modules/users/client/controllers/profile-edit-account.client.controller.js
@@ -13,12 +13,14 @@
     var vm = this;
 
     // Exposed
+    vm.updateUsername = updateUsername;
     vm.updateUserEmail = updateUserEmail;
     vm.resendUserEmailConfirm = resendUserEmailConfirm;
     vm.updateUserSubscriptions = updateUserSubscriptions;
     vm.updatingUserSubscriptions = false;
     vm.changeUserPassword = changeUserPassword;
     vm.user = Authentication.user;
+    vm.getUsernameValidationError = getUsernameValidationError;
 
     // Related to profile removal
     vm.removeProfileConfirm = false;
@@ -40,6 +42,41 @@
     vm.isNativeMobileApp = false;
 
     activate();
+
+    /**
+     * Parse $error and return a string
+     * @param {Object} usernameModel - Angular model for username form input
+     * @returns {String} error text
+     */
+    function getUsernameValidationError(usernameModel) {
+      if (!usernameModel || !usernameModel.$dirty || usernameModel.$valid) {
+        return '';
+      }
+
+      var err = usernameModel.$error || {};
+
+      if (err.required || usernameModel.$usernameValue === '') {
+        return 'Username is required.';
+      }
+
+      if (err.maxlength) {
+        return 'Too long, maximum length is 34 characters.';
+      }
+
+      if (err.minlength) {
+        return 'Too short, minumum length is 3 characters.';
+      }
+
+      if (err.pattern) {
+        return 'Invalid username.';
+      }
+
+      if (err.username) {
+        return 'This username is already in use or invalid.';
+      }
+
+      return 'Invalid username.';
+    }
 
     // Activate controller
     function activate() {
@@ -68,11 +105,30 @@
     }
 
     /**
+     * Change username
+     */
+    function updateUsername() {
+      vm.usernameSuccess = vm.usernameError = null;
+      var user = new Users(Authentication.user);
+      /* Just in case the user has changed the e-mail input */
+      delete user.email;
+
+      user.$update(function (response) {
+        messageCenterService.add('success', 'Username updated.');
+        vm.usernameSuccess = '';
+        vm.user = Authentication.user = response;
+      }, function (response) {
+        vm.usernameError = (response.data && response.data.message) || 'Something went wrong';
+      });
+    }
+    /**
      * Change user email
      */
     function updateUserEmail() {
       vm.emailSuccess = vm.emailError = null;
       var user = new Users(Authentication.user);
+      /* Just in case the user has changed the username input */
+      delete user.username;
 
       user.$update(function (response) {
         messageCenterService.add('success', 'Check your email for further instructions.');

modules/users/client/directives/tr-validate-username.client.directive.js
@@ -3,6 +3,16 @@
 
   /**
    * Directive to validate usernames (and check for availability)
+   *
+   * Usage:
+   * ```
+   * <input tr-validate-username>
+   * ```
+   *
+   * Or to avoid validating specific string (like user's previous username):
+   * ```
+   * <input tr-validate-username="{{::username}}">
+   * ```
    */
   angular
     .module('users')
@@ -17,13 +27,13 @@
       restrict: 'A',
       require: 'ngModel',
       link: function (scope, elem, attr, ngModel) {
-
         var minlength = angular.isDefined(attr.minlength) ? attr.minlength : 1;
 
         ngModel.$asyncValidators.username = function (modelValue) {
           return $q(function (resolve, reject) {
 
             ngModel.$setValidity('username', true);
+
             if (modelValue && modelValue.length >= minlength) {
               if (delayedUsernameValidation) {
                 $timeout.cancel(delayedUsernameValidation);
@@ -31,6 +41,12 @@
 
               delayedUsernameValidation = $timeout(function () {
                 delayedUsernameValidation = false;
+
+                // If current value is the same as initial input value, don't do anything
+                if (attr.trValidateUsername && modelValue === attr.trValidateUsername) {
+                  return resolve();
+                }
+
                 SignupValidation
                   .post({ username: modelValue })
                   .$promise

modules/users/client/views/profile/profile-edit-account.client.view.html
@@ -1,4 +1,3 @@
-
 <!-- Email -->
 <div class="panel panel-default">
   <div class="panel-heading">
@@ -181,6 +180,62 @@
   </div>
 </div>
 <!-- /Password -->
+
+<!-- Username -->
+<div class="panel panel-default">
+  <div class="panel-heading">
+    Username
+  </div>
+  <div class="panel-body">
+    <form ng-submit="profileEditAccount.updateUsername()" autocomplete="off" name="settingsUsernameForm">
+      <div class="form-horizontal">
+        <div class="form-group"
+             ng-class="{
+               'has-error': (settingsUsernameForm.username.$invalid && settingsUsernameForm.username.$dirty) || profileEditAccount.usernameError
+             }">
+          <label for="username" class="col-sm-3 text-right control-label">Username</label>
+          <div class="col-sm-9">
+            <input type="text"
+                   id="username"
+                   name="username"
+                   class="form-control"
+                   required
+                   autocapitalize="off"
+                   ng-minlength="3"
+                   ng-maxlength="34"
+                   ng-pattern="/^(?=.*[0-9A-Za-z])[0-9A-Za-z.\-_]{3,34}$/"
+                   tr-validate-username="{{::profileEditAccount.user.username}}"
+                   tr-focustip="'Can contain only letters, numbers, periods, hyphens, and underscores.<br>3—34 characters long.'"
+                   tooltip-is-open="!settingsUsernameForm.username.$pending && settingsUsernameForm.username.$dirty && !settingsUsernameForm.username.$valid"
+                   uib-tooltip="{{profileEditAccount.getUsernameValidationError(settingsUsernameForm.username)}}"
+                   tooltip-popup-delay="500"
+                   tooltip-trigger="'none'"
+                   tooltip-placement="top"
+                   ng-disabled="!profileEditAccount.user.usernameUpdateAllowed"
+                   ng-model="profileEditAccount.user.username">
+            <p class="help-block" ng-show="!profileEditAccount.usernameError">
+              Changing username is limited to once in 3 months.
+            </p>
+            <p class="help-block" ng-show="profileEditAccount.usernameError" ng-bind="profileEditAccount.usernameError"></p>
+            <p class="help-block" ng-show="profileEditAccount.usernameSuccess" ng-bind="profileEditAccount.usernameSuccess"></p>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="col-sm-push-3 col-sm-9">
+            <button type="submit"
+                    class="btn btn-primary"
+                    ng-disabled="!profileEditAccount.user.usernameUpdateAllowed || settingsUsernameForm.username.$invalid">
+              Change username
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </form>
+  </div>
+</div>
+<!-- /Username -->
 
 <!-- Data liberation -->
 <div class="panel panel-default">

modules/users/server/models/user.server.model.js
@@ -170,6 +170,9 @@ var UserSchema = new Schema({
     lowercase: true, // Stops users creating case sensitive duplicate usernames with "username" and "USERname", via @link https://github.com/meanjs/mean/issues/147
     trim: true
   },
+  usernameUpdated: {
+    type: Date
+  },
   // Stores unaltered original username
   displayUsername: {
     type: String,

modules/users/tests/server/user-profile.server.routes.tests.js
@@ -4,6 +4,7 @@ var should = require('should'),
     request = require('supertest'),
     path = require('path'),
     mongoose = require('mongoose'),
+    moment = require('moment'),
     User = mongoose.model('User'),
     config = require(path.resolve('./config/config')),
     express = require(path.resolve('./config/lib/express'));
@@ -181,6 +182,7 @@ describe('User profile CRUD tests', function () {
                 return done(userInfoErr);
               }
 
+              // Second time changing it
               userInfoRes.body.should.be.instanceof(Object);
               userInfoRes.body.firstName.should.be.equal('user_update_first');
               userInfoRes.body.lastName.should.be.equal('user_update_last');
@@ -569,6 +571,161 @@ describe('User profile CRUD tests', function () {
             });
         });
     });
+  });
+
+  describe('Username Update', function () {
+    it('should not let a new user to update', function (done) {
+      agent.post('/api/auth/signin')
+        .send(credentials)
+        .expect(200)
+        .end(function (err) {
+          if (err) {
+            return done(err);
+          }
+          var user2 = _user;
+          user2.username = _user.username + '01';
+          delete user2.email;
+          agent.put('/api/users')
+            .send(user2)
+            .expect(403)
+            .end(function (err, res) {
+              if (err) {
+                return done(err);
+              }
+              res.body.message.should.equal('You cannot change your username at this time.');
+              return done();
+            });
+        });
+    });
+
+    it('should update it an user created 3 months ago', function (done) {
+      var threeMonthsAgo = moment(user.created)
+        .subtract(3, 'months')
+        .toDate();
+      user.update({ $set: { created: threeMonthsAgo } }, function (err) {
+        should.not.exist(err);
+        agent.post('/api/auth/signin')
+          .send(credentials)
+          .expect(200)
+          .end(function (err) {
+            if (err) {
+              return done(err);
+            }
+            var user2 = _user;
+            user2.username = _user.username + '01';
+            delete user2.email;
+            agent.put('/api/users')
+              .send(user2)
+              .expect(200)
+              .end(function (err, res) {
+                if (err) {
+                  return done(err);
+                }
+                res.body.username.should.equal(user2.username);
+                return done();
+              });
+          });
+      });
+    });
+
+    it('should fail to update if the three months, since last update, have not passed by',
+      function (done) {
+        var threeMonthsAgo = moment(user.created)
+          .subtract(3, 'months')
+          .toDate();
+        user.update({ $set: { created: threeMonthsAgo } }, function (err) {
+          should.not.exist(err);
+          agent.post('/api/auth/signin')
+            .send(credentials)
+            .expect(200)
+            .end(function (err) {
+              if (err) {
+                return done(err);
+              }
+              // First change
+              var user2 = _user;
+              user2.username = _user.username + '01';
+              delete user2.email;
+              agent.put('/api/users')
+                .send(user2)
+                .expect(200)
+                .end(function (err, res) {
+                  if (err) {
+                    return done(err);
+                  }
+                  res.body.username.should.equal(user2.username);
+                  // Second time changing it
+                  user2.username = _user.username + '02';
+                  agent.put('/api/users')
+                    .send(user2)
+                    .end(function (err, res) {
+                      if (err) {
+                        return done(err);
+                      }
+                      res.body.message.should.equal('You cannot change your username at this time.');
+                      return done();
+                    });
+                });
+            });
+        });
+      });
+
+    it('should update if the three months rule have passed by',
+      function (done) {
+        var threeMonthsAgo = moment(user.created)
+          .subtract(3, 'months')
+          .toDate();
+        user.update({ $set: { created: threeMonthsAgo } }, function (err) {
+          should.not.exist(err);
+          agent.post('/api/auth/signin')
+            .send(credentials)
+            .expect(200)
+            .end(function (err) {
+              if (err) {
+                return done(err);
+              }
+              // First change
+              var user2 = _user;
+              user2.username = _user.username + '01';
+              delete user2.email;
+              agent.put('/api/users')
+                .send(user2)
+                .expect(200)
+                .end(function (err, res) {
+                  if (err) {
+                    return done(err);
+                  }
+                  res.body.username.should.equal(user2.username);
+                  User.findById(user._id, function (err, user) {
+                    if (err) {
+                      return done(err);
+                    }
+                    var threeMonthsAgo = moment(user.usernameUpdated)
+                      .subtract(3, 'months')
+                      .toDate();
+                    user.update(
+                      { $set: { usernameUpdated: threeMonthsAgo } },
+                      function (err) {
+                        if (err) {
+                          return done(err);
+                        }
+                        // Second time changing it
+                        user2.username = _user.username + '02';
+                        agent.put('/api/users')
+                          .send(user2)
+                          .end(function (err, res) {
+                            if (err) {
+                              return done(err);
+                            }
+                            res.body.username.should.equal(user2.username);
+                            return done();
+                          });
+                      });
+                  });
+                });
+            });
+        });
+      });
   });
 
   afterEach(function (done) {

commit 9d9522283b53e3e9bf06b658086c556ef9128b68

Fix username field staying disabled after signing in through the UI.
Auth endpoints (signin, signup, confirmEmail, removeOAuthProvider) called
sanitizeProfile(user) with a single argument, so the response never included
own-profile fields like usernameUpdateAllowed. The Angular client stores these
responses directly in Authentication.user, leaving the username change field
disabled for eligible members until a full page reload. Broken since username
changes were introduced in #606; previously reported as #860.

Introduce an explicit sanitizeOwnProfile() and use it at every call site that
returns the authenticated user's own profile.

Fixes #2756

Co-authored-by: Cursor <cursoragent@cursor.com>

modules/core/server/controllers/core.server.controller.js
@@ -16,7 +16,7 @@ exports.renderIndex = function (req, res) {
 
   // Expose user
   if (req.user) {
-    renderVars.user = userProfile.sanitizeProfile(req.user, req.user);
+    renderVars.user = userProfile.sanitizeOwnProfile(req.user);
 
     // `sanitizeProfile` strips `roles` so they never leak for *other*
     // members, but the client needs the *current* user's own roles to drive

modules/users/server/controllers/users.authentication.server.controller.js
@@ -135,7 +135,7 @@ exports.signup = function (req, res) {
       function (user, done) {
         req.login(user, function (err) {
           // Remove sensitive data befor sending user
-          user = userProfile.sanitizeProfile(user);
+          user = userProfile.sanitizeOwnProfile(user);
 
           done(err, user);
         });
@@ -348,7 +348,7 @@ exports.signin = function (req, res, next) {
       statsObject.tags.status = 'success';
       statService.stat(statsObject, function () {
         // Remove sensitive data before sending out
-        user = userProfile.sanitizeProfile(user);
+        user = userProfile.sanitizeOwnProfile(user);
         res.json(user);
       });
     });
@@ -488,7 +488,7 @@ exports.removeOAuthProvider = function (req, res) {
           }
 
           // Remove sensitive data before sending out
-          user = userProfile.sanitizeProfile(user);
+          user = userProfile.sanitizeOwnProfile(user);
           res.json(user);
         });
       }
@@ -773,7 +773,7 @@ exports.confirmEmail = function (req, res) {
       function (result, user) {
         // Return authenticated user
         // Remove sensitive data befor sending user
-        result.user = userProfile.sanitizeProfile(user);
+        result.user = userProfile.sanitizeOwnProfile(user);
 
         return res.json(result);
       },

modules/users/server/controllers/users.password.server.controller.js
@@ -259,7 +259,7 @@ exports.reset = function (req, res) {
 
       // Return authenticated user
       function (user) {
-        return res.json(profileHandler.sanitizeProfile(user, user));
+        return res.json(profileHandler.sanitizeOwnProfile(user));
       },
     ],
     function (err) {

modules/users/server/controllers/users.profile.server.controller.js
@@ -817,18 +817,26 @@ function isUsernameUpdateAllowed(user) {
   return moment().isSameOrAfter(allowedDate);
 }
 
-/**
- * Sanitize profile before sending it to frontend
- * - Ensures certain fields are removed before publishing
- * - Collects tribe id's into one simple array
- * - Removes tribe references that don't exist anymore (i.e. they are removed from `tribes` table but reference ID remains in the user's table)
- * - Sanitize description in case
- *
- * @param {Object} profile - User profile to sanitize
- * @param {Object} authenticatedUser - Currently authenticated user profile. Allows some fields if this matches to `profile`
- * @return {Object} Sanitized profile.
- */
-exports.sanitizeProfile = function (profile, authenticatedUser) {
+// Some responses sanitize another member's public profile, while authenticated
+// user responses need own-account fields such as usernameUpdateAllowed.
+function sameUser(profile, authenticatedUser) {
+  if (
+    !profile ||
+    !authenticatedUser ||
+    !profile._id ||
+    !authenticatedUser._id
+  ) {
+    return false;
+  }
+
+  if (authenticatedUser._id.equals) {
+    return authenticatedUser._id.equals(profile._id);
+  }
+
+  return authenticatedUser._id.toString() === profile._id.toString();
+}
+
+function sanitizeProfile(profile, isOwnProfile) {
   if (!profile) {
     return;
   }
@@ -863,9 +871,6 @@ exports.sanitizeProfile = function (profile, authenticatedUser) {
       }
     });
   }
-
-  const isOwnProfile =
-    authenticatedUser && authenticatedUser._id.equals(profile._id);
 
   if (isOwnProfile) {
     // Is user allowed to update their username?
@@ -912,6 +917,25 @@ exports.sanitizeProfile = function (profile, authenticatedUser) {
   delete profile.__v;
 
   return profile;
+}
+
+/**
+ * Sanitize profile before sending it to frontend
+ * - Ensures certain fields are removed before publishing
+ * - Collects tribe id's into one simple array
+ * - Removes tribe references that don't exist anymore (i.e. they are removed from `tribes` table but reference ID remains in the user's table)
+ * - Sanitize description in case
+ *
+ * @param {Object} profile - User profile to sanitize
+ * @param {Object} authenticatedUser - Currently authenticated user profile. Allows some fields if this matches to `profile`
+ * @return {Object} Sanitized profile.
+ */
+exports.sanitizeProfile = function (profile, authenticatedUser) {
+  return sanitizeProfile(profile, sameUser(profile, authenticatedUser));
+};
+
+exports.sanitizeOwnProfile = function (profile) {
+  return sanitizeProfile(profile, true);
 };
 
 /**
@@ -1209,7 +1233,7 @@ exports.removePushRegistration = function (req, res) {
     } else {
       return res.send({
         message: 'Removed registration.',
-        user: exports.sanitizeProfile(user, user),
+        user: exports.sanitizeOwnProfile(user),
       });
     }
   });
@@ -1337,7 +1361,7 @@ exports.addPushRegistration = function (req, res) {
           }
           return res.send({
             message: 'Saved registration.',
-            user: exports.sanitizeProfile(user, user),
+            user: exports.sanitizeOwnProfile(user),
           });
         });
       }

modules/core/tests/server/core.server.controller.tests.js
@@ -4,7 +4,7 @@ const languagesArray = require('../../../../config/languages/languages-array.jso
 
 require('should');
 
-const sanitizeProfile = user => ({
+const sanitizeOwnProfile = user => ({
   sanitized: true,
   username: user.username,
 });
@@ -12,7 +12,7 @@ const coreController = proxyquire(
   '../../server/controllers/core.server.controller',
   {
     '../../../users/server/controllers/users.profile.server.controller': {
-      sanitizeProfile,
+      sanitizeOwnProfile,
     },
   },
 );

modules/users/tests/server/user-authentication.server.controller.tests.js
@@ -786,6 +786,8 @@ describe('Authentication controller OAuth/Facebook unit tests', () => {
     it('logs in a valid user', async () => {
       const [saved] = await utils.saveUsers(utils.generateUsers(1));
       const userDoc = await User.findById(saved._id);
+      userDoc.created = new Date('2020-05-27T19:23:44.733Z');
+      await userDoc.save();
       const controller = loadSigninController(() => [null, userDoc, null]);
       const res = deferredResponse();
       const req = { login: (user, cb) => cb() };
@@ -793,6 +795,7 @@ describe('Authentication controller OAuth/Facebook unit tests', () => {
       await res.waitForResponse();
       res.statusCode.should.equal(200);
       res.body._id.toString().should.equal(userDoc._id.toString());
+      res.body.usernameUpdateAllowed.should.be.true();
     });
 
     it('returns 400 when login fails after authentication', async () => {

tests/e2e/features/auth-account/account-settings.spec.js
@@ -4,6 +4,7 @@ const {
   DEFAULT_PASSWORD,
   createUser,
   registerViaApi,
+  signIn,
   signInViaApi,
 } = require('../../support/helpers');
 const {
@@ -69,6 +70,44 @@ test.describe.serial('account settings feature coverage', () => {
     });
     expect(valid.ok()).toBeTruthy();
     expect((await valid.json()).tagline).toBe(tagline);
+  });
+
+  test('older members who sign in through the UI can change username', async ({
+    page,
+    request,
+  }, testInfo) => {
+    annotateFeature(testInfo, 'account.details-update', [
+      'Account edit page is reachable.',
+      'Valid account details update persists.',
+      'Invalid account details show validation errors.',
+    ]);
+
+    const user = createUser();
+    await registerViaApi(request, user);
+    await updateUserByUsername(user.username, {
+      $set: { created: new Date('2020-05-27T19:23:44.733Z') },
+      $unset: { usernameUpdated: '' },
+    });
+
+    await signIn(page, user);
+
+    const profileResponse = await page.request.get(
+      `/api/users/${user.username}`,
+    );
+    expect(profileResponse.ok()).toBeTruthy();
+    const profile = await profileResponse.json();
+    expect(profile.usernameUpdateAllowed).toBe(true);
+
+    await page.evaluate(`
+      const injector = window.angular.element(document.body).injector();
+      injector.get('$state').go('profile-edit.account');
+      injector.get('$rootScope').$applyAsync();
+    `);
+
+    await expect(page).toHaveURL(/\/profile\/edit\/account/);
+    await expect(
+      page.locator('form[name="settingsUsernameForm"] input[name="username"]'),
+    ).toBeEnabled();
   });
 
   test('members can request and confirm profile removal', async ({