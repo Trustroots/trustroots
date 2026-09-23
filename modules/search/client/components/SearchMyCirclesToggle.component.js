9c180ea42467c721da65b2201f91c026ebbe724a
guaka <guaka@guaka.org>
2026-09-24 00:54:10 +0100

    Document and decouple API wrapper use

--- modules/search/client/components/SearchMyCirclesToggle.component.js
+++ modules/search/client/components/SearchMyCirclesToggle.component.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
+
 import { listMemberships } from '@/modules/tribes/client/api/tribes.api';
 
 export default function SearchMyCirclesToggle({ selectedTribeIds, onChange }) {
--- modules/support/client/api/support.api.js
+++ modules/support/client/api/support.api.js
@@ -4,6 +4,12 @@ export async function send(request) {
   return await axios.post('/api/support', request);
 }
 
+/**
+ * Report a member using their username.
+ * @param {{ username: string }} user Member being reported.
+ * @param {string} message Report text.
+ * @returns {Promise<void>}
+ */
 export async function reportMember(user, message) {
   await send({ message, reportMember: user.username });
 }
