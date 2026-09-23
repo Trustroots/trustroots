416dbc68861afaddea60714ba57f6df216d4d172
guaka <guaka@guaka.org>
2026-09-23 21:54:48 +0100

    Deduplicate client API wrappers

--- modules/search/client/components/SearchMyCirclesToggle.component.js
+++ modules/search/client/components/SearchMyCirclesToggle.component.js
@@ -1,11 +1,6 @@
-import axios from 'axios';
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
-
-async function fetchMemberships() {
-  const { data } = await axios.get('/api/users/memberships');
-  return data;
-}
+import { listMemberships } from '@/modules/tribes/client/api/tribes.api';
 
 export default function SearchMyCirclesToggle({ selectedTribeIds, onChange }) {
   const [userTribeIds, setUserTribeIds] = useState([]);
@@ -15,7 +10,7 @@ export default function SearchMyCirclesToggle({ selectedTribeIds, onChange }) {
   useEffect(() => {
     let isMounted = true;
 
-    fetchMemberships()
+    listMemberships()
       .then(memberships => {
         if (!isMounted) {
           return;
--- modules/support/client/api/support.api.js
+++ modules/support/client/api/support.api.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 export async function send(request) {
   return await axios.post('/api/support', request);
 }
+
+export async function reportMember(user, message) {
+  await send({ message, reportMember: user.username });
+}
